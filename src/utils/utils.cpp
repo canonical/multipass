4064ddce46fee8bfee50e237030b89150dabe95e
Chris Townsend <christopher.townsend@canonical.com>
2018-02-01 14:34:55 -0500

    utils: Match one or more leading '/' when invalidating target paths
    This fixes some cases where QDir::cleanPath() does not sanitize the leading '/'.

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -56,7 +56,7 @@ bool mp::utils::valid_hostname(const QString& name_string)
 bool mp::utils::invalid_target_path(const QString& target_path)
 {
     QString sanitized_path{QDir::cleanPath(target_path)};
-    QRegExp matcher("/|/(dev|proc|sys)(/.*)*|/home/ubuntu/*");
+    QRegExp matcher("/+|/+(dev|proc|sys)(/.*)*|/+home/ubuntu/*");
 
     return matcher.exactMatch(sanitized_path);
 }
