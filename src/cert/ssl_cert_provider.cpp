99a416e7a430b2b03ea65d83bd38bbf8de59e273
Alberto Aguirre <alberto.aguirre@canonical.com>
2018-06-22 14:35:51 -0500

    [cert] Fix issues with internal classes
    Fix potential memory leaks and potential dangling pointer issues.

--- src/cert/ssl_cert_provider.cpp
+++ src/cert/ssl_cert_provider.cpp
@@ -38,14 +38,6 @@ namespace mp = multipass;
 
 namespace
 {
-struct ECKeyDeleter
-{
-    void operator()(EC_KEY* key)
-    {
-        EC_KEY_free(key);
-    }
-};
-
 class BIOMem
 {
 public:
@@ -58,40 +50,36 @@ public:
     std::string as_string()
     {
         std::vector<char> pem(bio->num_write);
-        BIO_read(bio, pem.data(), pem.size());
+        BIO_read(bio.get(), pem.data(), pem.size());
         return {pem.begin(), pem.end()};
     }
 
     BIO* get() const
     {
-        return bio;
+        return bio.get();
     }
 
 private:
-    BIO* bio{BIO_new(BIO_s_mem())};
+    std::unique_ptr<BIO, decltype(BIO_free)*> bio{BIO_new(BIO_s_mem()), BIO_free};
 };
 
 class WritableFile
 {
 public:
-    WritableFile(const QString& name) : fp{fopen(name.toStdString().c_str(), "wb")}
+    WritableFile(const QString& name) : fp{fopen(name.toStdString().c_str(), "wb"), fclose}
     {
         if (fp == nullptr)
             throw std::runtime_error(
                 fmt::format("failed to open file '{}': {}({})", name.toStdString(), strerror(errno), errno));
     }
-    ~WritableFile()
-    {
-        fclose(fp);
-    }
 
     FILE* get() const
     {
-        return fp;
+        return fp.get();
     }
 
 private:
-    FILE* fp;
+    std::unique_ptr<FILE, decltype(fclose)*> fp;
 };
 
 class EVPKey
@@ -101,23 +89,26 @@ public:
     {
         if (key == nullptr)
             throw std::runtime_error("Failed to allocate EVP_PKEY");
-    }
 
-    ~EVPKey()
-    {
-        EVP_PKEY_free(key);
-    }
+        std::unique_ptr<EC_KEY, decltype(EC_KEY_free)*> ec_key(EC_KEY_new_by_curve_name(NID_X9_62_prime256v1),
+                                                               EC_KEY_free);
+        if (ec_key == nullptr)
+            throw std::runtime_error("Failed to allocate ec key structure");
 
-    void assign(EC_KEY* a_key)
-    {
-        if (!EVP_PKEY_assign_EC_KEY(key, a_key))
+        if (EC_KEY_generate_key(ec_key.get()) == false)
+            throw std::runtime_error("Failed to generate key");
+
+        if (!EVP_PKEY_assign_EC_KEY(key.get(), ec_key.get()))
             throw std::runtime_error("Failed to assign key");
+
+        // EVPKey has ownership now
+        ec_key.release();
     }
 
-    std::string as_pem()
+    std::string as_pem() const
     {
         BIOMem mem;
-        auto bytes = PEM_write_bio_PrivateKey(mem.get(), key, nullptr, nullptr, 0, nullptr, nullptr);
+        auto bytes = PEM_write_bio_PrivateKey(mem.get(), key.get(), nullptr, nullptr, 0, nullptr, nullptr);
         if (bytes == 0)
             throw std::runtime_error("Failed to export certificate in PEM format");
         return mem.as_string();
@@ -126,7 +117,7 @@ public:
     void write(const QString& name)
     {
         WritableFile file{name};
-        if (!PEM_write_PrivateKey(file.get(), key, nullptr, nullptr, 0, nullptr, nullptr))
+        if (!PEM_write_PrivateKey(file.get(), key.get(), nullptr, nullptr, 0, nullptr, nullptr))
             throw std::runtime_error(
                 fmt::format("Failed writing certificate private key to file '{}'", name.toStdString()));
 
@@ -135,62 +126,67 @@ public:
 
     EVP_PKEY* get() const
     {
-        return key;
+        return key.get();
     }
 
 private:
-    EVP_PKEY* key{EVP_PKEY_new()};
+    std::unique_ptr<EVP_PKEY, decltype(EVP_PKEY_free)*> key{EVP_PKEY_new(), EVP_PKEY_free};
 };
 
-long to_long(const std::array<uint8_t, 4>& input)
+long random_long()
 {
     long out{0};
+    std::array<uint8_t, 4> bytes;
+    RAND_bytes(bytes.data(), bytes.size());
 
-    out |= input[0] & 0xFF;
-    out |= (input[1] << 8) & 0xFF00;
-    out |= (input[2] << 16) & 0xFF0000;
-    out |= (input[3] << 24) & 0xFF000000;
+    out |= bytes[0] & 0xFF;
+    out |= (bytes[1] << 8) & 0xFF00;
+    out |= (bytes[2] << 16) & 0xFF0000;
+    out |= (bytes[3] << 24) & 0xFF000000;
     return out;
+}
+
+std::vector<unsigned char> as_vector(const std::string& v)
+{
+    return {v.begin(), v.end()};
 }
 
 class X509Cert
 {
 public:
-    explicit X509Cert()
+    explicit X509Cert(const EVPKey& key)
     {
         if (x509 == nullptr)
             throw std::runtime_error("Failed to allocate x509 cert structure");
 
-        std::array<uint8_t, 4> random_bytes;
-        RAND_bytes(random_bytes.data(), random_bytes.size());
-        ASN1_INTEGER_set(X509_get_serialNumber(x509), to_long(random_bytes));
-        X509_gmtime_adj(X509_get_notBefore(x509), 0);
-        X509_gmtime_adj(X509_get_notAfter(x509), 31536000L);
+        ASN1_INTEGER_set(X509_get_serialNumber(x509.get()), random_long());
+        X509_gmtime_adj(X509_get_notBefore(x509.get()), 0);
+        X509_gmtime_adj(X509_get_notAfter(x509.get()), 31536000L);
 
-        auto name = X509_get_subject_name(x509);
-        X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, (unsigned char*)"CA", -1, -1, 0);
-        X509_NAME_add_entry_by_txt(name, "O", MBSTRING_ASC, (unsigned char*)"Canonical", -1, -1, 0);
-        X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (unsigned char*)"localhost", -1, -1, 0);
-        X509_set_issuer_name(x509, name);
-    }
+        constexpr int APPEND_ENTRY{-1};
+        constexpr int ADD_RDN{0};
 
-    ~X509Cert()
-    {
-        X509_free(x509);
-    }
+        auto country = as_vector("US");
+        auto org = as_vector("Canonical");
+        auto cn = as_vector("localhost");
 
-    void sign(const EVPKey& key)
-    {
-        if (!X509_set_pubkey(x509, key.get()))
+        auto name = X509_get_subject_name(x509.get());
+        X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, country.data(), country.size(), APPEND_ENTRY, ADD_RDN);
+        X509_NAME_add_entry_by_txt(name, "O", MBSTRING_ASC, org.data(), org.size(), APPEND_ENTRY, ADD_RDN);
+        X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, cn.data(), cn.size(), APPEND_ENTRY, ADD_RDN);
+        X509_set_issuer_name(x509.get(), name);
+
+        if (!X509_set_pubkey(x509.get(), key.get()))
             throw std::runtime_error("Failed to set certificate public key");
-        if (!X509_sign(x509, key.get(), EVP_sha256()))
+
+        if (!X509_sign(x509.get(), key.get(), EVP_sha256()))
             throw std::runtime_error("Failed to sign certificate");
     }
 
     std::string as_pem()
     {
         BIOMem mem;
-        auto bytes = PEM_write_bio_X509(mem.get(), x509);
+        auto bytes = PEM_write_bio_X509(mem.get(), x509.get());
         if (bytes == 0)
             throw std::runtime_error("Failed to write certificate in PEM format");
         return mem.as_string();
@@ -199,12 +195,12 @@ public:
     void write(const QString& name)
     {
         WritableFile file{name};
-        if (!PEM_write_X509(file.get(), x509))
+        if (!PEM_write_X509(file.get(), x509.get()))
             throw std::runtime_error(fmt::format("Failed writing certificate to file '{}'", name.toStdString()));
     }
 
 private:
-    X509* x509{X509_new()};
+    std::unique_ptr<X509, decltype(X509_free)*> x509{X509_new(), X509_free};
 };
 
 std::string contents_of(const QString& name)
@@ -224,31 +220,6 @@ std::string contents_of(const QString& name)
         fmt::format("failed to open file '{}': {}({})", name.toStdString(), strerror(errno), errno));
 }
 
-auto make_key()
-{
-    std::unique_ptr<EC_KEY, ECKeyDeleter> ec_key(EC_KEY_new_by_curve_name(NID_X9_62_prime256v1));
-    if (ec_key == nullptr)
-        throw std::runtime_error("Failed to allocate ec key structure");
-
-    if (EC_KEY_generate_key(ec_key.get()) == false)
-        throw std::runtime_error("Failed to generate key");
-
-    EVPKey key;
-    key.assign(ec_key.get());
-
-    // EVPKey has ownership now
-    ec_key.release();
-
-    return key;
-}
-
-auto make_certificate(const EVPKey& key)
-{
-    X509Cert cert;
-    cert.sign(key);
-    return cert;
-}
-
 mp::SSLCertProvider::KeyCertificatePair make_cert_key_pair(const QDir& cert_dir)
 {
     auto priv_key_path = cert_dir.filePath("multipass_cert_key.pem");
@@ -259,8 +230,8 @@ mp::SSLCertProvider::KeyCertificatePair make_cert_key_pair(const QDir& cert_dir)
         return {contents_of(cert_path), contents_of(priv_key_path)};
     }
 
-    auto key = make_key();
-    auto cert = make_certificate(key);
+    EVPKey key;
+    X509Cert cert{key};
 
     key.write(priv_key_path);
     cert.write(cert_path);
