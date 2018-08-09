b67a41159914cfa6ed4e366d323dde06d603f8a1
Alberto Aguirre <alberto.aguirre@canonical.com>
2018-08-09 09:12:04 -0500

    Add a certificate store

---
+++ include/multipass/client_cert_store.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2018 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_CLIENT_CERT_STORE_H
+#define MULTIPASS_CLIENT_CERT_STORE_H
+
+#include <multipass/cert_store.h>
+#include <multipass/path.h>
+
+namespace multipass
+{
+class ClientCertStore : public CertStore
+{
+public:
+    explicit ClientCertStore(const multipass::Path& cert_dir);
+    void add_cert(const std::string& pem_cert) override;
+    std::string PEM_cert_chain() const override;
+
+private:
+    Path cert_dir;
+};
+} // namespace multipass
+#endif // MULTIPASS_CLIENT_CERT_STORE_H
