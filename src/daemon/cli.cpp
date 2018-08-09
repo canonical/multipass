15098da262ff0bc3a1df4425ea34bf914a0b6eb8
Alberto Aguirre <alberto.aguirre@canonical.com>
2018-08-09 09:11:34 -0500

    daemon: cli: Add option to specify server address

--- src/daemon/cli.cpp
+++ src/daemon/cli.cpp
@@ -19,9 +19,15 @@
 
 #include <multipass/logging/standard_logger.h>
 #include <multipass/platform.h>
+#include <multipass/utils.h>
+
+#include <fmt/format.h>
 
 #include <QCommandLineOption>
 #include <QCommandLineParser>
+
+#include <algorithm>
+#include <cctype>
 
 namespace mp = multipass;
 namespace mpl = multipass::logging;
@@ -39,7 +45,7 @@ multipass::logging::Level to_logging_level(const QString& value)
     if (value == "debug")
         return mpl::Level::debug;
 
-    throw std::runtime_error("Invalid logging verbosity: " + value.toStdString());
+    throw std::runtime_error("invalid logging verbosity: " + value.toStdString());
 }
 } // namespace
 
@@ -53,9 +59,14 @@ mp::DaemonConfigBuilder mp::cli::parse(const QCoreApplication& app)
     QCommandLineOption logger_option{"logger", "specifies which logger to use", "platform|stderr"};
     QCommandLineOption verbosity_option{
         {"V", "verbosity"}, "specifies the logging verbosity level", "error|warning|info|debug"};
+    QCommandLineOption address_option{"address",
+                                      "specifies which address to use for the multipassd service;"
+                                      " a socket can be specified using unix:<socket_file>",
+                                      "server_name:port"};
 
     parser.addOption(logger_option);
     parser.addOption(verbosity_option);
+    parser.addOption(address_option);
 
     parser.process(app);
 
@@ -72,7 +83,30 @@ mp::DaemonConfigBuilder mp::cli::parse(const QCoreApplication& app)
         else if (logger == "stderr")
             builder.logger = std::make_unique<mpl::StandardLogger>(builder.verbosity_level);
         else
-            throw std::runtime_error("Invalid logger option: " + logger.toStdString());
+            throw std::runtime_error(fmt::format("invalid logger option '{}'", logger.toStdString()));
+    }
+
+    if (parser.isSet(address_option))
+    {
+        auto address = parser.value(address_option).toStdString();
+        if (address.empty())
+            throw std::runtime_error("empty server address");
+
+        auto tokens = mp::utils::split(address, ":");
+        const auto server_name = tokens[0];
+        if (tokens.size() == 1u)
+        {
+            if (server_name == "unix")
+                throw std::runtime_error(fmt::format("missing socket file in address '{}'", address));
+            else
+                throw std::runtime_error(fmt::format("missing port number in address '{}'", address));
+        }
+
+        const auto port = tokens[1];
+        if (server_name != "unix" && !mp::utils::has_only_digits(port))
+            throw std::runtime_error(fmt::format("invalid port number in address '{}'", address));
+
+        builder.server_address = address;
     }
 
     return builder;
