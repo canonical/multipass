a39d35dea3a77f41c090a74ac78402b82f2583de
Alberto Aguirre <alberto.aguirre@canonical.com>
2018-09-05 20:54:57 -0500

    3rd-party: Update libssh
    Use newest libssh version.
    
    When attempting to read a stream, a closed channel means there's
    no data - make sure to check this condition instead of reporting
    an error.
    
    Since 0.8, libssh no longer requires a mutex/threads implementation
    nor does it need an explicit call to ssh_init() to initialize the
    library.

--- src/ssh/ssh_session.cpp
+++ src/ssh/ssh_session.cpp
@@ -13,8 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Authored by: Alberto Aguirre <alberto.aguirre@canonical.com>
- *
  */
 
 #include <multipass/ssh/ssh_session.h>
@@ -25,71 +23,14 @@
 #include <libssh/callbacks.h>
 #include <libssh/socket.h>
 
-#include <mutex>
 #include <sstream>
 #include <stdexcept>
-#include <thread>
 
 namespace mp = multipass;
 
-namespace
-{
-int mutex_init(void** priv)
-{
-    auto mutex = new std::mutex();
-    *priv = mutex;
-    return 0;
-}
-
-int mutex_destroy(void** lock)
-{
-    auto mutex = reinterpret_cast<std::mutex*>(*lock);
-    delete mutex;
-    return 0;
-}
-
-int mutex_lock(void** lock)
-{
-    auto mutex = reinterpret_cast<std::mutex*>(*lock);
-    mutex->lock();
-    return 0;
-}
-
-int mutex_unlock(void** lock)
-{
-    auto mutex = reinterpret_cast<std::mutex*>(*lock);
-    mutex->unlock();
-    return 0;
-}
-
-unsigned long thread_id()
-{
-    // libssh + boringSSL do not use the given thread id
-    return 0;
-}
-
-void init_ssh()
-{
-    static struct ssh_threads_callbacks_struct mutex_wrappers
-    {
-        "cpp11_mutex", mutex_init, mutex_destroy, mutex_lock, mutex_unlock, thread_id
-    };
-    ssh_threads_set_callbacks(&mutex_wrappers);
-    ssh_init();
-}
-
-auto initialize_session()
-{
-    static std::once_flag flag;
-    std::call_once(flag, init_ssh);
-
-    return ssh_new();
-}
-}
-
 mp::SSHSession::SSHSession(const std::string& host, int port, const std::string& username,
                            const SSHKeyProvider* key_provider)
-    : session{initialize_session(), ssh_free}
+    : session{ssh_new(), ssh_free}
 {
     if (session == nullptr)
         throw std::runtime_error("Could not allocate ssh session");
