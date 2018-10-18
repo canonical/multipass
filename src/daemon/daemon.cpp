0a5064f1bb8bdc3d043eaf35e15ae6c6ec5698d3
Chris Townsend <christopher.townsend@canonical.com>
2018-10-18 16:31:47 -0400

    rpc/mount: Use the grpc map type for the uid/gid maps

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -1092,18 +1092,10 @@ try // clang-format on
                             fmt::format("source \"{}\" is not readable", request->source_path()), "");
     }
 
-    std::unordered_map<int, int> gid_map;
-    std::unordered_map<int, int> uid_map;
-
-    for (const auto& map : request->gid_maps())
-    {
-        gid_map[map.host_gid()] = map.instance_gid();
-    }
-
-    for (const auto& map : request->uid_maps())
-    {
-        uid_map[map.host_uid()] = map.instance_uid();
-    }
+    std::unordered_map<int, int> uid_map{request->mount_maps().uid_map().begin(),
+                                         request->mount_maps().uid_map().end()};
+    std::unordered_map<int, int> gid_map{request->mount_maps().gid_map().begin(),
+                                         request->mount_maps().gid_map().end()};
 
     fmt::memory_buffer errors;
     for (const auto& path_entry : request->target_paths())
