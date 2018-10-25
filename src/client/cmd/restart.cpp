cb5a6eb6c8246994d83c75c3fdbad27b301e844f
Ricardo Abreu <ricab@ricabhome.org>
2018-10-25 22:57:12 +0100

    Check the arguments of the restart command.
    Provide a restart-specific help message and refuse any restart arguments that
    would not be accepted by start. In particular, this rejects stop's time and
    cancel options.

--- src/client/cmd/restart.cpp
+++ src/client/cmd/restart.cpp
@@ -19,21 +19,28 @@
 
 #include <multipass/cli/argparser.h>
 
+#include <cassert>
+
 namespace mp = multipass;
 namespace cmd = multipass::cmd;
 using RpcMethod = mp::Rpc::Stub;
 
 mp::ReturnCode cmd::Restart::run(mp::ArgParser* parser)
 {
-    // TODO what if they are not there ?
+    auto ret = parse_args(parser);
+    if(ret != ParseCode::Ok)
+        return parser->returnCodeFrom(ret);
+
     Command * stop_cmd = parser->findCommand(QStringLiteral("stop"));
     Command * start_cmd = parser->findCommand(QStringLiteral("start"));
+    assert(stop_cmd && start_cmd); /* The restart command is tightly coupled
+    with stop and start commands. The alternative would require either
+    significant refactoring or code duplication. */
 
-    // The argument sequences that are acceptable in a restart command are those
-    // that are acceptable to both stop and start commands.
     auto stop_ret = stop_cmd->run(parser);
-    return stop_ret == ReturnCode::Ok ? start_cmd->run(parser) : stop_ret;
-    // TODO refuse stop's other params
+    return stop_ret == ReturnCode::Ok ? start_cmd->run(parser) : stop_ret; /*
+    The argument sequences that are acceptable in a restart command are also
+    acceptable to both stop and start commands. */
 }
 
 std::string cmd::Restart::name() const { return "restart"; }
@@ -44,13 +51,40 @@ QString cmd::Restart::short_help() const
 }
 
 QString cmd::Restart::description() const
-{ // TODO get "help restart" to work
+{
     return QStringLiteral("Restart the named instances. Exits with return\n"
                           "code 0 when the instances restart, or with an\n"
                           "error code if any fail to restart.");
 }
 
 mp::ParseCode cmd::Restart::parse_args(mp::ArgParser* parser)
-{ // TODO try to get rid of this
-    return ParseCode::CommandFail;
+{   // This code should probably be factored out (for multiple commands)
+    parser->addPositionalArgument("name", "Names of instances to restart",
+                                  "<name> [<name> ...]");
+
+    QCommandLineOption all_option("all", "Restart all instances");
+    parser->addOption(all_option);
+
+    auto status = parser->commandParse(this);
+    if (status != ParseCode::Ok)
+        return status;
+
+    auto num_names = parser->positionalArguments().count();
+    if (num_names == 0 && !parser->isSet(all_option))
+    {
+        cerr << "Name argument or --all is required\n";
+        return ParseCode::CommandLineError;
+    }
+
+    if (num_names > 0 && parser->isSet(all_option))
+    {
+        cerr << "Cannot specify name";
+        if (num_names > 1)
+            cerr << "s";
+        cerr << " when --all option set\n";
+        return ParseCode::CommandLineError;
+    }
+
+    return ParseCode::Ok; /* We are done with checks and that is all we need in
+                             the reset command */
 }
