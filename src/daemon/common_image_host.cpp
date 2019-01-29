b9d61555f26a7ff4900ea9eb2456ff035842a0d5
Ricardo Abreu <ricab@ricabhome.org>
2019-01-29 17:46:30 +0000

    Add common vm image host.

---
+++ src/daemon/common_image_host.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "common_image_host.h"
+
+namespace mp = multipass;
+
+mp::CommonVMImageHost::CommonVMImageHost(std::chrono::seconds manifest_time_to_live)
+  : manifest_time_to_live{manifest_time_to_live}, last_update{}
+{
+}
+
+void mp::CommonVMImageHost::for_each_entry_do(const Action& action)
+{
+    update_manifests();
+
+    for_each_entry_do_impl(action);
+}
+
+auto mp::CommonVMImageHost::info_for_full_hash(const std::string& full_hash) -> VMImageInfo
+{
+    update_manifests();
+
+    return info_for_full_hash_impl(full_hash);
+}
+
+void mp::CommonVMImageHost::update_manifests()
+{
+    const auto now = std::chrono::steady_clock::now();
+    if ((now - last_update) > manifest_time_to_live || empty())
+    {
+        clear();
+        update_manifests_impl();
+
+        last_update = now;
+    }
+}
