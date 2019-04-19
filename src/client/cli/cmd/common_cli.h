0782fdb5d013e6b2e4066b5cc9fb41fb63370c73
Chris Townsend <christopher.townsend@canonical.com>
2019-04-19 12:53:47 -0400

    build: Fix build failure due to bad merge in last rebase on master

--- src/client/cli/cmd/common_cli.h
+++ src/client/cli/cmd/common_cli.h
@@ -42,8 +42,6 @@ InstanceNames add_instance_names(const ArgParser* parser);
 InstanceNames add_instance_names(const ArgParser* parser, const std::string& default_name);
 ParseCode handle_format_option(const ArgParser* parser, Formatter** chosen_formatter, std::ostream& cerr);
 std::string instance_action_message_for(const InstanceNames& instance_names, const std::string& action_name);
-ReturnCode standard_failure_handler_for(const std::string& command, std::ostream& cerr, const grpc::Status& status,
-                                        const std::string& error_details = std::string());
 ReturnCode run_cmd(const QStringList& args, const ArgParser* parser, std::ostream& cout, std::ostream& cerr);
 ReturnCode run_cmd_and_retry(const QStringList& args, const ArgParser* parser, std::ostream& cout, std::ostream& cerr);
 
