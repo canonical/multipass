b0c61653bf7336e4fd47da4f1a3f5b74ee2b8ff9
Ricardo Abreu <ricab@ricabhome.org>
2019-04-30 17:18:44 +0100

    Fix typo in error messages
    Fixes #753.

--- src/client/argparser.cpp
+++ src/client/argparser.cpp
@@ -135,7 +135,7 @@ mp::ParseCode mp::ArgParser::parse()
     }
 
     // Fall through
-    cout << "Error: Unkown Command: '" << qPrintable(requested_command) << "', see \"";
+    cout << "Error: Unknown Command: '" << qPrintable(requested_command) << "', see \"";
     cout << qPrintable(arguments.at(0)) << " --help\"\n";
     return ParseCode::CommandLineError;
 }
