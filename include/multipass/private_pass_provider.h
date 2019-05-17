8e2e033e7d1d9bae717807f359bba01b2ae14c55
Ricardo Abreu <ricab@ricabhome.org>
2019-05-17 17:49:06 +0100

    [utils] Add PrivatePassProvider along w/ test/demo

---
+++ include/multipass/private_pass_provider.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2019 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_PRIVATE_PASS_PROVIDER_H
+#define MULTIPASS_PRIVATE_PASS_PROVIDER_H
+
+namespace multipass
+{
+/*
+ * Helper class for classes that want to make a function publicly visible but
+ * only callable privately and by friends. To use, inherit (publicly) using the
+ * CRTP idiom. Then require a PrivatePass in the intended function, and provide
+ * the only `pass` attribute privately to friends. Friends can then call the
+ * method using the pass.
+ */
+template <typename T>
+class PrivatePassProvider
+{
+public:
+    class PrivatePass
+    {
+    private:
+        constexpr PrivatePass();
+        friend class PrivatePassProvider<T>;
+    };
+
+private:
+    static constexpr const PrivatePass pass{}; // token to prove friendship
+    friend T;
+};
+} // namespace multipass
+
+template <typename T>
+inline constexpr multipass::PrivatePassProvider<T>::PrivatePass::PrivatePass()
+{
+}
+
+template <typename T>
+constexpr const typename multipass::PrivatePassProvider<T>::PrivatePass multipass::PrivatePassProvider<T>::pass;
+
+#endif // MULTIPASS_PRIVATE_PASS_PROVIDER_H
