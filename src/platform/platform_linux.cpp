ba801668a0e77d400fdec846539a0e42723ae223
Ricardo Abreu <ricab@ricabhome.org>
2019-05-29 17:57:57 +0100

    [settings] Add driver setting
    Whose default depends on the platform.

--- src/platform/platform_linux.cpp
+++ src/platform/platform_linux.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2018 Canonical, Ltd.
+ * Copyright (C) 2017-2019 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,8 +12,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Authored by: Alberto Aguirre <alberto.aguirre@canonical.com>
  *
  */
 
@@ -47,6 +45,11 @@ mp::ProcessFactory* process_factory()
 std::string mp::platform::default_server_address()
 {
     return {"unix:/run/multipass_socket"};
+}
+
+QString mp::platform::default_driver()
+{
+    return QStringLiteral("qemu");
 }
 
 mp::VirtualMachineFactory::UPtr mp::platform::vm_backend(const mp::Path& data_dir)
