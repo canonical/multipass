0f6471e40526c24cfc1bd7c0b72b9bb324353ca2
Ricardo Abreu <ricab@ricabhome.org>
2019-06-19 18:05:07 +0100

    [settings] Improve error messages

--- src/utils/settings.cpp
+++ src/utils/settings.cpp
@@ -85,9 +85,9 @@ void check_status(const QSettings& settings, const QString& attempted_operation)
 {
     auto status = settings.status();
     if (status || exists_but_unreadable(settings.fileName()))
-        throw mp::PersistentSettingsException{attempted_operation, status == QSettings::FormatError
-                                                                       ? QStringLiteral("format")
-                                                                       : QStringLiteral("access")};
+        throw mp::PersistentSettingsException{
+            attempted_operation, status == QSettings::FormatError ? QStringLiteral("format error")
+                                                                  : QStringLiteral("access error (consider `sudo`)")};
 }
 
 QString checked_get(QSettings& settings, const QString& key, const QString& fallback)
