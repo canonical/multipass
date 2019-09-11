81a436e3a233d1e0a97eeea9ba5c8327b8ea83b7
Ricardo Abreu <ricab@ricabhome.org>
2019-09-11 16:53:42 +0100

    [test] Use extracted env changing function

--- tests/test_platform_linux.cpp
+++ tests/test_platform_linux.cpp
@@ -97,17 +97,14 @@ TEST_F(PlatformLinux, test_autostart_desktop_file_properly_placed)
     QDir test_dir{QDir::temp().filePath(QString{"%1_%2"}.arg(mp::client_name, "autostart_test"))};
     ASSERT_FALSE(test_dir.exists());
 
-    const auto cleanup = sg::make_scope_guard(
-        [&test_dir, config_home_save = qgetenv("XDG_CONFIG_HOME"), data_dir_save = qgetenv("XDG_DATA_DIRS")]() {
-            test_dir.removeRecursively(); // succeeds if not there
-            qputenv("XDG_CONFIG_HOME", config_home_save);
-            qputenv("XDG_DATA_DIRS", data_dir_save);
-        });
-
     QDir data_dir{test_dir.filePath("data")};
     QDir config_dir{test_dir.filePath("config")};
-    qputenv("XDG_CONFIG_HOME", config_dir.path().toLatin1());
-    qputenv("XDG_DATA_DIRS", data_dir.path().toLatin1());
+    const auto guard_xdg_config = temporarily_change_env("XDG_CONFIG_HOME", config_dir.path().toLatin1());
+    const auto guard_xdg_data = temporarily_change_env("XDG_DATA_DIRS", data_dir.path().toLatin1());
+
+    const auto cleanup = sg::make_scope_guard([&test_dir]() {
+        test_dir.removeRecursively(); // succeeds if not there
+    });
 
     QDir mp_data_dir{data_dir.filePath(mp::client_name)};
     QDir autostart_dir{config_dir.filePath("autostart")};
