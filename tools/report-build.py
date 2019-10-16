107179e536077d287b5c312b5d4ab99ba4de1dde
Michał Sawicz <michal@sawicz.net>
2019-10-16 17:17:58 +0200

    [travis] update previous comment if applicable
    If there is a recent comment from us already, update it instead of
    adding a new one. This should normally keep all builds of the same
    commit together.

--- tools/report-build.py
+++ tools/report-build.py
@@ -6,6 +6,7 @@ import requests
 import sys
 
 HEADERS = {"Authorization": "Bearer {}".format(os.environ["GITHUB_TOKEN"])}
+EVENT_COUNT = 10
 
 
 def run_query(query, variables={}):
@@ -24,17 +25,35 @@ def run_query(query, variables={}):
         return request.json()
 
 
-pull_q = """
-query GetPullRequest($owner:String!, $name:String!, $number:Int!) {
+events_q = """
+query GetEvents($owner: String!, $name: String!, $pull_request: Int!, $count: Int!) {
   repository(owner: $owner, name: $name) {
-    pullRequest(number: $number) {
+    pullRequest(number: $pull_request) {
       id
+      timelineItems(last: $count, itemTypes: [PULL_REQUEST_COMMIT, ISSUE_COMMENT, HEAD_REF_FORCE_PUSHED_EVENT]) {
+        edges {
+          node {
+            __typename
+            ... on IssueComment {
+              author {
+                login
+              }
+              body
+              id
+              viewerCanUpdate
+            }
+          }
+        }
+      }
     }
+  }
+  viewer {
+    login
   }
 }
 """
 
-comment_m = """
+add_comment_m = """
 mutation AddComment($comment: AddCommentInput!) {
   addComment(input: $comment) {
     subject {
@@ -44,21 +63,62 @@ mutation AddComment($comment: AddCommentInput!) {
 }
 """
 
+update_comment_m = """
+mutation UpdateComment($comment: UpdateIssueCommentInput!) {
+  updateIssueComment(input: $comment) {
+    issueComment {
+      id
+    }
+  }
+}
+"""
+
 
 def main():
-    comment_body = "{} build available: [{}]({})".format(
-        build_id, build_url.split("/")[-1], build_url)
+    previous_comment = None
+    build_id = sys.argv[1]
+    build_url = sys.argv[2]
+    comment_body = ["{} build available: [{}]({})".format(
+        build_id, build_url.split("/")[-1], build_url)]
 
-    pull_request = run_query(pull_q, {
+    events_d = run_query(events_q, {
         "owner": os.environ["TRAVIS_REPO_SLUG"].split("/")[0],
         "name": os.environ["TRAVIS_REPO_SLUG"].split("/")[1],
-        "number": int(os.environ["TRAVIS_PULL_REQUEST"]),
-    })["data"]["repository"]["pullRequest"]
+        "pull_request": int(os.environ["TRAVIS_PULL_REQUEST"]),
+        "count": EVENT_COUNT
+    })["data"]
 
-    run_query(comment_m, {
+    viewer = events_d["viewer"]
+    pull_request = events_d["repository"]["pullRequest"]
+    events = pull_request["timelineItems"]["edges"]
+
+    for event in reversed(events):
+        try:
+            if event["node"]["author"]["login"] == viewer["login"] and event["node"]["viewerCanUpdate"]:
+                # found a recent commit we can update
+                for line in event["node"]["body"].splitlines():
+                    # include all builds with a different id
+                    if not line.startswith(build_id):
+                        comment_body.append(line)
+
+                comment_body.sort(key=lambda v: (v.upper(), v))
+
+                run_query(update_comment_m, {
+                    "comment": {
+                        "id": event["node"]["id"],
+                        "body": "\n".join(comment_body),
+                    }
+                })
+
+                sys.exit(0)
+        except KeyError:
+            # we've encountered a commit or a force event, add a new comment below
+            break
+
+    run_query(add_comment_m, {
         "comment": {
             "subjectId": pull_request["id"],
-            "body": comment_body
+            "body": "\n".join(comment_body),
         }
     })
 
