57c95ef8f664a6d585b2dbfa829d78fffe7f65ac
Michał Sawicz <michal@sawicz.net>
2019-10-18 12:30:47 +0200

    [travis] refactor report-build into classes
    In preparation for more complex operations.

--- tools/report-build.py
+++ tools/report-build.py
@@ -5,83 +5,106 @@ import pprint
 import requests
 import sys
 
-HEADERS = {"Authorization": "Bearer {}".format(os.environ["GITHUB_TOKEN"])}
+GITHUB_ENDPOINT = "https://api.github.com/graphql"
+GITHUB_HEADERS = {
+    "Authorization": "Bearer {}".format(os.environ["GITHUB_TOKEN"])
+}
 EVENT_COUNT = 10
 
 
-def run_query(query, variables={}):
-    request = requests.post('https://api.github.com/graphql',
-                            json={'query': query, 'variables': variables},
-                            headers=HEADERS)
-    if request.status_code != 200:
-        raise Exception("Query failed with status code {}:\n{}".format(
-            request.status_code, request.content.decode("utf-8")
-        ))
-    elif "errors" in request.json():
-        raise Exception("Query failed with errors:\n{}".format(
-            pprint.pformat(request.json())
-        ))
-    else:
-        return request.json()
+class GraphQLQuery():
+    api_endpoint = None
+    headers = {}
+    query = None
+    request = None
+
+    def run(self, variables={}):
+        if None in (self.api_endpoint, self.query):
+            raise Exception(
+                "You need to subclass GraphQLQuery and provide the"
+                " 'query' and 'api_endpoint' attributes")
+
+        self.request = requests.post(
+            self.api_endpoint, headers=self.headers,
+            json={'query': self.query, 'variables': variables})
+
+        if self.request.status_code != 200:
+            raise Exception("Query failed with status code {}:\n{}".format(
+                self.request.status_code, self.request.content.decode("utf-8")
+            ))
+        elif "errors" in self.request.json():
+            raise Exception("Query failed with errors:\n{}".format(
+                pprint.pformat(self.request.json())
+            ))
+        else:
+            return self.request.json()
 
 
-events_q = """
-query GetEvents($owner: String!, $name: String!, $pull_request: Int!, $count: Int!) {
-  repository(owner: $owner, name: $name) {
-    pullRequest(number: $pull_request) {
-      id
-      timelineItems(last: $count, itemTypes: [PULL_REQUEST_COMMIT, ISSUE_COMMENT, HEAD_REF_FORCE_PUSHED_EVENT]) {
-        edges {
-          node {
-            __typename
-            ... on IssueComment {
-              author {
-                login
-              }
-              body
+class GitHubQLQuery(GraphQLQuery):
+    api_endpoint = GITHUB_ENDPOINT
+    headers = GITHUB_HEADERS
+
+
+class GetEvents(GitHubQLQuery):
+    query = """
+        query GetEvents($owner: String!, $name: String!, $pull_request: Int!, $count: Int!) {
+          repository(owner: $owner, name: $name) {
+            pullRequest(number: $pull_request) {
               id
-              viewerCanUpdate
+              timelineItems(last: $count, itemTypes: [PULL_REQUEST_COMMIT, ISSUE_COMMENT, HEAD_REF_FORCE_PUSHED_EVENT]) {
+                edges {
+                  node {
+                    ... on IssueComment {
+                      author {
+                        login
+                      }
+                      body
+                      id
+                      viewerCanUpdate
+                    }
+                  }
+                }
+              }
+            }
+          }
+          viewer {
+            login
+          }
+        }
+    """
+
+
+class AddComment(GitHubQLQuery):
+    query = """
+        mutation AddComment($comment: AddCommentInput!) {
+          addComment(input: $comment) {
+            subject {
+              id
             }
           }
         }
-      }
-    }
-  }
-  viewer {
-    login
-  }
-}
-"""
+    """
 
-add_comment_m = """
-mutation AddComment($comment: AddCommentInput!) {
-  addComment(input: $comment) {
-    subject {
-      id
-    }
-  }
-}
-"""
 
-update_comment_m = """
-mutation UpdateComment($comment: UpdateIssueCommentInput!) {
-  updateIssueComment(input: $comment) {
-    issueComment {
-      id
-    }
-  }
-}
-"""
+class UpdateComment(GitHubQLQuery):
+    query = """
+        mutation UpdateComment($comment: UpdateIssueCommentInput!) {
+          updateIssueComment(input: $comment) {
+            issueComment {
+              id
+            }
+          }
+        }
+    """
 
 
 def main():
-    previous_comment = None
     build_id = sys.argv[1]
     build_url = sys.argv[2]
     comment_body = ["{} build available: [{}]({})".format(
         build_id, build_url.split("/")[-1], build_url)]
 
-    events_d = run_query(events_q, {
+    events_d = GetEvents().run({
         "owner": os.environ["TRAVIS_REPO_SLUG"].split("/")[0],
         "name": os.environ["TRAVIS_REPO_SLUG"].split("/")[1],
         "pull_request": int(os.environ["TRAVIS_PULL_REQUEST"]),
@@ -94,7 +117,8 @@ def main():
 
     for event in reversed(events):
         try:
-            if event["node"]["author"]["login"] == viewer["login"] and event["node"]["viewerCanUpdate"]:
+            if(event["node"]["author"]["login"] == viewer["login"]
+               and event["node"]["viewerCanUpdate"]):
                 # found a recent commit we can update
                 for line in event["node"]["body"].splitlines():
                     # include all builds with a different id
@@ -103,7 +127,7 @@ def main():
 
                 comment_body.sort(key=lambda v: (v.upper(), v))
 
-                run_query(update_comment_m, {
+                UpdateComment().run({
                     "comment": {
                         "id": event["node"]["id"],
                         "body": "\n".join(comment_body),
@@ -112,10 +136,11 @@ def main():
 
                 sys.exit(0)
         except KeyError:
-            # we've encountered a commit or a force event, add a new comment below
+            # we've encountered a commit or a force event,
+            # break out and add a new comment below
             break
 
-    run_query(add_comment_m, {
+    AddComment().run({
         "comment": {
             "subjectId": pull_request["id"],
             "body": "\n".join(comment_body),
