2e4912e483ee4592f90381dc84f79cfb70fb7aac
Michał Sawicz <michal@sawicz.net>
2020-02-24 18:24:39 +0100

    [logging] add trace level (Fixes #1371)
    Move AppArmor profile logging contents to the trace level to avoid spamming the logs.

--- src/platform/logger/journald_logger.cpp
+++ src/platform/logger/journald_logger.cpp
@@ -29,6 +29,8 @@ constexpr auto to_syslog_priority(const mpl::Level& level) noexcept
 {
     switch (level)
     {
+    case mpl::Level::trace:
+        return LOG_DEBUG;
     case mpl::Level::debug:
         return LOG_DEBUG;
     case mpl::Level::error:
