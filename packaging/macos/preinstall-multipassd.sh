af11db589670baf5f674af0fabbc6db399406950
Michał Sawicz <michal@sawicz.net>
2020-02-26 17:42:27 +0100

    [macos] set 10.12 as our minimum version
    Includes a check in preinstall script.

--- packaging/macos/preinstall-multipassd.sh
+++ packaging/macos/preinstall-multipassd.sh
@@ -5,10 +5,16 @@ set -e
 # Ref: https://developer.apple.com/documentation/hypervisor
 CPU_OK=`sysctl -n kern.hv_support`
 
+# TODO: Create an installer plugin to generate a user-friendly dialog if
+# the CPU is missing required features for multipass
+
 if [ "$CPU_OK" -ne "1" ] ; then
-    # TODO: Create an installer plugin to generate a user-friendly dialog if
-    # the CPU is missing required features for multipass
     echo "Your CPU is missing required features for the Hypervisor.framework"
+    exit 1
+fi
+
+if [ $( sw_vers -productVersion | cut -d. -f2 ) -lt 12 ]; then
+    echo "Multipass currently requires macOS 10.12 or newer"
     exit 1
 fi
 
