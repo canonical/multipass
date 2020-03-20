4251fd6104fccff16e9031544e4e151804ff69f8
Ricardo Abreu <ricab@ricabhome.org>
2020-03-20 14:58:22 +0000

    [winterm] Add profile GUID constant
    So it can be accessed in tests.

--- include/multipass/constants.h
+++ include/multipass/constants.h
@@ -22,14 +22,22 @@ namespace multipass
 {
 constexpr auto client_name = "multipass";
 constexpr auto daemon_name = "multipassd";
+
 constexpr auto min_memory_size = "128M";
 constexpr auto min_disk_size = "512M";
 constexpr auto min_cpu_cores = "1";
+
 constexpr auto default_memory_size = "1G";
 constexpr auto default_disk_size = "5G";
 constexpr auto default_cpu_cores = min_cpu_cores;
+
 constexpr auto home_automount_dir = "Home";
+
 constexpr auto driver_env_var = "MULTIPASS_VM_DRIVER";
+
+constexpr auto winterm_profile_guid =
+    "{aaaa9e6d-1e09-4be6-b76c-82b4ba1885fb}"; // identifies the primary Multipass profile in Windows Terminal
+
 constexpr auto petenv_key = "client.primary-name";     // This will eventually be moved to some dynamic settings schema
 constexpr auto driver_key = "local.driver";            // idem
 constexpr auto autostart_key = "client.gui.autostart"; // idem
