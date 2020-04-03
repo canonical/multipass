0dacaaf692c10693deae656fe6de71826cac5da5
Ricardo Abreu <ricab@ricabhome.org>
2020-04-03 19:26:52 +0100

    [util] Remove leftover QStandardPaths include

--- src/platform/platform_osx.cpp
+++ src/platform/platform_osx.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2019 Canonical, Ltd.
+ * Copyright (C) 2017-2020 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,7 +31,6 @@
 
 #include <QDir>
 #include <QFileInfo>
-#include <QStandardPaths>
 #include <QString>
 #include <QtGlobal>
 
