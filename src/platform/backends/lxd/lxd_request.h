758c6281e098f4a561df783b616e947993f50cba
Michał Sawicz <michal@sawicz.net>
2020-04-08 09:02:24 +0200

    [lxd] drop extra includes

--- src/platform/backends/lxd/lxd_request.h
+++ src/platform/backends/lxd/lxd_request.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Canonical, Ltd.
+ * Copyright (C) 2019-2020 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,8 +36,6 @@
 #include <multipass/optional.h>
 #include <multipass/format.h>
 #include <multipass/logging/log.h>
-
-#include <QDebug>
 
 namespace mp = multipass;
 namespace mpl = multipass::logging;
