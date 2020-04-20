e8031b557dacc2ae7cbfacd2bc26184ff0e0af93
Chris Townsend <christopher.townsend@canonical.com>
2020-04-20 11:01:17 -0400

    [lxd] Remove unnecessary forward declarations and update copyright date

--- src/platform/backends/lxd/lxd_virtual_machine.h
+++ src/platform/backends/lxd/lxd_virtual_machine.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Canonical, Ltd.
+ * Copyright (C) 2019-2020 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,8 +29,6 @@
 
 namespace multipass
 {
-class PowerShell;
-class SSHKeyProvider;
 class VirtualMachineDescription;
 class VMStatusMonitor;
 
