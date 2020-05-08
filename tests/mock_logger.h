4a81a191405ab88a3095f1ad9f34d2b33fa5c95d
Ricardo Abreu <ricab@ricabhome.org>
2020-05-08 19:47:38 +0100

    [test] Add test utilities to test logging
    These are currently used only on the private side.

--- tests/mock_logger.h
+++ tests/mock_logger.h
@@ -18,9 +18,13 @@
 #ifndef MULTIPASS_MOCK_LOGGER_H
 #define MULTIPASS_MOCK_LOGGER_H
 
+#include <multipass/logging/log.h>
 #include <multipass/logging/logger.h>
 
+#include <scope_guard.hpp>
+
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 namespace multipass
 {
@@ -39,6 +43,25 @@ public:
         return Property(&multipass::logging::CString::c_str, matcher);
     }
 };
+
+inline auto guarded_mock_logger()
+{
+    auto guard = sg::make_scope_guard([] { multipass::logging::set_logger(nullptr); });
+    auto mock_logger = std::make_shared<testing::StrictMock<MockLogger>>();
+    multipass::logging::set_logger(mock_logger);
+
+    return std::make_pair(mock_logger, std::move(guard)); // needs to be moved into the pair first (NRVO does not apply)
+}
+
+inline auto expect_log(multipass::logging::Level lvl, const std::string& substr)
+{
+    auto [mock_logger, guard] = guarded_mock_logger();
+
+    EXPECT_CALL(*mock_logger, log(lvl, testing::_, MockLogger::make_cstring_matcher(testing::HasSubstr(substr))));
+
+    return std::move(guard); // needs to be moved because it's only part of an implicit local var (NRVO does not apply)
+}
+
 } // namespace test
 } // namespace multipass
 
