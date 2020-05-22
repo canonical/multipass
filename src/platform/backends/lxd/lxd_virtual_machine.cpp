aa799ccaae7c48b79e33d5af9b53a90c8a3a6f72
Chris Townsend <christopher.townsend@canonical.com>
2020-05-22 12:42:06 -0400

    [lxd] Default to using 10GB for disk size if less than that is requested

--- src/platform/backends/lxd/lxd_virtual_machine.cpp
+++ src/platform/backends/lxd/lxd_virtual_machine.cpp
@@ -93,6 +93,20 @@ mp::optional<mp::IPAddress> get_ip_for(const QString& name, mp::NetworkAccessMan
     return mp::nullopt;
 }
 
+mp::MemorySize get_minimum_disk_size(const mp::MemorySize& requested_disk_size)
+{
+    mp::MemorySize lxd_min_disk_size{"10G"};
+
+    if (requested_disk_size > lxd_min_disk_size)
+    {
+        return requested_disk_size;
+    }
+    else
+    {
+        return lxd_min_disk_size;
+    }
+}
+
 } // namespace
 
 mp::LXDVirtualMachine::LXDVirtualMachine(const VirtualMachineDescription& desc, VMStatusMonitor& monitor,
@@ -125,11 +139,12 @@ mp::LXDVirtualMachine::LXDVirtualMachine(const VirtualMachineDescription& desc, 
         if (!desc.user_data_config.IsNull())
             config["user.user-data"] = QString::fromStdString(mpu::emit_cloud_config(desc.user_data_config));
 
-        QJsonObject devices{{"config", QJsonObject{{"source", "cloud-init:config"}, {"type", "disk"}}},
-                            {"root", QJsonObject{{"path", "/"},
-                                                 {"pool", "default"},
-                                                 {"size", QString::number(desc.disk_space.in_bytes())},
-                                                 {"type", "disk"}}}};
+        QJsonObject devices{
+            {"config", QJsonObject{{"source", "cloud-init:config"}, {"type", "disk"}}},
+            {"root", QJsonObject{{"path", "/"},
+                                 {"pool", "default"},
+                                 {"size", QString::number(get_minimum_disk_size(desc.disk_space).in_bytes())},
+                                 {"type", "disk"}}}};
 
         QJsonObject virtual_machine{
             {"name", name},
