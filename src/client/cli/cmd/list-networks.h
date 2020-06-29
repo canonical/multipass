6981d42f682b3bb5ef7079cc0d4443b91ce43ef3
Luis Peñaranda <luis.penaranda@canonical.com>
2020-06-29 16:54:29 -0300

    Add skeleton for the command list-networks
    For the time being, the answer is a hardcoded list of network interfaces.

---
+++ src/client/cli/cmd/list-networks.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_LIST_COMMANDS_H
+#define MULTIPASS_LIST_COMMANDS_H
+
+#include <multipass/cli/command.h>
+
+namespace multipass
+{
+class Formatter;
+
+namespace cmd
+{
+class ListNetworks final : public Command
+{
+public:
+    using Command::Command;
+    ReturnCode run(ArgParser* parser) override;
+
+    std::string name() const override;
+    std::vector<std::string> aliases() const override;
+    QString short_help() const override;
+    QString description() const override;
+
+private:
+    ParseCode parse_args(ArgParser* parser) override;
+
+    Formatter* chosen_formatter;
+};
+} // namespace cmd
+} // namespace multipass
+
+#endif // MULTIPASS_LIST_COMMANDS_H
