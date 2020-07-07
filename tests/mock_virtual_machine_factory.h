1f20570f079cfe726e37e76a665a3fba8380f9a7
Luis Peñaranda <luis.penaranda@canonical.com>
2020-07-07 14:23:55 -0300

    Move list-networks to VM factory

--- tests/mock_virtual_machine_factory.h
+++ tests/mock_virtual_machine_factory.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2019 Canonical, Ltd.
+ * Copyright (C) 2017-2020 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -41,6 +41,7 @@ struct MockVirtualMachineFactory : public VirtualMachineFactory
     MOCK_METHOD0(get_backend_version_string, QString());
     MOCK_METHOD5(create_image_vault,
                  VMImageVault::UPtr(std::vector<VMImageHost*>, URLDownloader*, const Path&, const Path&, const days&));
+    MOCK_METHOD0(list_networks, std::vector<std::tuple<std::string, std::string, std::string>>());
 };
 }
 }
--- tests/stub_virtual_machine_factory.h
+++ tests/stub_virtual_machine_factory.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2019 Canonical, Ltd.
+ * Copyright (C) 2017-2020 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -73,6 +73,11 @@ struct StubVirtualMachineFactory : public multipass::VirtualMachineFactory
                                                      const days& days_to_expire) override
     {
         return std::make_unique<StubVMImageVault>();
+    }
+
+    std::vector<std::tuple<std::string, std::string, std::string>> list_networks() override
+    {
+        return std::vector<std::tuple<std::string, std::string, std::string>>();
     }
 };
 }
