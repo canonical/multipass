dd36f8a375de7f06bb1121cc26e630d2ec3b9d2c
Ricardo Abreu <ricab@ricabhome.org>
2020-07-23 13:05:00 +0100

    [networking] Make `list_networks` const

--- src/platform/backends/lxd/lxd_virtual_machine_factory.h
+++ src/platform/backends/lxd/lxd_virtual_machine_factory.h
@@ -51,7 +51,7 @@ public:
     VMImageVault::UPtr create_image_vault(std::vector<VMImageHost*> image_hosts, URLDownloader* downloader,
                                           const Path& cache_dir_path, const Path& data_dir_path,
                                           const days& days_to_expire) override;
-    std::vector<NetworkInterfaceInfo> list_networks() override;
+    std::vector<NetworkInterfaceInfo> list_networks() const override;
 
 private:
     NetworkAccessManager::UPtr manager;
