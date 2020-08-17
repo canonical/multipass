46eb2a758aab6aacb14fbab3afbc06224cfed184
Luis Peñaranda <luis.penaranda@canonical.com>
2020-08-17 11:18:39 -0300

    Fix hyperkit test.

--- tests/hyperkit/test_hyperkit_backend.cpp
+++ tests/hyperkit/test_hyperkit_backend.cpp
@@ -41,7 +41,7 @@ struct HyperkitBackend : public testing::Test
                                                       mp::MemorySize{"3M"},
                                                       mp::MemorySize{}, // not used
                                                       "pied-piper-valley",
-                                                      "",
+                                                      {{"default", ""}},
                                                       "",
                                                       {dummy_image.name(), "", "", "", "", "", "", {}},
                                                       dummy_cloud_init_iso.name()};
