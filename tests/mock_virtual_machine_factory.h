a6f7a98f0bf6064ced7431e0b8ba01d564e40812
Luis Peñaranda <luis.penaranda@canonical.com>
2020-08-24 16:00:58 -0300

    Adapt tests to the new functions in the factory.

--- tests/mock_virtual_machine_factory.h
+++ tests/mock_virtual_machine_factory.h
@@ -42,6 +42,8 @@ struct MockVirtualMachineFactory : public VirtualMachineFactory
     MOCK_METHOD5(create_image_vault,
                  VMImageVault::UPtr(std::vector<VMImageHost*>, URLDownloader*, const Path&, const Path&, const days&));
     MOCK_CONST_METHOD0(list_networks, std::vector<NetworkInterfaceInfo>());
+    MOCK_CONST_METHOD6(make_cloud_init_image,
+                       Path(const std::string&, const QDir&, YAML::Node&, YAML::Node&, YAML::Node&, YAML::Node&));
 };
 }
 }
