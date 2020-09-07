f1ef010cc94e3f76aca1b0a3da3c74773b5a64a7
Luis Peñaranda <luis.penaranda@canonical.com>
2020-09-07 16:52:16 -0300

    Improve comments on backends list-networks.

--- src/platform/backends/lxd/lxd_virtual_machine_factory.cpp
+++ src/platform/backends/lxd/lxd_virtual_machine_factory.cpp
@@ -128,7 +128,8 @@ mp::VMImageVault::UPtr mp::LXDVirtualMachineFactory::create_image_vault(std::vec
     return std::make_unique<mp::LXDVMImageVault>(image_hosts, manager.get(), base_url, days_to_expire);
 }
 
+// This function returns an empty list as long as bridging is not implemented in the backend.
 auto multipass::LXDVirtualMachineFactory::list_networks() const -> std::vector<NetworkInterfaceInfo>
 {
-    return {}; // TODO
+    return {};
 }
