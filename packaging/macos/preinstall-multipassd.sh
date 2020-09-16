712ba095c9005261cfd11da5ec02e556a2abe67c
Chris Townsend <christopher.townsend@canonical.com>
2020-09-16 13:56:12 -0400

    [packaging/macos] Explicitly detect for version 10
    Fixes canonical/multipass#1664

--- packaging/macos/preinstall-multipassd.sh
+++ packaging/macos/preinstall-multipassd.sh
@@ -13,7 +13,7 @@ if [ "$CPU_OK" -ne "1" ] ; then
     exit 1
 fi
 
-if [ $( sw_vers -productVersion | cut -d. -f2 ) -lt 12 ]; then
+if [ $( sw_vers -productVersion | cut -d. -f1 ) -eq 10 -a $( sw_vers -productVersion | cut -d. -f2 ) -lt 12 ]; then
     echo "Multipass currently requires macOS 10.12 or newer"
     exit 1
 fi
