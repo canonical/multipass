1a94df08cd0054c489f2493cf52c5dc3ffde4da2
Ricardo Abreu <ricab@ricabhome.org>
2020-09-18 18:14:05 +0100

    [test] Fix failing tests

--- tests/qemu/test_dnsmasq_process_spec.cpp
+++ tests/qemu/test_dnsmasq_process_spec.cpp
@@ -32,7 +32,6 @@ struct TestDnsmasqProcessSpec : public Test
 {
     const QString data_dir{"/data"};
     const QString bridge_name{"bridgey"};
-    const QString pid_file_path{"/path/to/file.pid"};
     const std::string subnet{"1.2.3"};
     const QString conf_file_path{"/path/to/file.conf"};
 };
@@ -43,9 +42,9 @@ TEST_F(TestDnsmasqProcessSpec, default_arguments_correct)
 
     mpt::SetEnvScope e1("SNAP", "/something");
     mpt::SetEnvScope e2("SNAP_NAME", snap_name);
-    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, pid_file_path, subnet, conf_file_path);
+    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, subnet, conf_file_path);
     EXPECT_EQ(spec.arguments(),
-              QStringList({"--strict-order", "--bind-interfaces", "--pid-file=/path/to/file.pid", "--domain=multipass",
+              QStringList({"--strict-order", "--bind-interfaces", "--pid-file", "--domain=multipass",
                            "--local=/multipass/", "--except-interface=lo", "--interface=bridgey",
                            "--listen-address=1.2.3.1", "--dhcp-no-override", "--dhcp-authoritative",
                            "--dhcp-leasefile=/data/dnsmasq.leases", "--dhcp-hostsfile=/data/dnsmasq.hosts",
@@ -54,24 +53,23 @@ TEST_F(TestDnsmasqProcessSpec, default_arguments_correct)
 
 TEST_F(TestDnsmasqProcessSpec, apparmor_profile_has_correct_name)
 {
-    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, pid_file_path, subnet, conf_file_path);
+    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, subnet, conf_file_path);
 
     EXPECT_TRUE(spec.apparmor_profile().contains("profile multipass.dnsmasq"));
 }
 
 TEST_F(TestDnsmasqProcessSpec, apparmor_profile_permits_data_dirs)
 {
-    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, pid_file_path, subnet, conf_file_path);
+    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, subnet, conf_file_path);
 
     EXPECT_TRUE(spec.apparmor_profile().contains("/data/dnsmasq.leases rw,"));
     EXPECT_TRUE(spec.apparmor_profile().contains("/data/dnsmasq.hosts r,"));
-    EXPECT_TRUE(spec.apparmor_profile().contains("/path/to/file.pid w,"));
     EXPECT_TRUE(spec.apparmor_profile().contains("/path/to/file.conf r,"));
 }
 
 TEST_F(TestDnsmasqProcessSpec, apparmor_profile_identifier)
 {
-    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, pid_file_path, subnet, conf_file_path);
+    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, subnet, conf_file_path);
 
     EXPECT_EQ(spec.identifier(), "");
 }
@@ -83,7 +81,7 @@ TEST_F(TestDnsmasqProcessSpec, apparmor_profile_running_as_snap_correct)
 
     mpt::SetEnvScope e1("SNAP", snap_dir.path().toUtf8());
     mpt::SetEnvScope e2("SNAP_NAME", snap_name);
-    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, pid_file_path, subnet, conf_file_path);
+    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, subnet, conf_file_path);
 
     EXPECT_TRUE(spec.apparmor_profile().contains("signal (receive) peer=snap.multipass.multipassd"));
     EXPECT_TRUE(spec.apparmor_profile().contains(QString("%1/usr/sbin/dnsmasq ixr,").arg(snap_dir.path())));
@@ -99,7 +97,7 @@ TEST_F(TestDnsmasqProcessSpec, apparmor_profile_running_as_symlinked_snap_correc
 
     mpt::SetEnvScope e1("SNAP", link_dir.path().toUtf8());
     mpt::SetEnvScope e2("SNAP_NAME", snap_name);
-    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, pid_file_path, subnet, conf_file_path);
+    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, subnet, conf_file_path);
 
     EXPECT_TRUE(spec.apparmor_profile().contains(QString("%1/usr/sbin/dnsmasq ixr,").arg(snap_dir.path())));
 }
@@ -110,7 +108,7 @@ TEST_F(TestDnsmasqProcessSpec, apparmor_profile_not_running_as_snap_correct)
 
     mpt::UnsetEnvScope e("SNAP");
     mpt::SetEnvScope e2("SNAP_NAME", snap_name);
-    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, pid_file_path, subnet, conf_file_path);
+    mp::DNSMasqProcessSpec spec(data_dir, bridge_name, subnet, conf_file_path);
 
     EXPECT_TRUE(spec.apparmor_profile().contains("signal (receive) peer=unconfined"));
     EXPECT_TRUE(spec.apparmor_profile().contains(" /usr/sbin/dnsmasq ixr,")); // space wanted
