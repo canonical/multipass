4eea0c00c9213f6ff63d127e8cebdf8c42eb6af0
Michał Sawicz <michal@sawicz.net>
2020-10-12 19:56:43 +0200

    [qt] deal with QtNetwork deprecation
    Co-authored-by: Dimitris Kiziridis <ragouel@outlook.com>

--- src/network/local_socket_reply.cpp
+++ src/network/local_socket_reply.cpp
@@ -115,7 +115,11 @@ void mp::LocalSocketReply::abort()
     close();
 
     setError(OperationCanceledError, "Operation canceled");
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
+    emit errorOccurred(OperationCanceledError);
+#else
     emit error(OperationCanceledError);
+#endif
 
     setFinished(true);
     emit finished();
@@ -299,7 +303,11 @@ void mp::LocalSocketReply::parse_status(const QByteArray& status)
     {
         setError(QNetworkReply::ProtocolFailure, "Malformed HTTP response from server");
 
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
+        emit errorOccurred(QNetworkReply::ProtocolFailure);
+#else
         emit error(QNetworkReply::ProtocolFailure);
+#endif
 
         return;
     }
@@ -313,7 +321,11 @@ void mp::LocalSocketReply::parse_status(const QByteArray& status)
 
         setError(error_code, http_status_match.captured("message"));
 
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
+        emit errorOccurred(error_code);
+#else
         emit error(error_code);
+#endif
     }
 }
 
@@ -323,7 +335,11 @@ bool mp::LocalSocketReply::local_socket_write(const QByteArray& data)
     if (bytes_written < 0)
     {
         setError(QNetworkReply::InternalServerError, local_socket->errorString());
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
+        emit errorOccurred(QNetworkReply::InternalServerError);
+#else
         emit error(QNetworkReply::InternalServerError);
+#endif
 
         return false;
     }
