304233322ef3b8b776511e21b8e3b6db8e5baeb0
Chris Townsend <christopher.townsend@canonical.com>
2020-12-14 13:17:21 -0500

    [mocks] Add ability to mock platform free functions
    This commit also adds the is_remote_supported() and is_alias_supported() mocks.

---
+++ tests/mock_platform_functions.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "mock_platform_functions.h"
+
+namespace multipass
+{
+namespace platform
+{
+IMPL_MOCK_DEFAULT(1, is_remote_supported);
+IMPL_MOCK_DEFAULT(2, is_alias_supported);
+} // namespace platform
+} // namespace multipass
