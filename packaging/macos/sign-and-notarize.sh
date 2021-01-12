f1bdfada6f7db231279b0591867b9dcf17aebfef
Michał Sawicz <michal@sawicz.net>
2021-01-12 12:27:44 +0100

    [macos] avoid relying on `greadlink`

--- packaging/macos/sign-and-notarize.sh
+++ packaging/macos/sign-and-notarize.sh
@@ -146,7 +146,7 @@ function codesign_binaries {
             --sign "${SIGN_APP}"
 }
 
-SCRIPTDIR=$( dirname $( greadlink -f $0 ))
+SCRIPTDIR=$(perl -MCwd=realpath -e "print realpath '$0/..'")
 
 WORKDIR=$(mktemp -d)
 function clean_workdir
