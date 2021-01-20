36dc67feec4892540b87ae99e9f84f65172cbaaf
Chris Townsend <christopher.townsend@canonical.com>
2021-01-20 14:24:35 -0500

    [tests] Remove LXD image vault test that no longer applies

--- tests/lxd/test_lxd_image_vault.cpp
+++ tests/lxd/test_lxd_image_vault.cpp
@@ -187,22 +187,6 @@ TEST_F(LXDImageVault, throws_with_invalid_alias)
         Property(&std::runtime_error::what, StrEq(fmt::format("Unable to find an image matching \"{}\"", alias))));
 }
 
-TEST_F(LXDImageVault, throws_with_non_alias_queries)
-{
-    ON_CALL(*mock_network_access_manager.get(), createRequest(_, _, _)).WillByDefault([](auto...) {
-        return new mpt::MockLocalSocketReply(mpt::not_found_data, QNetworkReply::ContentNotFoundError);
-    });
-
-    mp::Query query{"", "", false, "", mp::Query::Type::HttpDownload};
-
-    mp::LXDVMImageVault image_vault{hosts,    &stub_url_downloader, mock_network_access_manager.get(),
-                                    base_url, cache_dir.path(),     mp::days{0}};
-
-    MP_EXPECT_THROW_THAT(image_vault.fetch_image(mp::FetchType::ImageOnly, query, stub_prepare, stub_monitor),
-                         std::runtime_error,
-                         Property(&std::runtime_error::what, StrEq("http and file based images are not supported")));
-}
-
 TEST_F(LXDImageVault, throws_with_invalid_remote)
 {
     ON_CALL(*mock_network_access_manager.get(), createRequest(_, _, _)).WillByDefault([](auto...) {
