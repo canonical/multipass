15bc3f57441227919e1670f8e87eea2ce1ec1f5b
Luis Peñaranda <luis.penaranda@canonical.com>
2021-01-29 12:11:39 -0300

    [cli] Handle  command.

--- src/client/cli/cmd/stop.cpp
+++ src/client/cli/cmd/stop.cpp
@@ -84,7 +84,8 @@ mp::ParseCode cmd::Stop::parse_args(mp::ArgParser* parser)
     QCommandLineOption time_option({"t", "time"}, "Time from now, in minutes, to delay shutdown of the instance",
                                    "time", "0");
     QCommandLineOption cancel_option({"c", "cancel"}, "Cancel a pending delayed shutdown");
-    parser->addOptions({all_option, time_option, cancel_option});
+    QCommandLineOption force_option({"f", "force"}, "Force switching the instance off");
+    parser->addOptions({all_option, time_option, cancel_option, force_option});
 
     auto status = parser->commandParse(this);
     if (status != ParseCode::Ok)
@@ -102,6 +103,12 @@ mp::ParseCode cmd::Stop::parse_args(mp::ArgParser* parser)
     if (parser->isSet(time_option) && parser->isSet(cancel_option))
     {
         cerr << "Cannot set \'time\' and \'cancel\' options at the same time\n";
+        return ParseCode::CommandLineError;
+    }
+
+    if (parser->isSet(force_option) && (parser->isSet(time_option) || parser->isSet(cancel_option)))
+    {
+        cerr << "Cannot set \'force\' along with \'time\' or \'cancel\' options at the same time\n";
         return ParseCode::CommandLineError;
     }
 
