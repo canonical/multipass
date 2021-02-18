1f1fa416725287261f3abff019512da1d7fb7a0e
Michał Sawicz <michal@sawicz.net>
2021-02-18 11:51:51 +0100

    [macos] refresh brew dependencies

--- 3rd-party/qemu-img-osx/qemu-img-fetcher.sh
+++ 3rd-party/qemu-img-osx/qemu-img-fetcher.sh
@@ -24,7 +24,7 @@ if [ ! -d "$OUT_DIR" ]; then
     mkdir "$OUT_DIR" || { echo >&2 "Unable to create destination directory '$OUT_DIR'. Aborting."; exit 1; }
 fi;
 
-DIST="sierra"
+DIST="mojave"
 
 BINDIR="${OUT_DIR}/bin"
 LIBDIR="${OUT_DIR}/lib"
@@ -55,9 +55,9 @@ mkdir -p "$LIBDIR"
 mkdir -p "$DOWNLOAD_DIR"
 
 # Get qemu-img binary (with shared lib requirements)
-get_brew_and_extract qemu-4.1.0
+get_brew_and_extract qemu-5.2.0
 
-cp "$DOWNLOAD_DIR/qemu/4.1.0/bin/qemu-img" "$BINDIR/"
+cp "$DOWNLOAD_DIR/qemu/5.2.0/bin/qemu-img" "$BINDIR/"
 
 
 
@@ -65,41 +65,41 @@ cp "$DOWNLOAD_DIR/qemu/4.1.0/bin/qemu-img" "$BINDIR/"
 # Learn this from "otool -L qemu-img"
 
 # glib - requres pcre
-get_brew_and_extract glib-2.62.0_1
+get_brew_and_extract glib-2.66.7
 
-cp "$DOWNLOAD_DIR/glib/2.62.0_1/lib/libgthread-2.0.0.dylib" "$LIBDIR/"
-cp "$DOWNLOAD_DIR/glib/2.62.0_1/lib/libglib-2.0.0.dylib"    "$LIBDIR/"
+cp "$DOWNLOAD_DIR/glib/2.66.7/lib/libgthread-2.0.0.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/glib/2.66.7/lib/libglib-2.0.0.dylib"    "$LIBDIR/"
 
 # pcre - required by glib (no other deps)
-get_brew_and_extract pcre-8.43
+get_brew_and_extract pcre-8.44
 
-cp "$DOWNLOAD_DIR/pcre/8.43/lib/libpcre.1.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/pcre/8.44/lib/libpcre.1.dylib" "$LIBDIR/"
 
 # gettext (no other deps)
-get_brew_and_extract gettext-0.20.1
+get_brew_and_extract gettext-0.21
 
-cp "$DOWNLOAD_DIR/gettext/0.20.1/lib/libintl.8.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/gettext/0.21/lib/libintl.8.dylib" "$LIBDIR/"
 
 # nettle (no other deps)
-get_brew_and_extract nettle-3.4.1
+get_brew_and_extract nettle-3.7
 
-cp "$DOWNLOAD_DIR/nettle/3.4.1/lib/libnettle.6.dylib"  "$LIBDIR/"
-cp "$DOWNLOAD_DIR/nettle/3.4.1/lib/libhogweed.4.dylib" "$LIBDIR/"  # needed by gnutls
+cp "$DOWNLOAD_DIR/nettle/3.7/lib/libnettle.8.dylib"  "$LIBDIR/"
+cp "$DOWNLOAD_DIR/nettle/3.7/lib/libhogweed.6.dylib" "$LIBDIR/"  # needed by gnutls
 
 # gnutls - requires p11-kit, libunistring, libtasn1, nettle (above), gmp, libintl (above), libidn2
-get_brew_and_extract gnutls-3.6.9
+get_brew_and_extract gnutls-3.6.15
 
-cp "$DOWNLOAD_DIR/gnutls/3.6.9/lib/libgnutls.30.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/gnutls/3.6.15/lib/libgnutls.30.dylib" "$LIBDIR/"
 
 # p11-kit - requires libffi
-get_brew_and_extract p11-kit-0.23.17
+get_brew_and_extract p11-kit-0.23.22
 
-cp "$DOWNLOAD_DIR/p11-kit/0.23.17/lib/libp11-kit.0.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/p11-kit/0.23.22/lib/libp11-kit.0.dylib" "$LIBDIR/"
 
 # libffi (no other deps)
-get_brew_and_extract libffi-3.2.1
+get_brew_and_extract libffi-3.3
 
-cp "$DOWNLOAD_DIR/libffi/3.2.1/lib/libffi.6.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/libffi/3.3/lib/libffi.7.dylib" "$LIBDIR/"
 
 # libunistring (no other deps)
 get_brew_and_extract libunistring-0.9.10
@@ -107,19 +107,19 @@ get_brew_and_extract libunistring-0.9.10
 cp "$DOWNLOAD_DIR/libunistring/0.9.10/lib/libunistring.2.dylib" "$LIBDIR/"
 
 # libtasn1 (no other deps)
-get_brew_and_extract libtasn1-4.14
+get_brew_and_extract libtasn1-4.16.0
 
-cp "$DOWNLOAD_DIR/libtasn1/4.14/lib/libtasn1.6.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/libtasn1/4.16.0/lib/libtasn1.6.dylib" "$LIBDIR/"
 
 # gmp (no other deps)
-get_brew_and_extract gmp-6.1.2_2
+get_brew_and_extract gmp-6.2.1
 
-cp "$DOWNLOAD_DIR/gmp/6.1.2_2/lib/libgmp.10.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/gmp/6.2.1/lib/libgmp.10.dylib" "$LIBDIR/"
 
 # libidn2 - requires libintl (above), libunistring (above)
-get_brew_and_extract libidn2-2.2.0_1
+get_brew_and_extract libidn2-2.3.0
 
-cp "$DOWNLOAD_DIR/libidn2/2.2.0_1/lib/libidn2.0.dylib" "$LIBDIR/"
+cp "$DOWNLOAD_DIR/libidn2/2.3.0/lib/libidn2.0.dylib" "$LIBDIR/"
 
 # add writable permission to allow library path rewriting
 chmod +w "$LIBDIR"/*.dylib
@@ -130,7 +130,7 @@ chmod +w "$BINDIR/qemu-img"
 install_name_tool -add_rpath "@executable_path/../lib/" "$BINDIR/qemu-img"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/glib/lib/libgthread-2.0.0.dylib" "@rpath/libgthread-2.0.0.dylib"   "$BINDIR/qemu-img"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/gnutls/lib/libgnutls.30.dylib"   "@rpath/libgnutls.30.dylib"       "$BINDIR/qemu-img"
-install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/nettle/lib/libnettle.6.dylib"    "@rpath/libnettle.6.dylib"        "$BINDIR/qemu-img"
+install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/nettle/lib/libnettle.8.dylib"    "@rpath/libnettle.8.dylib"        "$BINDIR/qemu-img"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/gettext/lib/libintl.8.dylib"     "@rpath/libintl.8.dylib"          "$BINDIR/qemu-img"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/glib/lib/libglib-2.0.0.dylib"    "@rpath/libglib-2.0.0.dylib"      "$BINDIR/qemu-img"
 
@@ -151,18 +151,18 @@ install_name_tool -add_rpath "@loader_path/" "$LIBDIR/libgnutls.30.dylib"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/p11-kit/lib/libp11-kit.0.dylib"  "@rpath/libp11-kit.0.dylib"         "$LIBDIR/libgnutls.30.dylib"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libunistring/lib/libunistring.2.dylib" "@rpath/libunistring.2.dylib" "$LIBDIR/libgnutls.30.dylib"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libtasn1/lib/libtasn1.6.dylib"   "@rpath/libtasn1.6.dylib"           "$LIBDIR/libgnutls.30.dylib"
-install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/nettle/lib/libnettle.6.dylib"    "@rpath/libnettle.6.dylib"          "$LIBDIR/libgnutls.30.dylib"
-install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/nettle/lib/libhogweed.4.dylib"   "@rpath/libhogweed.4.dylib"         "$LIBDIR/libgnutls.30.dylib"
+install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/nettle/lib/libnettle.8.dylib"    "@rpath/libnettle.8.dylib"          "$LIBDIR/libgnutls.30.dylib"
+install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/nettle/lib/libhogweed.6.dylib"   "@rpath/libhogweed.6.dylib"         "$LIBDIR/libgnutls.30.dylib"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/gmp/lib/libgmp.10.dylib"         "@rpath/libgmp.10.dylib"            "$LIBDIR/libgnutls.30.dylib"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/gettext/lib/libintl.8.dylib"     "@rpath/libintl.8.dylib"            "$LIBDIR/libgnutls.30.dylib"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libidn2/lib/libidn2.0.dylib"     "@rpath/libidn2.0.dylib"            "$LIBDIR/libgnutls.30.dylib"
 
-install_name_tool -add_rpath "@loader_path/" "$LIBDIR/libhogweed.4.dylib"
-install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/gmp/lib/libgmp.10.dylib"         "@rpath/libgmp.10.dylib"          "$LIBDIR/libhogweed.4.dylib"
-install_name_tool -change "@@HOMEBREW_CELLAR@@/nettle/3.4.1/lib/libnettle.6.dylib"    "@rpath/libnettle.6.dylib"      "$LIBDIR/libhogweed.4.dylib"
+install_name_tool -add_rpath "@loader_path/" "$LIBDIR/libhogweed.6.dylib"
+install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/gmp/lib/libgmp.10.dylib"         "@rpath/libgmp.10.dylib"          "$LIBDIR/libhogweed.6.dylib"
+install_name_tool -change "@@HOMEBREW_CELLAR@@/nettle/3.4.1/lib/libnettle.8.dylib"    "@rpath/libnettle.8.dylib"      "$LIBDIR/libhogweed.6.dylib"
 
 install_name_tool -add_rpath "@loader_path/" "$LIBDIR/libp11-kit.0.dylib"
-install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libffi/lib/libffi.6.dylib"       "@rpath/libffi.6.dylib"           "$LIBDIR/libp11-kit.0.dylib"
+install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libffi/lib/libffi.7.dylib"       "@rpath/libffi.7.dylib"           "$LIBDIR/libp11-kit.0.dylib"
 
 install_name_tool -add_rpath "@loader_path/" "$LIBDIR/libidn2.0.dylib"
 install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libunistring/lib/libunistring.2.dylib" "@rpath/libunistring.2.dylib" "$LIBDIR/libidn2.0.dylib"
