d09cb252e9c1deca6e113d6c259baeeaef302d1b
Luis Peñaranda <luis.penaranda@canonical.com>
2021-03-24 10:47:34 -0300

    [tests][cli] Add more checks for AliasDict.

--- tests/test_alias_dict.cpp
+++ tests/test_alias_dict.cpp
@@ -60,4 +60,47 @@ TEST_F(AliasDictionary, works_with_broken_file)
 
     ASSERT_TRUE(dict.empty());
 }
+
+typedef std::vector<std::pair<std::string, mp::AliasDefinition>> AliasesVector;
+
+struct WriteReadTeststuite : public AliasDictionary, public WithParamInterface<AliasesVector>
+{
+};
+
+void create_alias_file(const std::string& file_name,
+                       const std::vector<std::pair<std::string, mp::AliasDefinition>>& aliases)
+{
+    mp::AliasDict writer(file_name);
+
+    for (const auto& alias : aliases)
+        writer.add_alias(alias.first, alias.second);
+}
+
+TEST_P(WriteReadTeststuite, writes_and_reads_files)
+{
+    auto aliases_vector = GetParam();
+
+    mpt::TempDir temp_dir;
+    std::string fake_db_file{(temp_dir.path() + "/fake_config_file").toStdString()};
+
+    create_alias_file(fake_db_file, aliases_vector);
+
+    mp::AliasDict reader(fake_db_file);
+
+    for (const auto& alias : aliases_vector)
+    {
+        auto read_value = reader.get_alias(alias.first);
+        ASSERT_TRUE(read_value);
+        ASSERT_EQ((*read_value).instance, alias.second.instance);
+        ASSERT_EQ((*read_value).command, alias.second.command);
+        ASSERT_EQ((*read_value).arguments, alias.second.arguments);
+    }
+
+    ASSERT_EQ((size_t)std::distance(reader.cbegin(), reader.cend()), (size_t)aliases_vector.size());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    AliasDictionary, WriteReadTeststuite,
+    Values(AliasesVector{}, AliasesVector{{"w", {"fake", "w", {}}}}, AliasesVector{{"ipf", {"instance", "ip", {"a"}}}},
+           AliasesVector{{"lsp", {"primary", "ls", {}}}, {"llp", {"primary", "ls", {"-l", "-a"}}}}));
 } // namespace
