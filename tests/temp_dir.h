495b0e39b7d2c89bf556607f2ae7969ab7bcad21
Luis Peñaranda <luis.penaranda@canonical.com>
2021-03-25 15:29:07 -0300

    [tests] Add filePath() to TempDir.
    This mimics the function QTemporaryDir::filePath().

--- tests/temp_dir.h
+++ tests/temp_dir.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Canonical, Ltd.
+ * Copyright (C) 2017-2021 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,6 +32,11 @@ public:
     QString path() const
     {
         return the_path;
+    }
+
+    QString filePath(const QString& fileName)
+    {
+        return dir.filePath(fileName);
     }
 
 private:
