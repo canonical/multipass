8395628d185ecdce56b0e9b30f3416b5fe4ac60d
Ricardo Abreu <ricab@ricabhome.org>
2021-03-30 19:19:06 +0100

    [test] Condense matching of DBus argument map

--- tests/linux/test_backend_utils.cpp
+++ tests/linux/test_backend_utils.cpp
@@ -281,29 +281,20 @@ TEST(BackendUtils, bridge_creation_creates_connections)
     auto mock_interface = std::make_unique<MockDBusInterface>();
     EXPECT_CALL(*mock_interface, is_valid).WillOnce(Return(true));
 
-    auto arg_matcher = [](const QVariant& arg) {
-        if (auto outer_map = arg.value<QMap<QString, QVariantMap>>(); outer_map.size() != 2)
-            return false;
-        else if (auto outer_it = outer_map.find("connection"); outer_it == outer_map.end() || outer_it->size() != 2)
-            return false;
-        else if (auto inner_it = outer_it->find("id"); inner_it == outer_it->end())
-            return false;
-        else if (inner_it->toString() != "qtbr0")
-            return false;
-        else if (inner_it = outer_it->find("type"); inner_it == outer_it->end())
-            return false;
-        else if (inner_it->toString() != "bridge")
-            return false;
-        else if (outer_it = outer_map.find("bridge"); outer_it == outer_map.end() || outer_it->size() != 1)
-            return false;
-        else if (inner_it = outer_it->find("interface-name"); inner_it == outer_it->end())
-            return false;
-        else if (inner_it->toString() != "qtbr0")
-            return false;
-        else
-            return true;
-    };
-    EXPECT_CALL(*mock_interface, call(QDBus::Block, Eq("AddConnection"), Truly(arg_matcher)))
+    auto arg_matcher = Truly([](const QVariant& arg) {
+        QMap<QString, QVariantMap> outer_map;
+        QMap<QString, QVariantMap>::const_iterator outer_it;
+        QVariantMap::const_iterator inner_it;
+
+        return (outer_map = arg.value<QMap<QString, QVariantMap>>()).size() == 2 &&
+               (outer_it = outer_map.find("connection")) != outer_map.end() && outer_it->size() == 2 &&
+               (inner_it = outer_it->find("id")) != outer_it->end() && inner_it->toString() == "qtbr0" &&
+               (inner_it = outer_it->find("type")) != outer_it->end() && inner_it->toString() == "bridge" &&
+               (outer_it = outer_map.find("bridge")) != outer_map.end() && outer_it->size() == 1 &&
+               (inner_it = outer_it->find("interface-name")) != outer_it->end() && inner_it->toString() == "qtbr0";
+    });
+
+    EXPECT_CALL(*mock_interface, call(QDBus::Block, Eq("AddConnection"), arg_matcher))
         .WillOnce(Return(QDBusMessage{}.createReply(QVariant::fromValue(QDBusObjectPath{"/some/obj/path"}))));
 
     auto mock_bus = MockDBusConnection{};
