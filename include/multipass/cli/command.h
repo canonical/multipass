cdbc712bb1c5662ee4fc352a1c3522262b120715
Chris Townsend <christopher.townsend@canonical.com>
2021-04-14 10:04:16 -0400

    [cli] Remove printing out log messages wholesale
    Let each command handler decide how they want to print out log messages as printing
    wholesale can mess up output on certain commands such as launch.

--- include/multipass/cli/command.h
+++ include/multipass/cli/command.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Canonical, Ltd.
+ * Copyright (C) 2017-2021 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -80,8 +80,6 @@ protected:
 
         while (reader->Read(&reply))
         {
-            if (!reply.log_line().empty())
-                cerr << reply.log_line() << "\n";
             streaming_callback(reply);
         }
 
