b5c49ea33055b382805b622ee77dbd967ae38a60
Chris Townsend <christopher.townsend@canonical.com>
2021-04-16 09:52:21 -0400

    [platform] Add function to get Workflows URL override

--- src/platform/platform_linux.cpp
+++ src/platform/platform_linux.cpp
@@ -138,6 +138,11 @@ std::map<std::string, mp::NetworkInterfaceInfo> mp::platform::Platform::get_netw
     return detail::get_network_interfaces_from(sysfs);
 }
 
+QString mp::platform::Platform::get_workflows_url_override()
+{
+    return QString::fromUtf8(qgetenv("MULTIPASS_WORKFLOWS_URL"));
+}
+
 bool mp::platform::Platform::is_alias_supported(const std::string& alias, const std::string& remote)
 {
     return true;
