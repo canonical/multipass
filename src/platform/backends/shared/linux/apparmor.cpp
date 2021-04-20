819ed1c254a220c058d82b8b7c7206495bcdd939
Chris Townsend <christopher.townsend@canonical.com>
2021-04-20 15:51:02 -0400

    [apparmor] Trivial formatting fix for log messages

--- src/platform/backends/shared/linux/apparmor.cpp
+++ src/platform/backends/shared/linux/apparmor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2020 Canonical, Ltd.
+ * Copyright (C) 2019-2021 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -95,7 +95,7 @@ void mp::AppArmor::load_policy(const QByteArray& aa_policy) const
     process.closeWriteChannel();
     process.waitForFinished();
 
-    mpl::log(mpl::Level::trace, "daemon", fmt::format("Loading AppArmor policy: \n{}", aa_policy));
+    mpl::log(mpl::Level::trace, "daemon", fmt::format("Loading AppArmor policy:\n{}", aa_policy));
 
     if (process.exitCode() != 0)
     {
@@ -113,7 +113,7 @@ void mp::AppArmor::remove_policy(const QByteArray& aa_policy) const
     process.closeWriteChannel();
     process.waitForFinished();
 
-    mpl::log(mpl::Level::trace, "daemon", fmt::format("Removing AppArmor policy: \n{}", aa_policy));
+    mpl::log(mpl::Level::trace, "daemon", fmt::format("Removing AppArmor policy:\n{}", aa_policy));
 
     if (process.exitCode() != 0)
     {
