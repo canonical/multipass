5c1845e03e81e573db4fa373c1db395371da882f
Ricardo Abreu <ricardo.abreu@canonical.com>
2021-05-12 17:56:37 +0100

    [test] Return planted instance-json filename
    So that it can be used in the tests.

--- tests/test_daemon.cpp
+++ tests/test_daemon.cpp
@@ -1068,14 +1068,15 @@ std::vector<std::string> old_releases{"10.04",   "lucid",  "11.10",   "oneiric",
 INSTANTIATE_TEST_SUITE_P(DaemonRefuseRelease, RefuseBridging, Combine(Values("release", ""), ValuesIn(old_releases)));
 INSTANTIATE_TEST_SUITE_P(DaemonRefuseSnapcraft, RefuseBridging, Values(std::make_tuple("snapcraft", "core")));
 
-std::unique_ptr<mpt::TempDir> plant_instance_json(const std::string& contents) // unique_ptr bypasses missing move ctor
+std::pair<std::unique_ptr<mpt::TempDir>, QString> // unique_ptr bypasses missing move ctor
+plant_instance_json(const std::string& contents)
 {
     auto temp_dir = std::make_unique<mpt::TempDir>();
     QString filename(temp_dir->path() + "/multipassd-vm-instances.json");
 
     mpt::make_file_with_content(filename, contents);
 
-    return temp_dir;
+    return {std::move(temp_dir), filename};
 }
 
 constexpr auto ghost_template = R"(
@@ -1113,8 +1114,8 @@ TEST_F(Daemon, skips_over_instance_ghosts_in_db) // which will have been sometim
     auto ghost2 = fmt::format(ghost_template, "ghost2");
     auto valid1 = fmt::format(valid_template, id1, "56");
     auto valid2 = fmt::format(valid_template, id2, "78");
-    auto temp_dir = plant_instance_json(fmt::format("{{\n{},\n{},\n{},\n{}\n}}", std::move(ghost1), std::move(ghost2),
-                                                    std::move(valid1), std::move(valid2)));
+    const auto [temp_dir, filename] = plant_instance_json(fmt::format(
+        "{{\n{},\n{},\n{},\n{}\n}}", std::move(ghost1), std::move(ghost2), std::move(valid1), std::move(valid2)));
 
     config_builder.data_directory = temp_dir->path();
     auto mock_factory = use_a_mock_vm_factory();
@@ -1131,7 +1132,8 @@ TEST_F(Daemon, ctor_drops_removed_instances)
     const std::string stayed{"foo"}, gone{"fighters"};
     auto stayed_json = fmt::format(valid_template, stayed, "12");
     auto gone_json = fmt::format(valid_template, gone, "34");
-    auto temp_dir = plant_instance_json(fmt::format("{{\n{},\n{}\n}}", std::move(stayed_json), std::move(gone_json)));
+    const auto [temp_dir, filename] =
+        plant_instance_json(fmt::format("{{\n{},\n{}\n}}", std::move(stayed_json), std::move(gone_json)));
     config_builder.data_directory = temp_dir->path();
 
     auto mock_image_vault = std::make_unique<NiceMock<mpt::MockVMImageVault>>();
@@ -1198,7 +1200,7 @@ TEST_F(Daemon, prevents_repetition_of_loaded_mac_addresses)
     config_builder.vault = std::make_unique<NiceMock<mpt::MockVMImageVault>>();
 
     std::string repeated_mac{"52:54:00:bd:19:41"};
-    auto temp_dir = plant_instance_json(fake_json_contents(repeated_mac, {}));
+    const auto [temp_dir, filename] = plant_instance_json(fake_json_contents(repeated_mac, {}));
     config_builder.data_directory = temp_dir->path();
 
     auto mock_factory = use_a_mock_vm_factory();
@@ -1217,7 +1219,7 @@ TEST_F(Daemon, does_not_hold_on_to_repeated_mac_addresses_when_loading)
     std::string mac_addr("52:54:00:73:76:28");
     std::vector<mp::NetworkInterface> extra_interfaces{mp::NetworkInterface{"eth0", mac_addr, true}};
 
-    auto temp_dir = plant_instance_json(fake_json_contents(mac_addr, extra_interfaces));
+    const auto [temp_dir, filename] = plant_instance_json(fake_json_contents(mac_addr, extra_interfaces));
     config_builder.data_directory = temp_dir->path();
 
     auto mock_factory = use_a_mock_vm_factory();
@@ -1232,7 +1234,7 @@ TEST_F(Daemon, does_not_hold_on_to_macs_when_loading_fails)
     std::string mac1{"52:54:00:73:76:28"}, mac2{"52:54:00:bd:19:41"};
     std::vector<mp::NetworkInterface> extra_interfaces{mp::NetworkInterface{"eth0", mac2, true}};
 
-    auto temp_dir = plant_instance_json(fake_json_contents(mac1, extra_interfaces));
+    const auto [temp_dir, filename] = plant_instance_json(fake_json_contents(mac1, extra_interfaces));
     config_builder.data_directory = temp_dir->path();
 
     auto mock_image_vault = std::make_unique<NiceMock<mpt::MockVMImageVault>>();
