d40369f595174098a273f897f829cd7e8fd3a9d0
Chris Townsend <christopher.townsend@canonical.com>
2021-05-17 15:50:43 -0400

    [lxd_image_vault] Set DELETE wait timeout to 2 minutes

--- src/platform/backends/lxd/lxd_vm_image_vault.cpp
+++ src/platform/backends/lxd/lxd_vm_image_vault.cpp
@@ -304,7 +304,7 @@ void mp::LXDVMImageVault::remove(const std::string& name)
         auto task_reply = lxd_request(
             manager, "DELETE", QUrl(QString("%1/virtual-machines/%2").arg(base_url.toString()).arg(name.c_str())));
 
-        lxd_wait(manager, base_url, task_reply, 300000);
+        lxd_wait(manager, base_url, task_reply, 120000);
     }
     catch (const LXDNotFoundException&)
     {
