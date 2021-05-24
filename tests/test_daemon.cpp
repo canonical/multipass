10611f62721b1ba145dc92e85483c9054cd44ef6
Chris Townsend <christopher.townsend@canonical.com>
2021-05-24 13:29:16 -0400

    [tests] Adapt tests to use a bogus path for VMImage verification failure

--- tests/test_daemon.cpp
+++ tests/test_daemon.cpp
@@ -1148,7 +1148,9 @@ TEST_F(Daemon, ctor_drops_removed_instances)
     EXPECT_CALL(*mock_image_vault, fetch_image(_, Field(&mp::Query::name, stayed), _, _))
         .WillRepeatedly(DoDefault()); // returns an image that can be verified to exist for this instance
     EXPECT_CALL(*mock_image_vault, fetch_image(_, Field(&mp::Query::name, gone), _, _))
-        .WillOnce(Return(mp::VMImage{})); // an image that can't be verified to exist for this instance
+        .WillOnce(
+            Return(mp::VMImage{"/path/to/nowhere", "", "", "", "", "", "", {}})); // an image that can't be verified to
+                                                                                  // exist for this instance
     config_builder.vault = std::move(mock_image_vault);
 
     auto mock_factory = use_a_mock_vm_factory();
@@ -1250,7 +1252,9 @@ TEST_F(Daemon, does_not_hold_on_to_macs_when_loading_fails)
 
     auto mock_image_vault = std::make_unique<NiceMock<mpt::MockVMImageVault>>();
     EXPECT_CALL(*mock_image_vault, fetch_image)
-        .WillOnce(Return(mp::VMImage{})) // cause the Daemon's ctor to fail verifying that the img exists
+        .WillOnce(
+            Return(mp::VMImage{"/path/to/nowhere", "", "", "", "", "", "", {}})) // cause the Daemon's ctor to fail
+                                                                                 // verifying that the img exists
         .WillRepeatedly(DoDefault());
     config_builder.vault = std::move(mock_image_vault);
 
