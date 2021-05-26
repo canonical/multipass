84486b7379e9c05d0d1406491d08713694dff7e3
Saad Ur Rahman <saad.ur.rahman@gmail.com>
2021-05-26 12:05:57 -0400

    [cli] <suspend> tightened up.

--- tests/test_cli_client.cpp
+++ tests/test_cli_client.cpp
@@ -1565,20 +1565,22 @@ TEST_F(Client, suspend_cmd_disabled_petenv)
 {
     const auto custom_petenv = "";
     EXPECT_CALL(mock_settings, get(Eq(mp::petenv_key))).WillRepeatedly(Return(custom_petenv));
+    EXPECT_CALL(mock_daemon, suspend(_, _, _));
 
-    EXPECT_THAT(send_command({"suspend"}), Eq(mp::ReturnCode::CommandFail));
+    EXPECT_THAT(send_command({"suspend"}), Eq(mp::ReturnCode::CommandLineError));
     EXPECT_THAT(send_command({"suspend", "-h"}), Eq(mp::ReturnCode::Ok));
+    EXPECT_THAT(send_command({"suspend", "--all"}), Eq(mp::ReturnCode::Ok));
 }
 
 TEST_F(Client, suspend_cmd_disabled_petenv_with_instance)
 {
     const auto custom_petenv = "";
     EXPECT_CALL(mock_settings, get(Eq(mp::petenv_key))).WillRepeatedly(Return(custom_petenv));
-    const auto petenv_matcher = make_instance_in_repeated_field_matcher<mp::SuspendRequest, 1>("foo");
-    EXPECT_CALL(mock_daemon, suspend(_, petenv_matcher, _));
+    EXPECT_CALL(mock_daemon, suspend(_, _, _)).Times(2);
 
     EXPECT_THAT(send_command({"suspend", "foo"}), Eq(mp::ReturnCode::Ok));
     EXPECT_THAT(send_command({"suspend", "-h"}), Eq(mp::ReturnCode::Ok));
+    EXPECT_THAT(send_command({"suspend", "--all"}), Eq(mp::ReturnCode::Ok));
 }
 
 // restart cli tests
