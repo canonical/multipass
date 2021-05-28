2bed851b58470ca3cff88f1a90990f6d08102122
Luis Peñaranda <luis.penaranda@canonical.com>
2021-05-28 15:56:13 -0300

    [qemu] Implement forced stop.

--- src/platform/backends/qemu/qemu_virtual_machine.h
+++ src/platform/backends/qemu/qemu_virtual_machine.h
@@ -50,7 +50,7 @@ public:
     ~QemuVirtualMachine();
 
     void start() override;
-    void shutdown() override;
+    void shutdown(bool force = false) override;
     void suspend() override;
     State current_state() override;
     int ssh_port() override;
