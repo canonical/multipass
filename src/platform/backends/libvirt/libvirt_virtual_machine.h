192820e9fd8d17bf069f232109b62c9bafe105c9
Luis Peñaranda <luis.penaranda@canonical.com>
2021-05-28 15:57:51 -0300

    [libvirt] Implement forced stop.

--- src/platform/backends/libvirt/libvirt_virtual_machine.h
+++ src/platform/backends/libvirt/libvirt_virtual_machine.h
@@ -44,7 +44,7 @@ public:
     ~LibVirtVirtualMachine();
 
     void start() override;
-    void shutdown() override;
+    void shutdown(bool force = false) override;
     void suspend() override;
     State current_state() override;
     int ssh_port() override;
