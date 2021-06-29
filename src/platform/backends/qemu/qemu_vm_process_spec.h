6b63f716d4c7cc303ec352c3a20e8291aac61129
Chris Townsend <christopher.townsend@canonical.com>
2021-06-29 10:18:45 -0400

    [backend/qemu] Support passing in of platform-specific QEMU arguments

--- src/platform/backends/qemu/qemu_vm_process_spec.h
+++ src/platform/backends/qemu/qemu_vm_process_spec.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2020 Canonical, Ltd.
+ * Copyright (C) 2019-2021 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,8 +39,8 @@ public:
 
     static QString default_machine_type();
 
-    explicit QemuVMProcessSpec(const VirtualMachineDescription& desc, const QString& qemu_netdev,
-                               const multipass::optional<ResumeData>& resume_data);
+    explicit QemuVMProcessSpec(const VirtualMachineDescription& desc, const QStringList& qemu_platform_args,
+                               const QString& qemu_netdev, const multipass::optional<ResumeData>& resume_data);
 
     QStringList arguments() const override;
 
@@ -49,6 +49,7 @@ public:
 
 private:
     const VirtualMachineDescription desc;
+    const QStringList qemu_platform_args;
     const QString qemu_netdev;
     const multipass::optional<ResumeData> resume_data;
 };
