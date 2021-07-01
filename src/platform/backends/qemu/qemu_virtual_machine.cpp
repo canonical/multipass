canonical/multipass

commit 8b1a4404a34eec9c011d87c7a392a29122e04fc1

[backend/qemu] Fix logic error of when to actually suspend qemu
This was exposed when mocking the qemu process.

src/platform/backends/qemu/qemu_virtual_machine.cpp
@@ -313,14 +313,13 @@ void mp::QemuVirtualMachine::suspend()
 {
     if ((state == State::running || state == State::delayed_shutdown) && vm_process->running())
     {
-        vm_process->write(hmc_to_qmp_json("savevm " + QString::fromStdString(suspend_tag)));
-
         if (update_shutdown_status)
         {
             state = State::suspending;
             update_state();
-
             update_shutdown_status = false;
+
+            vm_process->write(hmc_to_qmp_json("savevm " + QString::fromStdString(suspend_tag)));
             vm_process->wait_for_finished();
             vm_process.reset(nullptr);
         }

commit 62d5c83fbf6188945439ea9c1bc3587e52b66300

[backend/qemu] Fix regression with suspend logic
This was caused by 8b1a4404a.

Fixes #2152

src/platform/backends/qemu/qemu_virtual_machine.cpp
@@ -228,8 +228,6 @@ mp::QemuVirtualMachine::~QemuVirtualMachine()
         {
             shutdown();
         }
-
-        vm_process->wait_for_finished();
     }
 
     remove_tap_device(QString::fromStdString(tap_device_name));
@@ -318,11 +316,11 @@ void mp::QemuVirtualMachine::suspend()
             state = State::suspending;
             update_state();
             update_shutdown_status = false;
-
-            vm_process->write(hmc_to_qmp_json("savevm " + QString::fromStdString(suspend_tag)));
-            vm_process->wait_for_finished();
-            vm_process.reset(nullptr);
         }
+
+        vm_process->write(hmc_to_qmp_json("savevm " + QString::fromStdString(suspend_tag)));
+        vm_process->wait_for_finished();
+        vm_process.reset(nullptr);
     }
     else if (state == State::off || state == State::suspended)
     {