c920125297b8f16d55ab8620771f8377d37dce16
Michał Sawicz <michal.sawicz@canonical.com>
2021-07-02 17:31:55 +0200

    Merge #2156
    2156: [launch] add trailing space to bridge confirmation a=Saviq r=ricab

--- src/client/cli/cmd/launch.cpp
+++ src/client/cli/cmd/launch.cpp
@@ -525,9 +525,9 @@ auto cmd::Launch::ask_metrics_permission(const mp::LaunchReply& reply) -> OptInS
 bool cmd::Launch::ask_bridge_permission(multipass::LaunchReply& reply)
 {
     static constexpr auto plural = "Multipass needs to create {} to connect to {}.\nThis will temporarily disrupt "
-                                   "connectivity on those interfaces.\n\nDo you want to continue (yes/no)?";
+                                   "connectivity on those interfaces.\n\nDo you want to continue (yes/no)? ";
     static constexpr auto singular = "Multipass needs to create a {} to connect to {}.\nThis will temporarily disrupt "
-                                     "connectivity on that interface.\n\nDo you want to continue (yes/no)?";
+                                     "connectivity on that interface.\n\nDo you want to continue (yes/no)? ";
     static constexpr auto nodes = on_windows() ? "switches" : "bridges";
     static constexpr auto node = on_windows() ? "switch" : "bridge";
 
