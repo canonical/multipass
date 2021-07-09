09d336291cb7ce2d077ffc52574e60cbb17f7793
Ricardo Abreu <ricardo.abreu@canonical.com>
2021-07-09 17:11:36 +0100

    [daemon] Validate image as part of create args

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -468,8 +468,11 @@ void validate_image(const mp::LaunchRequest* request, const mp::VMImageVault& va
     }
 }
 
-auto validate_create_arguments(const mp::LaunchRequest* request, const mp::VirtualMachineFactory& factory)
+auto validate_create_arguments(const mp::LaunchRequest* request, const mp::DaemonConfig* config)
 {
+    assert(config && config->factory && config->workflow_provider && config->vault && "null ptr somewhere...");
+    validate_image(request, *config->vault, *config->workflow_provider);
+
     static const auto min_mem = try_mem_size(mp::min_memory_size);
     static const auto min_disk = try_mem_size(mp::min_disk_size);
     assert(min_mem && min_disk);
@@ -507,7 +510,7 @@ auto validate_create_arguments(const mp::LaunchRequest* request, const mp::Virtu
         option_errors.add_error_codes(mp::LaunchError::INVALID_HOSTNAME);
 
     std::vector<std::string> nets_need_bridging;
-    auto extra_interfaces = validate_extra_interfaces(request, factory, nets_need_bridging, option_errors);
+    auto extra_interfaces = validate_extra_interfaces(request, *config->factory, nets_need_bridging, option_errors);
 
     struct CheckedArguments
     {
@@ -2176,8 +2179,7 @@ std::string mp::Daemon::check_instance_exists(const std::string& instance_name) 
 void mp::Daemon::create_vm(const CreateRequest* request, grpc::ServerWriter<CreateReply>* server,
                            std::promise<grpc::Status>* status_promise, bool start)
 {
-    validate_image(request, *config->vault, *config->workflow_provider);
-    auto checked_args = validate_create_arguments(request, *config->factory);
+    auto checked_args = validate_create_arguments(request, config.get());
 
     if (!checked_args.option_errors.error_codes().empty())
     {
