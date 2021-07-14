919c23cef0fcb8a5ca665f0559f80c9fad95317b
Chris Townsend <christopher.townsend@canonical.com>
2021-07-14 09:31:44 -0400

    [build/mac] Various fixes for including QEMU in the package

--- packaging/macos/fixup-qemu-and-deps.sh
+++ packaging/macos/fixup-qemu-and-deps.sh
@@ -6,3 +6,6 @@ CMAKE_BINARY_DIR=$1
 
 dylibbundler -of -x ${CMAKE_BINARY_DIR}/bin/qemu-system-* -b -d ${CMAKE_BINARY_DIR}/lib/ -p @executable_path/../lib -i /usr/lib
 dylibbundler -of -x ${CMAKE_BINARY_DIR}/bin/qemu-img -b -d ${CMAKE_BINARY_DIR}/lib/ -p @executable_path/../lib -i /usr/lib
+
+# Resign the binary since we modify it above
+codesign --entitlements ${CMAKE_BINARY_DIR}/3rd-party/qemu/accel/hvf/entitlements.plist --force -s - ${CMAKE_BINARY_DIR}/bin/qemu-system-*
