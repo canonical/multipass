73912c900be5cf6f7778452c14c9bc897d557394
Luis Peñaranda <luis.penaranda@canonical.com>
2021-09-06 09:05:48 -0300

    [json_writer] Use file_ops to ease testing.

--- src/utils/json_writer.cpp
+++ src/utils/json_writer.cpp
@@ -17,6 +17,7 @@
  *
  */
 
+#include <multipass/file_ops.h>
 #include <multipass/json_writer.h>
 
 #include <QFile>
@@ -29,6 +30,6 @@ void mp::write_json(const QJsonObject& root, QString file_name)
     QJsonDocument doc{root};
     auto raw_json = doc.toJson();
     QFile db_file{file_name};
-    db_file.open(QIODevice::ReadWrite | QIODevice::Truncate);
-    db_file.write(raw_json);
+    MP_FILEOPS.open(db_file, QIODevice::ReadWrite | QIODevice::Truncate);
+    MP_FILEOPS.write(db_file, raw_json);
 }
