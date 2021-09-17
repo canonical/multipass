b43ec0277cad88856332077cb8dfa3f11dc058a9
Chris Townsend <christopher.townsend@canonical.com>
2021-09-17 11:05:55 -0600

    [packaging/qemu] Drop signing qemu in the fixup script as this is done elsewhere

--- packaging/macos/fixup-qemu-and-deps.sh
+++ packaging/macos/fixup-qemu-and-deps.sh
@@ -12,6 +12,3 @@ fi
 if ! otool -L "${CMAKE_BINARY_DIR}/bin/qemu-img" | grep -q "${EXECUTABLE_PATH}"; then
     dylibbundler -of -x ${CMAKE_BINARY_DIR}/bin/qemu-img -b -d ${CMAKE_BINARY_DIR}/lib/ -p @executable_path/../lib -i /usr/lib
 fi
-
-# Resign the binary since we modify it above
-codesign --entitlements ${CMAKE_BINARY_DIR}/3rd-party/qemu/accel/hvf/entitlements.plist --force -s - ${CMAKE_BINARY_DIR}/bin/qemu-system-*
