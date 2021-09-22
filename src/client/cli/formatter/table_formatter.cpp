e90df3b480646e8d7084f5eea0cc2a3553dcb697
Chris Townsend <christopher.townsend@canonical.com>
2021-09-22 11:17:03 -0400

    [fmt] Update calls to format_to to use the buffer iterator
    This is due to the deprecation of the memory buffer overload.

--- src/client/cli/formatter/table_formatter.cpp
+++ src/client/cli/formatter/table_formatter.cpp
@@ -42,44 +42,50 @@ std::string mp::TableFormatter::format(const InfoReply& reply) const
 
     for (const auto& info : format::sorted(reply.info()))
     {
-        fmt::format_to(buf, "{:<16}{}\n", "Name:", info.name());
-        fmt::format_to(buf, "{:<16}{}\n", "State:", mp::format::status_string_for(info.instance_status()));
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n", "Name:", info.name());
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n",
+                       "State:", mp::format::status_string_for(info.instance_status()));
 
         int ipv4_size = info.ipv4_size();
-        fmt::format_to(buf, "{:<16}{}\n", "IPv4:", ipv4_size ? info.ipv4(0) : "--");
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n", "IPv4:", ipv4_size ? info.ipv4(0) : "--");
 
         for (int i = 1; i < ipv4_size; ++i)
-            fmt::format_to(buf, "{:<16}{}\n", "", info.ipv4(i));
+            fmt::format_to(std::back_inserter(buf), "{:<16}{}\n", "", info.ipv4(i));
 
         if (int ipv6_size = info.ipv6_size())
         {
-            fmt::format_to(buf, "{:<16}{}\n", "IPv6:", info.ipv6(0));
+            fmt::format_to(std::back_inserter(buf), "{:<16}{}\n", "IPv6:", info.ipv6(0));
 
             for (int i = 1; i < ipv6_size; ++i)
-                fmt::format_to(buf, "{:<16}{}\n", "", info.ipv6(i));
+                fmt::format_to(std::back_inserter(buf), "{:<16}{}\n", "", info.ipv6(i));
         }
 
-        fmt::format_to(buf, "{:<16}{}\n", "Release:", info.current_release().empty() ? "--" : info.current_release());
-        fmt::format_to(buf, "{:<16}", "Image hash:");
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n",
+                       "Release:", info.current_release().empty() ? "--" : info.current_release());
+        fmt::format_to(std::back_inserter(buf), "{:<16}", "Image hash:");
         if (info.id().empty())
-            fmt::format_to(buf, "{}\n", "Not Available");
+            fmt::format_to(std::back_inserter(buf), "{}\n", "Not Available");
         else
-            fmt::format_to(buf, "{}{}\n", info.id().substr(0, 12),
+            fmt::format_to(std::back_inserter(buf), "{}{}\n", info.id().substr(0, 12),
                            !info.image_release().empty() ? fmt::format(" (Ubuntu {})", info.image_release()) : "");
-        fmt::format_to(buf, "{:<16}{}\n", "CPU(s):", info.cpu_count().empty() ? "--" : info.cpu_count());
-        fmt::format_to(buf, "{:<16}{}\n", "Load:", info.load().empty() ? "--" : info.load());
-        fmt::format_to(buf, "{:<16}{}\n", "Disk usage:", to_usage(info.disk_usage(), info.disk_total()));
-        fmt::format_to(buf, "{:<16}{}\n", "Memory usage:", to_usage(info.memory_usage(), info.memory_total()));
+
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n",
+                       "CPU(s):", info.cpu_count().empty() ? "--" : info.cpu_count());
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n", "Load:", info.load().empty() ? "--" : info.load());
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n",
+                       "Disk usage:", to_usage(info.disk_usage(), info.disk_total()));
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}\n",
+                       "Memory usage:", to_usage(info.memory_usage(), info.memory_total()));
 
         auto mount_paths = info.mount_info().mount_paths();
-        fmt::format_to(buf, "{:<16}{}", "Mounts:", mount_paths.empty() ? "--\n" : "");
+        fmt::format_to(std::back_inserter(buf), "{:<16}{}", "Mounts:", mount_paths.empty() ? "--\n" : "");
 
         for (auto mount = mount_paths.cbegin(); mount != mount_paths.cend(); ++mount)
         {
             if (mount != mount_paths.cbegin())
-                fmt::format_to(buf, "{:<16}", "");
-            fmt::format_to(buf, "{:{}} => {}\n", mount->source_path(), info.mount_info().longest_path_len(),
-                           mount->target_path());
+                fmt::format_to(std::back_inserter(buf), "{:<16}", "");
+            fmt::format_to(std::back_inserter(buf), "{:{}} => {}\n", mount->source_path(),
+                           info.mount_info().longest_path_len(), mount->target_path());
 
             auto mount_maps = mount->mount_maps();
             auto uid_mappings_size = mount_maps.uid_mappings_size();
@@ -90,7 +96,7 @@ std::string mp::TableFormatter::format(const InfoReply& reply) const
                 auto host_uid = uid_map_pair.host_id();
                 auto instance_uid = uid_map_pair.instance_id();
 
-                fmt::format_to(buf, "{:>{}}{}:{}{}", (i == 0) ? "UID map: " : "", (i == 0) ? 29 : 0,
+                fmt::format_to(std::back_inserter(buf), "{:>{}}{}:{}{}", (i == 0) ? "UID map: " : "", (i == 0) ? 29 : 0,
                                std::to_string(host_uid),
                                (instance_uid == mp::default_id) ? "default" : std::to_string(instance_uid),
                                (i == uid_mappings_size - 1) ? "\n" : ", ");
@@ -102,7 +108,7 @@ std::string mp::TableFormatter::format(const InfoReply& reply) const
                 auto host_gid = gid_mapping->host_id();
                 auto instance_gid = gid_mapping->instance_id();
 
-                fmt::format_to(buf, "{:>{}}{}:{}{}{}",
+                fmt::format_to(std::back_inserter(buf), "{:>{}}{}:{}{}{}",
                                (gid_mapping == mount_maps.gid_mappings().cbegin()) ? "GID map: " : "",
                                (gid_mapping == mount_maps.gid_mappings().cbegin()) ? 29 : 0, std::to_string(host_gid),
                                (instance_gid == mp::default_id) ? "default" : std::to_string(instance_gid),
@@ -111,7 +117,7 @@ std::string mp::TableFormatter::format(const InfoReply& reply) const
             }
         }
 
-        fmt::format_to(buf, "\n");
+        fmt::format_to(std::back_inserter(buf), "\n");
     }
 
     auto output = fmt::to_string(buf);
@@ -138,14 +144,14 @@ std::string mp::TableFormatter::format(const ListReply& reply) const
     const std::string::size_type ip_column_width = 17;
 
     const auto row_format = "{:<{}}{:<{}}{:<{}}{:<}\n";
-    fmt::format_to(buf, row_format, "Name", name_column_width, "State", state_column_width, "IPv4", ip_column_width,
-                   "Image");
+    fmt::format_to(std::back_inserter(buf), row_format, "Name", name_column_width, "State", state_column_width, "IPv4",
+                   ip_column_width, "Image");
 
     for (const auto& instance : format::sorted(reply.instances()))
     {
         int ipv4_size = instance.ipv4_size();
 
-        fmt::format_to(buf, row_format, instance.name(), name_column_width,
+        fmt::format_to(std::back_inserter(buf), row_format, instance.name(), name_column_width,
                        mp::format::status_string_for(instance.instance_status()), state_column_width,
                        ipv4_size ? instance.ipv4(0) : "--", ip_column_width,
                        instance.current_release().empty() ? "Not Available"
@@ -153,8 +159,8 @@ std::string mp::TableFormatter::format(const ListReply& reply) const
 
         for (int i = 1; i < ipv4_size; ++i)
         {
-            fmt::format_to(buf, row_format, "", name_column_width, "", state_column_width, instance.ipv4(i),
-                           instance.ipv4(i).size(), "");
+            fmt::format_to(std::back_inserter(buf), row_format, "", name_column_width, "", state_column_width,
+                           instance.ipv4(i), instance.ipv4(i).size(), "");
         }
     }
 
@@ -179,12 +185,13 @@ std::string mp::TableFormatter::format(const NetworksReply& reply) const
         5);
 
     const auto row_format = "{:<{}}{:<{}}{:<}\n";
-    fmt::format_to(buf, row_format, "Name", name_column_width, "Type", type_column_width, "Description");
+    fmt::format_to(std::back_inserter(buf), row_format, "Name", name_column_width, "Type", type_column_width,
+                   "Description");
 
     for (const auto& interface : format::sorted(reply.interfaces()))
     {
-        fmt::format_to(buf, row_format, interface.name(), name_column_width, interface.type(), type_column_width,
-                       interface.description());
+        fmt::format_to(std::back_inserter(buf), row_format, interface.name(), name_column_width, interface.type(),
+                       type_column_width, interface.description());
     }
 
     return fmt::to_string(buf);
@@ -197,7 +204,7 @@ std::string mp::TableFormatter::format(const FindReply& reply) const
     if (reply.images_info().empty())
         return "No images found.\n";
 
-    fmt::format_to(buf, "{:<28}{:<18}{:<17}{:<}\n", "Image", "Aliases", "Version", "Description");
+    fmt::format_to(std::back_inserter(buf), "{:<28}{:<18}{:<17}{:<}\n", "Image", "Aliases", "Version", "Description");
 
     for (const auto& image : reply.images_info())
     {
@@ -205,7 +212,7 @@ std::string mp::TableFormatter::format(const FindReply& reply) const
 
         mp::format::filter_aliases(aliases);
 
-        fmt::format_to(buf, "{:<28}{:<18}{:<17}{:<}\n", mp::format::image_string_for(aliases[0]),
+        fmt::format_to(std::back_inserter(buf), "{:<28}{:<18}{:<17}{:<}\n", mp::format::image_string_for(aliases[0]),
                        fmt::format("{}", fmt::join(aliases.cbegin() + 1, aliases.cend(), ",")), image.version(),
                        fmt::format("{}{}", image.os().empty() ? "" : image.os() + " ", image.release()));
     }
@@ -216,15 +223,15 @@ std::string mp::TableFormatter::format(const FindReply& reply) const
 std::string mp::TableFormatter::format(const VersionReply& reply, const std::string& client_version) const
 {
     fmt::memory_buffer buf;
-    fmt::format_to(buf, "{:<12}{}\n", "multipass", client_version);
+    fmt::format_to(std::back_inserter(buf), "{:<12}{}\n", "multipass", client_version);
 
     if (!reply.version().empty())
     {
-        fmt::format_to(buf, "{:<12}{}\n", "multipassd", reply.version());
+        fmt::format_to(std::back_inserter(buf), "{:<12}{}\n", "multipassd", reply.version());
 
         if (mp::cmd::update_available(reply.update_info()))
         {
-            fmt::format_to(buf, "{}", mp::cmd::update_notice(reply.update_info()));
+            fmt::format_to(std::back_inserter(buf), "{}", mp::cmd::update_notice(reply.update_info()));
         }
     }
     return fmt::to_string(buf);
@@ -246,16 +253,16 @@ std::string mp::TableFormatter::format(const mp::AliasDict& aliases) const
 
     const auto row_format = "{:<{}}{:<{}}{:<{}}{:<}\n";
 
-    fmt::format_to(buf, row_format, "Alias", alias_width, "Instance", instance_width, "Command", command_width,
-                   "Working directory");
+    fmt::format_to(std::back_inserter(buf), row_format, "Alias", alias_width, "Instance", instance_width, "Command",
+                   command_width, "Working directory");
 
     for (const auto& elt : sort_dict(aliases))
     {
         const auto& name = elt.first;
         const auto& def = elt.second;
 
-        fmt::format_to(buf, row_format, name, alias_width, def.instance, instance_width, def.command, command_width,
-                       def.working_directory);
+        fmt::format_to(std::back_inserter(buf), row_format, name, alias_width, def.instance, instance_width,
+                       def.command, command_width, def.working_directory);
     }
 
     return fmt::to_string(buf);
