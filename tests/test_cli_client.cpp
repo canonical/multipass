2072944b47d813b334beb7917a042bb94be32371
Luis Peñaranda <luis.penaranda@canonical.com>
2021-10-20 15:34:08 -0300

    [tests] Expect correct warning message from CLI.

--- tests/test_cli_client.cpp
+++ tests/test_cli_client.cpp
@@ -2753,7 +2753,7 @@ TEST_F(ClientAlias, unalias_succeeds_even_if_script_cannot_be_removed)
 
     std::stringstream cerr_stream;
     EXPECT_EQ(send_command({"unalias", "another_alias"}, trash_stream, cerr_stream), mp::ReturnCode::Ok);
-    EXPECT_THAT(cerr_stream.str(), HasSubstr("Warning: bbb for 'another_alias'"));
+    EXPECT_THAT(cerr_stream.str(), Eq("Warning when removing alias script for 'another_alias': bbb\n"));
 
     std::stringstream cout_stream;
     send_command({"aliases", "--format=csv"}, cout_stream);
