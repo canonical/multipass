d144466e111cc4839a06e53d9693818452841082
Luis Peñaranda <luis.penaranda@canonical.com>
2021-10-21 10:03:42 -0300

    [tests] Remove platform call from FakeAliasConfig.

--- tests/fake_alias_config.h
+++ tests/fake_alias_config.h
@@ -20,7 +20,6 @@
 
 #include <multipass/cli/alias_dict.h>
 #include <multipass/constants.h>
-#include <multipass/platform.h>
 
 #include "mock_standard_paths.h"
 #include "stub_terminal.h"
@@ -58,12 +57,6 @@ struct FakeAliasConfig
 
         for (const auto& alias : aliases)
             writer.add_alias(alias.first, alias.second);
-    }
-
-    void create_fake_alias_scripts(const std::vector<std::pair<std::string, mp::AliasDefinition>>& aliases)
-    {
-        for (const auto& alias : aliases)
-            MP_PLATFORM.create_alias_script(alias.first, alias.second);
     }
 
     mpt::TempDir fake_alias_dir;
