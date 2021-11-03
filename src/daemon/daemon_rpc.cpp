549ae874e9c9f66d99a6c03ed80da7d2d58c9c06
Chris Townsend <christopher.townsend@canonical.com>
2021-11-03 13:18:39 -0400

    [daemon/rpc] Add client certificate if provided passphrase is validated

--- src/daemon/daemon_rpc.cpp
+++ src/daemon/daemon_rpc.cpp
@@ -241,8 +241,15 @@ grpc::Status mp::DaemonRpc::get(grpc::ServerContext* context, const GetRequest* 
 grpc::Status mp::DaemonRpc::authenticate(grpc::ServerContext* context, const AuthenticateRequest* request,
                                          grpc::ServerWriter<AuthenticateReply>* response)
 {
-    return emit_signal_and_wait_for_result(
+    auto status = emit_signal_and_wait_for_result(
         std::bind(&DaemonRpc::on_authenticate, this, request, response, std::placeholders::_1));
+
+    if (status.ok())
+    {
+        client_cert_store->add_cert(client_cert_from(context));
+    }
+
+    return status;
 }
 
 template <typename OperationSignal>
