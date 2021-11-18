9980d57d83d027ea346389ab336acefa05403af6
Chris Townsend <christopher.townsend@canonical.com>
2021-11-18 15:23:33 -0500

    [tests] Remove client timeout tests in main client test file

--- tests/client/test_cli_client.cpp
+++ tests/client/test_cli_client.cpp
@@ -22,20 +22,16 @@
 #include <tests/mock_settings.h>
 #include <tests/mock_standard_paths.h>
 #include <tests/mock_stdcin.h>
-#include <tests/mock_utils.h>
 #include <tests/path.h>
 
 #include <multipass/constants.h>
 #include <multipass/exceptions/settings_exceptions.h>
 
 #include <QKeySequence>
+#include <QTemporaryDir>
 #include <QTemporaryFile>
-#include <QtCore/QTemporaryDir>
-#include <QtGlobal>
 
-#include <chrono>
 #include <initializer_list>
-#include <thread>
 #include <utility>
 
 namespace mp = multipass;
@@ -2259,101 +2255,6 @@ TEST_F(Client, help_cmd_launch_same_launch_cmd_help)
     EXPECT_THAT(help_cmd_launch.str(), Ne(""));
     EXPECT_THAT(help_cmd_launch.str(), Eq(launch_cmd_help.str()));
 }
-
-const std::vector<std::string> timeout_commands{"launch", "start", "restart", "shell"};
-const std::vector<std::string> valid_timeouts{"120", "1234567"};
-const std::vector<std::string> invalid_timeouts{"-1", "0", "a", "3min", "15.51", ""};
-
-struct TimeoutCorrectSuite : Client, WithParamInterface<std::tuple<std::string, std::string>>
-{
-};
-
-TEST_P(TimeoutCorrectSuite, cmds_with_timeout_ok)
-{
-    const auto& [command, timeout] = GetParam();
-
-    EXPECT_CALL(mock_daemon, launch).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, start).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, restart).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, ssh_info).Times(AtMost(1));
-    EXPECT_THAT(send_command({command, "--timeout", timeout}), Eq(mp::ReturnCode::Ok));
-}
-
-INSTANTIATE_TEST_SUITE_P(Client, TimeoutCorrectSuite, Combine(ValuesIn(timeout_commands), ValuesIn(valid_timeouts)));
-
-struct TimeoutNullSuite : Client, WithParamInterface<std::string>
-{
-};
-
-TEST_P(TimeoutNullSuite, cmds_with_timeout_null_bad)
-{
-    EXPECT_THAT(send_command({GetParam(), "--timeout"}), Eq(mp::ReturnCode::CommandLineError));
-}
-
-INSTANTIATE_TEST_SUITE_P(Client, TimeoutNullSuite, ValuesIn(timeout_commands));
-
-struct TimeoutInvalidSuite : Client, WithParamInterface<std::tuple<std::string, std::string>>
-{
-};
-
-TEST_P(TimeoutInvalidSuite, cmds_with_invalid_timeout_bad)
-{
-    std::stringstream cerr_stream;
-    const auto& [command, timeout] = GetParam();
-
-    EXPECT_THAT(send_command({command, "--timeout", timeout}, trash_stream, cerr_stream),
-                Eq(mp::ReturnCode::CommandLineError));
-
-    EXPECT_EQ(cerr_stream.str(), "error: --timeout value has to be a positive integer\n");
-}
-
-INSTANTIATE_TEST_SUITE_P(Client, TimeoutInvalidSuite, Combine(ValuesIn(timeout_commands), ValuesIn(invalid_timeouts)));
-
-struct TimeoutSuite : Client, WithParamInterface<std::string>
-{
-    void SetUp() override
-    {
-        Client::SetUp();
-
-        ON_CALL(mock_daemon, launch).WillByDefault(request_sleeper<mp::LaunchRequest, mp::LaunchReply>);
-        ON_CALL(mock_daemon, start).WillByDefault(request_sleeper<mp::StartRequest, mp::StartReply>);
-        ON_CALL(mock_daemon, restart).WillByDefault(request_sleeper<mp::RestartRequest, mp::RestartReply>);
-        ON_CALL(mock_daemon, ssh_info).WillByDefault(request_sleeper<mp::SSHInfoRequest, mp::SSHInfoReply>);
-    }
-
-    template <typename RequestType, typename ReplyType>
-    static grpc::Status request_sleeper(grpc::ServerContext* context, const RequestType* request,
-                                        grpc::ServerWriter<ReplyType>* response)
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        return grpc::Status::OK;
-    }
-};
-
-TEST_P(TimeoutSuite, command_exits_on_timeout)
-{
-    auto [mock_utils, guard] = mpt::MockUtils::inject();
-
-    EXPECT_CALL(mock_daemon, launch).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, start).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, restart).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, ssh_info).Times(AtMost(1));
-    EXPECT_CALL(*mock_utils, exit(mp::timeout_exit_code));
-
-    send_command({GetParam(), "--timeout", "1"});
-}
-
-TEST_P(TimeoutSuite, command_completes_without_timeout)
-{
-    EXPECT_CALL(mock_daemon, launch).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, start).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, restart).Times(AtMost(1));
-    EXPECT_CALL(mock_daemon, ssh_info).Times(AtMost(1));
-
-    EXPECT_EQ(send_command({GetParam(), "--timeout", "5"}), mp::ReturnCode::Ok);
-}
-
-INSTANTIATE_TEST_SUITE_P(Client, TimeoutSuite, ValuesIn(timeout_commands));
 
 struct ClientLogMessageSuite : Client, WithParamInterface<std::vector<std::string>>
 {
