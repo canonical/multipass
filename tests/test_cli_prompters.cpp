0cf64021790d9d6c9fdab0e63160fe25bbf6d3e6
Chris Townsend <christopher.townsend@canonical.com>
2021-12-09 09:19:43 -0500

    [tests] Add tests for PassphrasePrompter

--- tests/test_cli_prompters.cpp
+++ tests/test_cli_prompters.cpp
@@ -16,6 +16,7 @@
  */
 
 #include "common.h"
+#include "mock_terminal.h"
 #include "stub_terminal.h"
 
 #include <multipass/cli/prompters.h>
@@ -51,11 +52,34 @@ TEST_F(CLIPrompters, PlainReturnsText)
     EXPECT_EQ(prompt.prompt(""), "value");
 }
 
-class CLIPromptersCinState : public CLIPrompters, public WithParamInterface<std::ios_base::iostate>
+// Note that the following test does not actually test if the terminal echos or not-
+// that is specific to platform terminal types.
+TEST_F(CLIPrompters, passphraseCallsEchoAndReturnsExpectedPassphrase)
+{
+    const std::string prompter_string{"Enter passphrase: "};
+    const std::string passphrase{"foo"};
+    mpt::MockTerminal mock_terminal;
+
+    cin.str(passphrase + "\n");
+
+    EXPECT_CALL(mock_terminal, set_cin_echo(false)).Times(1);
+    EXPECT_CALL(mock_terminal, set_cin_echo(true)).Times(1);
+    EXPECT_CALL(mock_terminal, cout()).Times(2).WillRepeatedly([this]() -> std::ostream& { return cout; });
+    EXPECT_CALL(mock_terminal, cin()).Times(2).WillRepeatedly([this]() -> std::istream& { return cin; });
+
+    mp::PassphrasePrompter prompter{&mock_terminal};
+
+    auto input = prompter.prompt(prompter_string);
+
+    EXPECT_EQ(cout.str(), prompter_string + "\n");
+    EXPECT_EQ(input, passphrase);
+}
+
+class CLIPromptersBadCinState : public CLIPrompters, public WithParamInterface<std::ios_base::iostate>
 {
 };
 
-TEST_P(CLIPromptersCinState, PlainThrows)
+TEST_P(CLIPromptersBadCinState, PlainThrows)
 {
     auto prompt = mp::PlainPrompter{&term};
 
@@ -63,4 +87,5 @@ TEST_P(CLIPromptersCinState, PlainThrows)
     MP_EXPECT_THROW_THAT(prompt.prompt(""), mp::PromptException, mpt::match_what(HasSubstr("Failed to read value")));
 }
 
-INSTANTIATE_TEST_SUITE_P(BadCin, CLIPromptersCinState, Values(std::ios::eofbit, std::ios::failbit, std::ios::badbit));
+INSTANTIATE_TEST_SUITE_P(CLIPrompters, CLIPromptersBadCinState,
+                         Values(std::ios::eofbit, std::ios::failbit, std::ios::badbit));
