673eb77c164fdda3da8d5e8b7eb85a3ad78c7857
Chris Townsend <christopher.townsend@canonical.com>
2021-12-14 10:58:01 -0500

    [tests] Add new unit tests for testing UnixTerminal

---
+++ tests/unix/test_unix_terminal.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2021 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <tests/common.h>
+
+#include "mock_libc_functions.h"
+
+#include <src/platform/console/unix_terminal.h>
+
+#include <tuple>
+
+namespace mp = multipass;
+namespace mpt = multipass::test;
+
+using namespace testing;
+
+namespace
+{
+struct TestUnixTerminal : public Test
+{
+    mp::UnixTerminal unix_terminal;
+    const int fake_fd{42};
+};
+} // namespace
+
+TEST_F(TestUnixTerminal, cinFdReturnsExpectedFd)
+{
+    REPLACE(fileno, [this](auto stream) {
+        EXPECT_EQ(stream, stdin);
+        return fake_fd;
+    });
+
+    EXPECT_EQ(unix_terminal.cin_fd(), fake_fd);
+}
+
+TEST_F(TestUnixTerminal, coutFdReturnsExpectedFd)
+{
+    REPLACE(fileno, [this](auto stream) {
+        EXPECT_EQ(stream, stdout);
+        return fake_fd;
+    });
+
+    EXPECT_EQ(unix_terminal.cout_fd(), fake_fd);
+}
+
+TEST_F(TestUnixTerminal, isLiveReturnsTrueWhenTTY)
+{
+    REPLACE(fileno, [this](auto) { return fake_fd; });
+
+    REPLACE(isatty, [this](auto fd) {
+        EXPECT_EQ(fd, fake_fd);
+        return 1;
+    });
+
+    EXPECT_TRUE(unix_terminal.cin_is_live());
+    EXPECT_TRUE(unix_terminal.cout_is_live());
+}
+
+TEST_F(TestUnixTerminal, isLiveReturnsFalseWhenNotTTY)
+{
+    REPLACE(fileno, [this](auto) { return fake_fd; });
+
+    REPLACE(isatty, [this](auto fd) {
+        EXPECT_EQ(fd, fake_fd);
+        return 0;
+    });
+
+    EXPECT_FALSE(unix_terminal.cin_is_live());
+    EXPECT_FALSE(unix_terminal.cout_is_live());
+}
+
+TEST_F(TestUnixTerminal, setsEchoOnTerminal)
+{
+    REPLACE(fileno, [this](auto) { return fake_fd; });
+
+    REPLACE(tcgetattr, [](auto...) { return 0; });
+
+    REPLACE(tcsetattr, [](auto, auto, auto termios_p) {
+        EXPECT_TRUE((termios_p->c_lflag & ECHO) == ECHO);
+        return 0;
+    });
+
+    unix_terminal.set_cin_echo(true);
+}
+
+TEST_F(TestUnixTerminal, unsetsEchoOnTerminal)
+{
+    REPLACE(fileno, [this](auto) { return fake_fd; });
+
+    REPLACE(tcgetattr, [](auto...) { return 0; });
+
+    REPLACE(tcsetattr, [](auto, auto, auto termios_p) {
+        EXPECT_FALSE((termios_p->c_lflag & ECHO) == ECHO);
+        return 0;
+    });
+
+    unix_terminal.set_cin_echo(false);
+}
