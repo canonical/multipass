db39041675096a628878eefac286f668c372af2b
Ricardo Abreu <ricardo.abreu@canonical.com>
2022-02-11 17:40:08 +0000

    [tests] Fix coverage misses of newly mocked funcs

--- tests/linux/test_platform_linux.cpp
+++ tests/linux/test_platform_linux.cpp
@@ -208,6 +208,16 @@ TEST_F(PlatformLinux, test_empty_sync_winterm_profiles)
     EXPECT_NO_THROW(mp::platform::sync_winterm_profiles());
 }
 
+TEST_F(PlatformLinux, test_default_driver) // TODO@nomerge private side
+{
+    EXPECT_THAT(MP_PLATFORM.default_driver(), AnyOf("qemu", "lxd"));
+}
+
+TEST_F(PlatformLinux, test_default_privileged_mounts) // TODO@nomerge private side
+{
+    EXPECT_EQ(MP_PLATFORM.default_privileged_mounts(), "true");
+}
+
 TEST_F(PlatformLinux, test_autostart_desktop_file_properly_placed)
 {
     try
