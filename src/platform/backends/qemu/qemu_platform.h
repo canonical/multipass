852d54710f3f80c0ef2b61e2ba1fdeb92d8c1193
Chris Townsend <christopher.townsend@canonical.com>
2022-03-01 05:17:50 -0500

    [qemu/platform] networks() should be const

--- src/platform/backends/qemu/qemu_platform.h
+++ src/platform/backends/qemu/qemu_platform.h
@@ -54,7 +54,7 @@ public:
     {
         return {};
     };
-    virtual std::vector<NetworkInterfaceInfo> networks()
+    virtual std::vector<NetworkInterfaceInfo> networks() const
     {
         throw NotImplementedOnThisBackendException("networks");
     };
