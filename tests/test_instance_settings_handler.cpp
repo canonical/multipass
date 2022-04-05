8f22497db23575e3b76ff0a518e92fe4b6f675b3
Ricardo Abreu <ricardo.abreu@canonical.com>
2022-04-05 14:19:01 +0100

    [tests] Check ins-mod keys for deleted/preparing
    Expand instance-mod keys test, to verify that all instance keys are
    returned regardless of whether instances are preparing or deleted.

--- tests/test_instance_settings_handler.cpp
+++ tests/test_instance_settings_handler.cpp
@@ -25,6 +25,7 @@
 #include <QString>
 
 #include <string>
+#include <tuple>
 #include <unordered_map>
 
 namespace mp = multipass;
@@ -33,35 +34,69 @@ using namespace testing;
 
 namespace
 {
-struct TestInstanceSettingsKeys : public TestWithParam<std::vector<const char*>>
+enum class SpecialInstanceState
+{
+    none,
+    preparing,
+    deleted
+};
+using InstanceName = const char*;
+using Instance = std::tuple<InstanceName, SpecialInstanceState>;
+using Instances = std::vector<Instance>;
+using InstanceLists = std::vector<Instances>;
+
+struct TestInstanceSettingsKeys : public TestWithParam<Instances>
 {
 };
 
 TEST_P(TestInstanceSettingsKeys, keysCoversAllPropertiesForAllInstances)
 {
     constexpr auto props = std::array{"cpus", "disk", "memory"};
-    const auto names = GetParam();
+    const auto intended_instances = GetParam();
 
-    auto vms = std::unordered_map<std::string, mp::VirtualMachine::ShPtr>{};
     auto specs = std::unordered_map<std::string, mp::VMSpecs>{};
+    auto vms = std::unordered_map<std::string, mp::VirtualMachine::ShPtr>{};
+    auto deleted_vms = std::unordered_map<std::string, mp::VirtualMachine::ShPtr>{};
+    auto preparing_vms = std::unordered_set<std::string>{};
     auto expected_keys = std::vector<QString>{};
 
-    for (const auto& name : names)
+    for (const auto& [name, special_state] : intended_instances)
     {
-        specs[name] = {};
+        specs[name];
+
+        if (special_state == SpecialInstanceState::preparing)
+            preparing_vms.emplace(name);
+        else if (special_state == SpecialInstanceState::deleted)
+            deleted_vms[name];
+
         for (const auto& prop : props)
             expected_keys.push_back(QString{"local.%1.%2"}.arg(name, prop));
     }
 
-    auto handler = mp::InstanceSettingsHandler{specs, vms, /*deleted_instances=*/{}, /*preparing_instances=*/{},
-                                               /*instance_persister=*/[] {}};
+    auto handler = mp::InstanceSettingsHandler{specs, vms, deleted_vms, preparing_vms, /*instance_persister=*/[] {}};
 
     EXPECT_THAT(handler.keys(), UnorderedElementsAreArray(expected_keys));
 }
 
-INSTANTIATE_TEST_SUITE_P(TestInstanceSettingsHandler, TestInstanceSettingsKeys,
-                         ValuesIn(std::vector<std::vector<const char*>>{{},
-                                                                        {"morning-light-mountain"},
-                                                                        {"foo", "bar", "MORNING-LIGHT-MOUNTAIN"},
-                                                                        {"a", "b", "c", "d", "e", "f", "g"}}));
+INSTANTIATE_TEST_SUITE_P(TestInstanceSettingsKeysEmpty, TestInstanceSettingsKeys, Values(Instances{}));
+INSTANTIATE_TEST_SUITE_P(TestInstanceSettingsKeysSingle, TestInstanceSettingsKeys,
+                         Values(Instances{{"morning-light-mountain", SpecialInstanceState::none}},
+                                Instances{{"morning-light-mountain", SpecialInstanceState::deleted}},
+                                Instances{{"morning-light-mountain", SpecialInstanceState::preparing}}));
+INSTANTIATE_TEST_SUITE_P(TestInstanceSettingsKeysMultiple, TestInstanceSettingsKeys,
+                         ValuesIn(InstanceLists{{{"foo", SpecialInstanceState::none},
+                                                 {"bar", SpecialInstanceState::none},
+                                                 {"baz", SpecialInstanceState::none}},
+                                                {{"foo", SpecialInstanceState::deleted},
+                                                 {"bar", SpecialInstanceState::preparing},
+                                                 {"baz", SpecialInstanceState::preparing}},
+                                                {{"foo", SpecialInstanceState::deleted},
+                                                 {"bar", SpecialInstanceState::none},
+                                                 {"baz", SpecialInstanceState::deleted}},
+                                                {{"foo", SpecialInstanceState::preparing},
+                                                 {"bar", SpecialInstanceState::preparing},
+                                                 {"baz", SpecialInstanceState::none}},
+                                                {{"foo", SpecialInstanceState::none},
+                                                 {"bar", SpecialInstanceState::none},
+                                                 {"baz", SpecialInstanceState::preparing}}}));
 } // namespace
