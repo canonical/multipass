ce103a9b2666c179e9566fa8b092aa3cd7014cc5
Luis Peñaranda <luis.penaranda@canonical.com>
2022-06-28 12:35:32 -0300

    [tests] Make Blueprint tests expect a new check.

--- tests/test_blueprint_provider.cpp
+++ tests/test_blueprint_provider.cpp
@@ -317,7 +317,7 @@ TEST_F(VMBlueprintProvider, allBlueprintsReturnsExpectedInfo)
 
     auto blueprints = blueprint_provider.all_blueprints();
 
-    EXPECT_EQ(blueprints.size(), 10ul);
+    EXPECT_EQ(blueprints.size(), 11ul);
 
     EXPECT_TRUE(std::find_if(blueprints.cbegin(), blueprints.cend(), [](const mp::VMImageInfo& blueprint_info) {
                     return ((blueprint_info.aliases.size() == 1) && (blueprint_info.aliases[0] == "test-blueprint1") &&
@@ -583,7 +583,7 @@ TEST_F(VMBlueprintProvider, allBlueprintsReturnsExpectedInfoForArch)
 
     auto blueprints = blueprint_provider.all_blueprints();
 
-    EXPECT_EQ(blueprints.size(), 11ul);
+    EXPECT_EQ(blueprints.size(), 12ul);
     EXPECT_TRUE(std::find_if(blueprints.cbegin(), blueprints.cend(), [](const mp::VMImageInfo& blueprint_info) {
                     return ((blueprint_info.aliases.size() == 1) && (blueprint_info.aliases[0] == "arch-only") &&
                             (blueprint_info.release_title == "An arch-only blueprint"));
