580a262843ed690782bf95bb93b9b670d814462e
Andrei Toterman <andrei.toterman@canonical.com>
2022-07-06 15:44:49 +0300

    mock sftp client

---
+++ tests/mock_sftp_client.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019-2022 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MOCK_SFTP_CLIENT_H
+#define MOCK_SFTP_CLIENT_H
+
+#include "common.h"
+#include <multipass/ssh/sftp_client.h>
+
+namespace multipass::test
+{
+struct MockSFTPClient : public SFTPClient
+{
+    MOCK_METHOD(bool, is_dir, (const fs::path& path), (override));
+    MOCK_METHOD(bool, push,
+                (const fs::path& source_path, const fs::path& target_path, QFlags<TransferFlags> flags,
+                 std::ostream& err_sink),
+                (override));
+    MOCK_METHOD(bool, pull,
+                (const fs::path& source_path, const fs::path& target_path, QFlags<TransferFlags> flags,
+                 std::ostream& err_sink),
+                (override));
+    MOCK_METHOD(void, push_file, (const fs::path& source_path, const fs::path& target_path), (override));
+    MOCK_METHOD(void, pull_file, (const fs::path& source_path, const fs::path& target_path), (override));
+    MOCK_METHOD(bool, push_dir, (const fs::path& source_path, const fs::path& target_path, std::ostream& err_sink),
+                (override));
+    MOCK_METHOD(bool, pull_dir, (const fs::path& source_path, const fs::path& target_path, std::ostream& err_sink),
+                (override));
+    MOCK_METHOD(void, from_cin, (std::istream & cin, const fs::path& target_path), (override));
+    MOCK_METHOD(void, to_cout, (const fs::path& source_path, std::ostream& cout), (override));
+};
+} // namespace multipass::test
+
+#endif // MOCK_SFTP_CLIENT_H
