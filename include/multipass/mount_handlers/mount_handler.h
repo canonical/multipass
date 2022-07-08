94c0725500b5d9cf23139e0fe8dd397850dee7ae
Chris Townsend <christopher.townsend@canonical.com>
2022-07-08 12:49:19 -0400

    [mount_handler] Pass grpc server as a std::variant
    This helps handle the different reply types cleanly.

--- include/multipass/mount_handlers/mount_handler.h
+++ include/multipass/mount_handlers/mount_handler.h
@@ -20,27 +20,41 @@
 
 #include <multipass/disabled_copy_move.h>
 #include <multipass/id_mappings.h>
+#include <multipass/rpc/multipass.grpc.pb.h>
 #include <multipass/ssh/ssh_key_provider.h>
 
 #include <string>
+#include <variant>
 
 namespace multipass
 {
 class VirtualMachine;
+
+using ServerVariant =
+    std::variant<grpc::ServerWriterInterface<StartReply>*, grpc::ServerWriterInterface<LaunchReply>*,
+                 grpc::ServerWriterInterface<MountReply>*, grpc::ServerWriterInterface<RestartReply>*>;
 
 class MountHandler : private DisabledCopyMove
 {
 public:
     virtual ~MountHandler() = default;
 
-    virtual void start_mount(VirtualMachine* vm, const std::string& source_path, const std::string& target_path,
-                             const id_mappings& gid_mappings, const id_mappings& uid_mappings) = 0;
+    virtual void start_mount(VirtualMachine* vm, ServerVariant server, const std::string& source_path,
+                             const std::string& target_path, const id_mappings& gid_mappings,
+                             const id_mappings& uid_mappings) = 0;
     virtual bool stop_mount(const std::string& instance, const std::string& path) = 0;
     virtual void stop_all_mounts_for_instance(const std::string& instance) = 0;
     virtual bool has_instance_already_mounted(const std::string& instance, const std::string& path) const = 0;
 
 protected:
     MountHandler(const SSHKeyProvider& ssh_key_provider) : ssh_key_provider(&ssh_key_provider){};
+
+    template <typename Reply>
+    Reply make_reply_from_server(grpc::ServerWriterInterface<Reply>&)
+    {
+        return Reply{};
+    }
+
     const SSHKeyProvider* ssh_key_provider;
 };
 
