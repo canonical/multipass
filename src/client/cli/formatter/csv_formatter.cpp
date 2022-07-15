eeaf988f46cc55373ce2d2a360d2c75215439b0e
sharder996 <sharder996@gmail.com>
2022-07-15 08:27:50 -0500

    linting changes

--- src/client/cli/formatter/csv_formatter.cpp
+++ src/client/cli/formatter/csv_formatter.cpp
@@ -27,8 +27,9 @@ std::string mp::CSVFormatter::format(const InfoReply& reply) const
 {
     fmt::memory_buffer buf;
     fmt::format_to(
-        buf, "Name,State,Ipv4,Ipv6,Release,Image hash,Image release,CPU(s),Load,Disk usage,Disk total,Memory usage,Memory "
-             "total,Mounts,AllIPv4\n");
+        buf,
+        "Name,State,Ipv4,Ipv6,Release,Image hash,Image release,CPU(s),Load,Disk usage,Disk total,Memory usage,Memory "
+        "total,Mounts,AllIPv4\n");
 
     for (const auto& info : format::sorted(reply.info()))
     {
@@ -93,7 +94,9 @@ std::string mp::CSVFormatter::format(const FindReply& reply) const
 
         mp::format::filter_aliases(aliases);
 
-        auto image_id = aliases[0].remote_name().empty() ? aliases[0].alias() : fmt::format("{}:{}", aliases[0].remote_name(), aliases[0].alias());
+        auto image_id = aliases[0].remote_name().empty()
+                            ? aliases[0].alias()
+                            : fmt::format("{}:{}", aliases[0].remote_name(), aliases[0].alias());
         fmt::format_to(buf, "{},{},{},{},{},{}\n", image_id, aliases[0].remote_name(),
                        fmt::join(aliases.cbegin() + 1, aliases.cend(), ";"), image.os(), image.release(),
                        image.version());
