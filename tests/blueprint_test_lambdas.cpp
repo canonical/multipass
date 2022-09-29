1d78305bea4fb08eacabb2946c890fd1af6f3dbf
Luis Peñaranda <luis.penaranda@canonical.com>
2022-09-29 12:02:47 -0300

    [tests] Check SHA goes into fetch_image().

--- tests/blueprint_test_lambdas.cpp
+++ tests/blueprint_test_lambdas.cpp
@@ -35,11 +35,11 @@ namespace mpt = multipass::test;
 
 std::function<mp::VMImage(const mp::FetchType&, const mp::Query&, const mp::VMImageVault::PrepareAction&,
                           const mp::ProgressMonitor&, const bool, const std::optional<std::string>)>
-mpt::fetch_image_lambda(const std::string& release, const std::string& remote)
+mpt::fetch_image_lambda(const std::string& release, const std::string& remote, const bool must_have_checksum)
 {
-    return [&release, &remote](const mp::FetchType& fetch_type, const mp::Query& query,
-                               const mp::VMImageVault::PrepareAction& prepare, const mp::ProgressMonitor& monitor,
-                               const bool unlock, const std::optional<std::string> checksum) {
+    return [&release, &remote, must_have_checksum](
+               const mp::FetchType& fetch_type, const mp::Query& query, const mp::VMImageVault::PrepareAction& prepare,
+               const mp::ProgressMonitor& monitor, const bool unlock, const std::optional<std::string> checksum) {
         EXPECT_EQ(query.release, release);
         if (remote.empty())
         {
@@ -48,6 +48,11 @@ mpt::fetch_image_lambda(const std::string& release, const std::string& remote)
         else
         {
             EXPECT_EQ(query.remote_name, remote);
+        }
+
+        if (must_have_checksum)
+        {
+            EXPECT_NE(checksum, std::nullopt);
         }
 
         return mpt::StubVMImageVault().fetch_image(fetch_type, query, prepare, monitor, unlock, checksum);
@@ -75,13 +80,15 @@ std::function<mp::Query(const std::string&, mp::VirtualMachineDescription&, mp::
 mpt::fetch_blueprint_for_lambda(const int& num_cores, const mp::MemorySize& mem_size, const mp::MemorySize& disk_space,
                                 const std::string& release, const std::string& remote,
                                 std::optional<std::pair<std::string, mp::AliasDefinition>> alias,
-                                std::optional<std::string> workspace)
+                                std::optional<std::string> workspace, std::optional<std::string> sha256)
 {
-    return [&num_cores, &mem_size, &disk_space, &release, &remote, alias,
-            workspace](const auto&, mp::VirtualMachineDescription& vm_desc, mp::ClientLaunchData& l_data) -> mp::Query {
+    return [&num_cores, &mem_size, &disk_space, &release, &remote, alias, workspace,
+            sha256](const auto&, mp::VirtualMachineDescription& vm_desc, mp::ClientLaunchData& l_data) -> mp::Query {
         vm_desc.num_cores = num_cores;
         vm_desc.mem_size = mem_size;
         vm_desc.disk_space = disk_space;
+        if (sha256)
+            vm_desc.image.id = *sha256;
 
         if (alias)
             l_data.aliases_to_be_created.emplace(alias->first, alias->second);
--- tests/test_daemon_launch.cpp
+++ tests/test_daemon_launch.cpp
@@ -105,3 +105,53 @@ TEST_F(TestDaemonLaunch, blueprintFoundMountsWorkspaceWithNameOverride)
         EXPECT_EQ(reply.workspaces_to_be_created(0), command_line_name);
     }
 }
+
+TEST_F(TestDaemonLaunch, v2BlueprintFoundPropagatesSha)
+{
+    const std::string name{"ultimo-blueprint"};
+    const std::string command_line_name{"name-override"};
+    static constexpr int num_cores = 4;
+    const std::string mem_size_str{"4G"};
+    const auto mem_size = mp::MemorySize(mem_size_str);
+    const std::string disk_space_str{"25G"};
+    const auto disk_space = mp::MemorySize(disk_space_str);
+    const std::string remote{"release"};
+    const std::string release{"focal"};
+    const std::string sha256{"0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"};
+
+    auto mock_factory = use_a_mock_vm_factory();
+    auto mock_image_vault = std::make_unique<NiceMock<mpt::MockVMImageVault>>();
+    auto mock_blueprint_provider = std::make_unique<NiceMock<mpt::MockVMBlueprintProvider>>();
+
+    EXPECT_CALL(*mock_factory, create_virtual_machine(_, _))
+        .WillOnce(mpt::create_virtual_machine_lambda(num_cores, mem_size, disk_space, command_line_name));
+
+    // The expectation of this test is set in fetch_image_lambda().
+    EXPECT_CALL(*mock_image_vault, fetch_image(_, _, _, _, _, _))
+        .WillOnce(mpt::fetch_image_lambda(release, remote, true));
+
+    EXPECT_CALL(*mock_blueprint_provider, fetch_blueprint_for(_, _, _))
+        .WillOnce(mpt::fetch_blueprint_for_lambda(num_cores, mem_size, disk_space, release, remote, std::nullopt,
+                                                  std::nullopt, sha256));
+
+    EXPECT_CALL(*mock_blueprint_provider, name_from_blueprint(_)).WillOnce(Return(name));
+
+    config_builder.blueprint_provider = std::move(mock_blueprint_provider);
+    config_builder.vault = std::move(mock_image_vault);
+
+    mp::Daemon daemon{config_builder.build()};
+
+    mp::LaunchRequest request;
+    request.set_instance_name(command_line_name);
+    request.set_image(name);
+
+    mp::LaunchReply reply;
+    StrictMock<mpt::MockServerReaderWriter<mp::LaunchReply, mp::LaunchRequest>> writer{};
+
+    EXPECT_CALL(writer, Write(_, _)).WillRepeatedly([&reply](const mp::LaunchReply& written_reply, auto) -> bool {
+        reply = written_reply;
+        return true;
+    });
+
+    auto status = call_daemon_slot(daemon, &mp::Daemon::launch, request, writer);
+}
