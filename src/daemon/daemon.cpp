98a912552324aa1b7d15282b85e9fe10256ba05d
Ricardo Abreu <ricardo.abreu@canonical.com>
2022-10-19 11:58:20 +0100

    [daemon] Write migrated QEMU instance-image JSON

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -2973,9 +2973,9 @@ grpc::Status mp::Daemon::migrate_from_hyperkit(grpc::ServerReaderWriterInterface
         }
     }
 
-    // Write QEMU instance DB
     if (instances_migrated)
     {
+        // Write QEMU instances DB
         qemu_instances_doc.setObject(qemu_instances_json);
         [[maybe_unused]] auto reset_success = qemu_instances_db->reset(); /* seek back to the beginning of the file, to
                                                                              overwrite its current contents */
@@ -2983,6 +2983,16 @@ grpc::Status mp::Daemon::migrate_from_hyperkit(grpc::ServerReaderWriterInterface
 
         MP_FILEOPS.write(*qemu_instances_db, qemu_instances_doc.toJson()); /* overwrites with more content, so no need
                                                                               to erase first */
+
+        // Write QEMU instance images DB
+        // TODO@nomerge extract this common code
+        qemu_instance_images_doc.setObject(qemu_instance_images_json);
+        reset_success = qemu_instance_images_db->reset(); /* seek back to the beginning of the file, to overwrite its
+                                                             current contents */
+        assert(reset_success);
+
+        MP_FILEOPS.write(*qemu_instance_images_db, qemu_instance_images_doc.toJson()); /* overwrites with more content,
+                                                                                          so no need to erase first */
     }
 
     return grpc::Status::OK;
