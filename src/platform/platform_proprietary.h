cee54299fd5b6de4977f8426eb5176cb02850833
Chris Townsend <christopher.townsend@canonical.com>
2023-01-12 08:36:25 -0500

    [proprietary] Update unlock code

--- src/platform/platform_proprietary.h
+++ src/platform/platform_proprietary.h
@@ -26,7 +26,7 @@ namespace multipass
 {
 namespace platform
 {
-constexpr auto unlock_code{"honeyed-bigeye"};
+constexpr auto unlock_code{"fatherly-sheatfish"};
 // clang-format off
 const std::unordered_set<std::string> supported_release_aliases{"core",   "core16", "core18", "core20", "core22",
                                                                 "default", "ubuntu", "lts",
