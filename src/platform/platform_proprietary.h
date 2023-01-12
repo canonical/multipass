e27e4fddb82c8e5aea70a2ea0795ac03e2030fad
Chris Townsend <christopher.townsend@canonical.com>
2023-01-12 08:42:16 -0500

    [proprietary] Update list of supported Ubuntu versions

--- src/platform/platform_proprietary.h
+++ src/platform/platform_proprietary.h
@@ -33,8 +33,8 @@ const std::unordered_set<std::string> supported_release_aliases{"core",   "core1
                                                                 "16.04", "x", "xenial",
                                                                 "18.04", "b", "bionic",
                                                                 "20.04", "f", "focal",
-                                                                "21.10", "i", "impish",
-                                                                "22.04", "j", "jammy"};
+                                                                "22.04", "j", "jammy",
+                                                                "22.10", "k", "kinetic"};
 // clang-format on
 const std::unordered_set<std::string> supported_core_aliases{"core", "core16", "core18", "core20", "core22"};
 const std::unordered_set<std::string> supported_snapcraft_aliases{"core18", "18.04",  "core20",
