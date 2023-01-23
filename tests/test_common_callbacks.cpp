1f24b525f0cfb7a587ed3977e1f5b819f6ba8352
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-01-23 13:12:22 +0000

    [tests] Check logging of iterative callback too

--- tests/test_common_callbacks.cpp
+++ tests/test_common_callbacks.cpp
@@ -15,6 +15,7 @@
  */
 
 #include "common.h"
+#include "stub_terminal.h"
 
 #include <src/client/cli/cmd/animated_spinner.h>
 #include <src/client/cli/cmd/common_callbacks.h>
@@ -22,37 +23,52 @@
 #include <sstream>
 
 namespace mp = multipass;
+namespace mpt = mp::test;
 using namespace testing;
 
 struct TestSpinnerCallbacks : public Test
 {
     std::ostringstream out, err;
+    std::istringstream in;
+    mpt::StubTerminal term{out, err, in};
     mp::AnimatedSpinner spinner{out};
 };
 
-TEST_F(TestSpinnerCallbacks, logging_spinner_callback_logs)
+using IterativeCallback = bool;
+struct TestLoggingSpinnerCallbacks : public TestSpinnerCallbacks, public WithParamInterface<IterativeCallback>
+{
+    std::function<void(const mp::MountReply&, grpc::ClientReaderWriterInterface<mp::MountRequest, mp::MountReply>*)>
+    make_callback()
+    {
+        if (GetParam())
+            return mp::make_iterative_spinner_callback<mp::MountRequest, mp::MountReply>(spinner, term);
+        else
+            return mp::make_logging_spinner_callback<mp::MountRequest, mp::MountReply>(spinner, err);
+    }
+};
+
+TEST_P(TestLoggingSpinnerCallbacks, logging_spinner_callback_logs)
 {
     constexpr auto log = "message in a bottle";
 
-    mp::PurgeReply reply;
+    mp::MountReply reply;
     reply.set_log_line(log);
 
-    auto cb = mp::make_logging_spinner_callback<mp::PurgeRequest, mp::PurgeReply>(spinner, err);
-    cb(reply, nullptr);
+    make_callback()(reply, nullptr);
 
     EXPECT_THAT(err.str(), StrEq(log));
     EXPECT_THAT(out.str(), MatchesRegex(R"(\s*)")); /* this is not empty because print stops, stop clears, and clear
                                                        prints carriage returns and spaces */
 }
 
-TEST_F(TestSpinnerCallbacks, logging_spinner_callback_ignores_empty_log)
+TEST_P(TestLoggingSpinnerCallbacks, logging_spinner_callback_ignores_empty_log)
 {
-    mp::PurgeReply reply;
-
-    auto cb = mp::make_logging_spinner_callback<mp::PurgeRequest, mp::PurgeReply>(spinner, err);
-    cb(reply, nullptr);
+    mp::MountReply reply;
+    make_callback()(reply, nullptr);
 
     EXPECT_TRUE(err.str().empty());
     EXPECT_THAT(out.str(), MatchesRegex(R"(\s*)")); /* this is not empty because print stops, stop clears, and clear
                                                        prints carriage returns and spaces */
 }
+
+INSTANTIATE_TEST_SUITE_P(TestLoggingSpinnerCallbacks, TestLoggingSpinnerCallbacks, Values(false, true));
