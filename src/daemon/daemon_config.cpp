48f4d02d4c1824aa16b99a2db45d55341909b4d1
sharder996 <sharder996@gmail.com>
2023-01-23 07:45:34 -0800

    lock down entire mp dir

--- src/daemon/daemon_config.cpp
+++ src/daemon/daemon_config.cpp
@@ -124,6 +124,8 @@ std::unique_ptr<const mp::DaemonConfig> mp::DaemonConfigBuilder::build()
         else
             data_directory = MP_STDPATHS.writableLocation(StandardPaths::AppDataLocation);
     }
+    MP_PLATFORM.set_permissions(storage_path, QFileDevice::ReadOwner | QFileDevice::WriteOwner | QFileDevice::ExeOwner);
+
     if (url_downloader == nullptr)
         url_downloader = std::make_unique<URLDownloader>(cache_directory, std::chrono::seconds{10});
     if (factory == nullptr)
