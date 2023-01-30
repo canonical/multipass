43bd53f5eb6c7e9c31b6fa51c2330e50543625a2
sharder996 <sharder996@gmail.com>
2023-01-30 11:01:57 -0800

    remove year from copyright

--- include/multipass/cli/return_codes.h
+++ include/multipass/cli/return_codes.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2022 Canonical, Ltd.
+ * Copyright (C) Canonical, Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 3, as published by
