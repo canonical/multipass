a2a775ae8d964d45f3ad9348f2b0b76481d3f0cd
Luis Peñaranda <luis.penaranda@canonical.com>
2023-02-07 15:22:27 -0300

    [flutter] Add barebone project.

---
+++ src/client/desktop_gui/linux/main.cc
@@ -0,0 +1,7 @@
+#include "my_application.h"
+
+int main(int argc, char** argv)
+{
+    g_autoptr(MyApplication) app = my_application_new();
+    return g_application_run(G_APPLICATION(app), argc, argv);
+}
