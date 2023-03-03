a134bc9a8f911be4a659ead64b444141bacfedf8
Andrei Toterman <andrei.toterman@canonical.com>
2023-03-03 19:06:00 +0100

    [flutter] refactor vms table

--- src/client/desktop_gui/lib/vms_table.dart
+++ src/client/desktop_gui/lib/vms_table.dart
@@ -1,86 +1,115 @@
+import 'package:collection/collection.dart';
 import 'package:data_table_2/data_table_2.dart';
 import 'package:filesize/filesize.dart';
 import 'package:flutter/material.dart';
 
-import 'generated/multipass.pb.dart';
+import 'generated/multipass.pbgrpc.dart';
+import 'instance_actions_button.dart';
 
 class VMsTable extends StatefulWidget {
-  final Iterable<InfoReply_Info> infos;
+  final Iterable<InfoReply_Info>? infos;
+  final ValueSetter<Map<String, InfoReply_Info>>? onSelect;
+  final Map<String, InfoReply_Info> selected;
 
-  const VMsTable({super.key, required this.infos});
+  const VMsTable({
+    super.key,
+    this.infos,
+    this.selected = const {},
+    this.onSelect,
+  });
 
   @override
   State<VMsTable> createState() => _VMsTableState();
 }
 
 class _VMsTableState extends State<VMsTable> {
-  final selected = <String>{};
   int? sortIndex;
   bool sortAscending = true;
-  String Function(InfoReply_Info)? sortByExtractor;
+  String Function(InfoReply_Info)? sortExtractor;
 
-  final tableHeaders = {
-    'Name': (InfoReply_Info info) => info.name,
-    'Status': (InfoReply_Info info) => info.instanceStatus.status.name,
-    'Usage': (InfoReply_Info info) => info.diskUsage,
-    'Total': (InfoReply_Info info) => info.diskTotal,
-    'Image': (InfoReply_Info info) => info.currentRelease,
-    'Ipv4': (InfoReply_Info info) => info.ipv4.isEmpty ? '' : info.ipv4.first,
-    'Mounts': null,
-    'Actions': null,
-  };
+  DataColumnSortCallback _extractorToSortCallback(
+    String Function(InfoReply_Info)? extractor,
+  ) =>
+      (index, ascending) => setState(() {
+            sortIndex = (sortIndex == index && ascending) ? null : index;
+            sortAscending = ascending;
+            sortExtractor = extractor;
+          });
 
-  Iterable<InfoReply_Info> get sortedInfos => sortByExtractor == null
-      ? widget.infos
-      : (widget.infos.toList()
-        ..sort((a, b) =>
-            (sortIndex != null ? 1 : 0) *
-            (sortAscending ? 1 : -1) *
-            sortByExtractor!(a).compareTo(sortByExtractor!(b))));
+  late final _headers = {
+    const DataColumn2(label: Text('Name')): (InfoReply_Info info) => info.name,
+    const DataColumn2(label: Text('State'), fixedWidth: 130):
+        (InfoReply_Info info) => info.instanceStatus.status.name,
+    const DataColumn2(label: Text('Memory usage'), numeric: true): null,
+    const DataColumn2(label: Text('Disk usage'), numeric: true): null,
+    const DataColumn2(label: Text('CPU(s)'), size: ColumnSize.S, numeric: true):
+        (InfoReply_Info info) => info.cpuCount,
+    const DataColumn2(label: Text('Load'), numeric: true): null,
+    const DataColumn2(label: Text('Image'), size: ColumnSize.L):
+        (InfoReply_Info info) => info.currentRelease,
+    const DataColumn2(label: Text('Ipv4')): (InfoReply_Info info) =>
+        info.ipv4.firstOrNull ?? '',
+    const DataColumn2(label: Text('Actions'), fixedWidth: 75, numeric: true):
+        null,
+  }.entries.map((e) => DataColumn2(
+        label: e.key.label,
+        size: e.key.size,
+        numeric: e.key.numeric,
+        fixedWidth: e.key.fixedWidth,
+        onSort: e.value == null ? null : _extractorToSortCallback(e.value),
+      ));
+
+  Iterable<InfoReply_Info> get sortedInfos => sortExtractor == null
+      ? widget.infos ?? []
+      : (widget.infos ?? []).sorted((a, b) =>
+          (sortIndex != null ? 1 : 0) *
+          (sortAscending ? 1 : -1) *
+          sortExtractor!(a).compareTo(sortExtractor!(b)));
+
+  DataRow2 _buildRow(InfoReply_Info info) {
+    return DataRow2(
+      selected: widget.selected.containsKey(info.name),
+      onSelectChanged: (select) => widget.onSelect?.call(
+        select!
+            ? ({...widget.selected}..[info.name] = info)
+            : ({...widget.selected}..remove(info.name)),
+      ),
+      cells: [
+        DataCell(Text(info.name)),
+        DataCell(Text(info.instanceStatus.status.name.toLowerCase())),
+        DataCell(Text(info.memoryUsage.isEmpty
+            ? 'N/A'
+            : '${filesize(info.memoryUsage)}/${filesize(info.memoryTotal)}')),
+        DataCell(Text(info.diskUsage.isEmpty
+            ? 'N/A'
+            : '${filesize(info.diskUsage)}/${filesize(info.diskTotal)}')),
+        DataCell(Text(info.cpuCount.isEmpty ? 'N/A' : info.cpuCount)),
+        DataCell(Text(info.load.isEmpty ? 'N/A' : info.load)),
+        DataCell(Text(info.currentRelease)),
+        DataCell(Text(info.ipv4.firstOrNull ?? 'N/A')),
+        DataCell(InstanceActionsButton(info.instanceStatus.status, info.name)),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final infos = sortedInfos;
+
     return DataTable2(
+      empty: widget.infos == null
+          ? const Align(
+              alignment: Alignment.topCenter,
+              child: LinearProgressIndicator(),
+            )
+          : const Center(child: Text("No instances")),
       sortColumnIndex: sortIndex,
       sortAscending: sortAscending,
-      columns: [
-        for (final e in tableHeaders.entries)
-          DataColumn2(
-            label: Text(e.key),
-            onSort: (index, ascending) => setState(() {
-              sortIndex = (sortIndex == index && ascending) ? null : index;
-              sortAscending = ascending;
-              sortByExtractor = e.value;
-            }),
-          )
-      ],
-      rows: [
-        for (final info in sortedInfos)
-          DataRow2(
-            selected: selected.contains(info.name),
-            onSelectChanged: (select) => setState(() {
-              select! ? selected.add(info.name) : selected.remove(info.name);
-            }),
-            cells: [
-              DataCell(Text(info.name)),
-              DataCell(Text(info.instanceStatus.status.name.toLowerCase())),
-              DataCell(Text(
-                  info.diskUsage.isEmpty ? 'N/A' : filesize(info.diskUsage))),
-              DataCell(Text(
-                  info.diskTotal.isEmpty ? 'N/A' : filesize(info.diskTotal))),
-              DataCell(Text(info.currentRelease)),
-              DataCell(Text(info.ipv4.isEmpty ? 'N/A' : info.ipv4.first)),
-              DataCell(Text(info.mountInfo.mountPaths.isNotEmpty
-                  ? info.mountInfo.mountPaths.first.sourcePath
-                  : '')),
-              DataCell(IconButton(
-                splashRadius: 20,
-                icon: const Icon(Icons.more_vert),
-                onPressed: () => print(info),
-              )),
-            ],
-          )
-      ],
+      onSelectAll: (select) => widget.onSelect?.call(
+        select! ? {for (final info in infos) info.name: info} : {},
+      ),
+      columns: _headers.toList(),
+      rows: infos.map(_buildRow).toList(),
     );
   }
 }
