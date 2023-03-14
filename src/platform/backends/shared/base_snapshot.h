6cf081d2ffdd74a55de5dacfaeb32e3931f62d0c
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-03-14 23:15:12 +0000

    [shared] Add an empty BaseSnapshot implementation

---
+++ src/platform/backends/shared/base_snapshot.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_BASE_SNAPSHOT_H
+#define MULTIPASS_BASE_SNAPSHOT_H
+
+#include <multipass/memory_size.h>
+#include <multipass/snapshot.h>
+
+namespace multipass
+{
+
+class BaseSnapshot : public Snapshot
+{
+public:
+};
+
+} // namespace multipass
+
+#endif // MULTIPASS_BASE_SNAPSHOT_H
