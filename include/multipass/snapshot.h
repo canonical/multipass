02f6ba2e1e5fd8efd20095e5651da18ccf466677
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-03-14 17:25:40 +0000

    [interface] Define a generic Snapshot interface

---
+++ include/multipass/snapshot.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_SNAPSHOT_H
+#define MULTIPASS_SNAPSHOT_H
+
+#include "disabled_copy_move.h"
+#include "virtual_machine.h"
+
+#include <string>
+#include <unordered_map>
+
+class QJsonObject;
+
+namespace multipass
+{
+class MemorySize;
+
+class Snapshot : private DisabledCopyMove
+{
+public:
+    virtual ~Snapshot() = default;
+
+    virtual const std::string& get_name() const = 0;
+    virtual const std::string& get_comment() const = 0;
+    virtual const Snapshot* get_parent() const = 0;
+
+    virtual int get_num_cores() const = 0;
+    virtual MemorySize get_mem_size() const = 0;
+    virtual MemorySize get_disk_space() const = 0;
+    virtual VirtualMachine::State get_state() const = 0;
+    virtual const std::unordered_map<std::string, VMMount>& get_mounts() const = 0;
+    virtual const QJsonObject& get_metadata() const = 0;
+};
+} // namespace multipass
+
+#endif // MULTIPASS_SNAPSHOT_H
