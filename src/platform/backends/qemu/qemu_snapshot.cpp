3659b1088661f16125d6b7c033d21ba084745ba6
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-05-18 20:25:42 +0100

    [qemu] Implement creating a snapshot with qemu-img

--- src/platform/backends/qemu/qemu_snapshot.cpp
+++ src/platform/backends/qemu/qemu_snapshot.cpp
@@ -16,14 +16,34 @@
  */
 
 #include "qemu_snapshot.h"
+#include <multipass/platform.h>
+#include <multipass/process/simple_process_spec.h>
 
-namespace multipass
-{
+namespace mp = multipass;
+namespace mpp = mp::platform;
 
-QemuSnapshot::QemuSnapshot(const std::string& name, const std::string& comment, std::shared_ptr<const Snapshot> parent,
-                           const VMSpecs& specs)
-    : BaseSnapshot(name, comment, std::move(parent), specs)
+namespace
 {
+const auto snapshot_template = QStringLiteral("@%1"); /* avoid colliding with suspension snapshots; prefix with a char
+                                                         that can't be part of the name, to avoid confusion */
 }
 
-} // namespace multipass
+mp::QemuSnapshot::QemuSnapshot(const std::string& name, const std::string& comment,
+                               std::shared_ptr<const Snapshot> parent, const VMSpecs& specs, const QString& image_path)
+    : BaseSnapshot(name, comment, std::move(parent), specs), image_path{image_path}
+{
+    /* TODO@snapshots verify no snapshot with the same tag exists yet (otherwise creation would succeed and then we'd be
+       unable to identify the snapshot by name) */
+    auto process = mpp::make_process(
+        mp::simple_process_spec("qemu-img", // TODO@ricab extract making spec
+                                QStringList{"snapshot", "-c", snapshot_template.arg(get_name().c_str()), image_path}));
+
+    auto process_state = process->execute();
+    if (!process_state.completed_successfully())
+    {
+        throw std::runtime_error(fmt::format("Internal error: qemu-img failed ({}) with output:\n{}",
+                                             process_state.failure_message(), process->read_all_standard_error()));
+    }
+
+    // TODO@ricab consider a dedicated method to shoot the snapshot on the backend
+}
