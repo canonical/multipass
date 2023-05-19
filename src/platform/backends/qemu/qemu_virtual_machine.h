5d6c145d2ddac25696bb7d4b2ba2518267c7c777
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-05-19 18:48:22 +0100

    [vm] Add method to make specific snapshot w/ JSON

--- src/platform/backends/qemu/qemu_virtual_machine.h
+++ src/platform/backends/qemu/qemu_virtual_machine.h
@@ -73,6 +73,7 @@ protected:
     {
     }
 
+    std::shared_ptr<Snapshot> make_specific_snapshot(const QJsonObject& json) override;
     std::shared_ptr<Snapshot> make_specific_snapshot(const std::string& name, const std::string& comment,
                                                      std::shared_ptr<const Snapshot> parent,
                                                      const VMSpecs& specs) override;
