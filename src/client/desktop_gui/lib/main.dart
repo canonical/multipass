9221b9dc5f5dcf7bafa7f925c9bc3d545ed5dacb
Andrei Toterman <andrei.toterman@canonical.com>
2023-06-07 16:19:05 +0200

    [gui] add bare-bones flutter project

---
+++ src/client/desktop_gui/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: MyHomePage()));
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('$_counter')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _counter++),
+      ),
+    );
+  }
+}
