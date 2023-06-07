28ee2688f6620848940f872649b76a6aad36d25c
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-06-07 11:57:14 +0100

    [exception] Fix preamble in new header

--- include/multipass/exceptions/internal_timeout_exception.h
+++ include/multipass/exceptions/internal_timeout_exception.h
@@ -1,6 +1,19 @@
-//
-// Created by ricab on 06-06-2023.
-//
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef MULTIPASS_INTERNAL_TIMEOUT_EXCEPTION_H
 #define MULTIPASS_INTERNAL_TIMEOUT_EXCEPTION_H
