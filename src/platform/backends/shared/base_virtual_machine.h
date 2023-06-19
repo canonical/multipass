e2b5613a82b8665d028a31d48bcc53b98a19e982
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-06-19 20:28:46 +0100

    [vm] Extract making of parent-update rollback

--- src/platform/backends/shared/base_virtual_machine.h
+++ src/platform/backends/shared/base_virtual_machine.h
@@ -103,8 +103,10 @@ private:
     auto make_deleted_head_rollback(const QString& head_path, const bool& wrote_head);
     void deleted_head_rollback_helper(const QString& head_path, const bool& wrote_head,
                                       std::shared_ptr<Snapshot>& old_head);
-    void delete_snapshot_helper(const QDir& snapshot_dir, Snapshot& snapshot);
     void update_parents(const QDir& snapshot_dir, std::shared_ptr<Snapshot>& deleted_parent);
+    auto make_parent_update_rollback(const std::shared_ptr<Snapshot>& deleted_parent,
+                                     std::unordered_map<Snapshot*, QString>& updated_snapshot_paths) const;
+    void delete_snapshot_helper(const QDir& snapshot_dir, Snapshot& snapshot);
 
 private:
     SnapshotMap snapshots;
