dd15b6e88cae321295d0aa6e5e04dd286d38c7e1
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-06-27 18:15:48 +0100

    [exceptions] Group dedicated snapshot exceptions
    Group dedicated snapshot exceptions in a single header file.

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -27,7 +27,7 @@
 #include <multipass/exceptions/image_vault_exceptions.h>
 #include <multipass/exceptions/invalid_memory_size_exception.h>
 #include <multipass/exceptions/not_implemented_on_this_backend_exception.h>
-#include <multipass/exceptions/snapshot_name_taken.h>
+#include <multipass/exceptions/snapshot_exceptions.h>
 #include <multipass/exceptions/sshfs_missing_error.h>
 #include <multipass/exceptions/start_exception.h>
 #include <multipass/format.h>
