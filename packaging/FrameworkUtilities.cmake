cd4a9533c970a90441644181da7569d8cd32c336
Chris Townsend <christopher.townsend@canonical.com>
2023-07-21 15:10:48 -0400

    [packaging/macos] Rework how dependencies are determined for a binary

--- packaging/FrameworkUtilities.cmake
+++ packaging/FrameworkUtilities.cmake
@@ -11,8 +11,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Authored by: Gerry Boland <gerry.boland@canonical.com>
 
 include(GetPrerequisites)
 
@@ -51,33 +49,6 @@ function(get_binary_rpaths binary rpaths_var)
   set(${rpaths_var} ${${rpaths_var}} PARENT_SCOPE)
 endfunction()
 
-#
-# get_binary_rpathed_libraries(BINARY RPATH_LIBS_VAR)
-#
-# Analyses the $BINARY and places a list of library paths relative to the rpath in $RPATH_LIBS_VAR
-#
-function(get_binary_rpathed_libraries binary rpathed_libs_var)
-  find_program(otool_cmd "otool")
-  mark_as_advanced(otool_cmd)
-
-  if(otool_cmd)
-    execute_process(
-      COMMAND "${otool_cmd}" -L "${binary}"
-      OUTPUT_VARIABLE load_cmds_ov
-      )
-    string(REGEX REPLACE "[^\n]+@rpath/([^\n]+) \\(compatibility[^\n]+\n" "@rpath/\\1\n" load_cmds_ov "${load_cmds_ov}")
-    string(REGEX MATCHALL "@rpath/[^\n]+" load_cmds_ov "${load_cmds_ov}")
-    string(REGEX REPLACE "@rpath" "" load_cmds_ov "${load_cmds_ov}")
-    if(load_cmds_ov)
-      foreach(rpath ${load_cmds_ov})
-        gp_append_unique(${rpathed_libs_var} "${rpath}")
-      endforeach()
-    endif()
-  endif()
-
-  set(${rpathed_libs_var} ${${rpathed_libs_var}} PARENT_SCOPE)
-endfunction()
-
 
 #
 # install_frameworks(BINARY FRAMEWORK_DESTINATION)
@@ -92,15 +63,16 @@ endfunction()
 function(install_frameworks binary framework_destination)
   message(STATUS "Attempting to auto-copy all the $rpath linked frameworks of ${binary} into the install directory ${framework_destination}")
   get_binary_rpaths("${binary}" BINARY_RPATHS ${ARGN})
-  get_binary_rpathed_libraries("${binary}" RPATHED_LIBS)
+  get_prerequisites("${binary}" RPATHED_LIBS 1 1 0 "" "${BINARY_RPATHS}")
 
   foreach(rpath ${BINARY_RPATHS})
     foreach(lib ${RPATHED_LIBS})
-        message(STATUS "Checking for ${rpath}${lib}")
-        if(EXISTS "${rpath}${lib}")
-          message(STATUS "copying COMMAND ${CMAKE_COMMAND} -E copy ${rpath}${lib} ${framework_destination}${lib}")
-          execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${rpath}${lib}" "${framework_destination}${lib}")
-        endif()
+      string(REPLACE "@rpath" "" lib "${lib}")
+      message(STATUS "Checking for ${rpath}${lib}")
+      if(EXISTS "${rpath}${lib}")
+        message(STATUS "copying COMMAND ${CMAKE_COMMAND} -E copy ${rpath}${lib} ${framework_destination}${lib}")
+        execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${rpath}${lib}" "${framework_destination}${lib}")
+      endif()
     endforeach()
   endforeach()
 endfunction()
