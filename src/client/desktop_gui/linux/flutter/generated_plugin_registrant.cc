5ffe54ecfda40536bd797ac4bceaaa37ceabfcf7
Andrei Toterman <andrei.toterman@canonical.com>
2023-08-09 18:09:40 +0300

    [gui] add tray icon

--- src/client/desktop_gui/linux/flutter/generated_plugin_registrant.cc
+++ src/client/desktop_gui/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,18 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <screen_retriever/screen_retriever_plugin.h>
+#include <tray_menu/tray_menu_plugin.h>
+#include <window_manager/window_manager_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) screen_retriever_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ScreenRetrieverPlugin");
+  screen_retriever_plugin_register_with_registrar(screen_retriever_registrar);
+  g_autoptr(FlPluginRegistrar) tray_menu_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "TrayMenuPlugin");
+  tray_menu_plugin_register_with_registrar(tray_menu_registrar);
+  g_autoptr(FlPluginRegistrar) window_manager_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "WindowManagerPlugin");
+  window_manager_plugin_register_with_registrar(window_manager_registrar);
 }
