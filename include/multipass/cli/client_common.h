b85bd49a99393e8b6316dc827f2ca6aa6e446477
Andrei Toterman <andrei.toterman@canonical.com>
2023-09-01 19:36:34 +0200

    [client] remove stuff related to autostart

--- include/multipass/cli/client_common.h
+++ include/multipass/cli/client_common.h
@@ -71,7 +71,6 @@ std::string get_server_address();
 std::unique_ptr<SSLCertProvider> get_cert_provider();
 void set_logger();
 void set_logger(multipass::logging::Level verbosity); // full param qualification makes sure msvc is happy
-void pre_setup();
 void post_setup();
 const std::regex yes_answer{"y|yes", std::regex::icase | std::regex::optimize};
 const std::regex no_answer{"n|no", std::regex::icase | std::regex::optimize};
