47c71dcfa95f45a6ba49fdc20447db2f42bd2a6f
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-09-12 21:38:48 +0100

    [settings] Implement snapshot settings keys

--- src/daemon/snapshot_settings_handler.cpp
+++ src/daemon/snapshot_settings_handler.cpp
@@ -17,6 +17,16 @@
 
 #include "snapshot_settings_handler.h"
 
+#include <multipass/constants.h>
+
+#include <QString>
+
+namespace
+{
+constexpr auto name_suffix = "name";
+constexpr auto comment_suffix = "comment";
+} // namespace
+
 multipass::SnapshotSettingsHandler::SnapshotSettingsHandler(
     std::unordered_map<std::string, VirtualMachine::ShPtr>& operative_instances,
     const std::unordered_map<std::string, VirtualMachine::ShPtr>& deleted_instances,
@@ -29,7 +39,17 @@ multipass::SnapshotSettingsHandler::SnapshotSettingsHandler(
 
 std::set<QString> multipass::SnapshotSettingsHandler::keys() const
 {
-    return std::set<QString>{};
+    static const auto key_template = QStringLiteral("%1.%2.%3.%4").arg(daemon_settings_root);
+    std::set<QString> ret;
+
+    const auto& const_operative_instances = operative_instances;
+    for (const auto* instance_map : {&const_operative_instances, &deleted_instances})
+        for (const auto& [vm_name, vm] : *instance_map)
+            for (const auto& snapshot : vm->view_snapshots())
+                for (const auto& suffix : {name_suffix, comment_suffix})
+                    ret.insert(key_template.arg(vm_name.c_str(), snapshot->get_name().c_str(), suffix));
+
+    return ret;
 }
 
 QString multipass::SnapshotSettingsHandler::get(const QString& key) const
