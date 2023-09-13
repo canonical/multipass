3908e63dbd0c8abe3b57cec11ffedebfebddfeeb
Luis Peñaranda <luis.penaranda@canonical.com>
2023-09-13 08:58:24 -0300

    [daemon] Add local.instance.bridged setting.

--- src/daemon/instance_settings_handler.cpp
+++ src/daemon/instance_settings_handler.cpp
@@ -19,6 +19,7 @@
 
 #include <multipass/constants.h>
 #include <multipass/exceptions/invalid_memory_size_exception.h>
+#include <multipass/settings/bool_setting_spec.h>
 
 #include <QRegularExpression>
 #include <QStringList>
@@ -30,6 +31,7 @@ namespace
 constexpr auto cpus_suffix = "cpus";
 constexpr auto mem_suffix = "memory";
 constexpr auto disk_suffix = "disk";
+constexpr auto bridged_suffix = "bridged";
 
 enum class Operation
 {
@@ -46,7 +48,7 @@ QRegularExpression make_key_regex()
 {
     const auto instance_pattern = QStringLiteral("(?<instance>.+)");
     const auto prop_template = QStringLiteral("(?<property>%1)");
-    const auto either_prop = QStringList{cpus_suffix, mem_suffix, disk_suffix}.join("|");
+    const auto either_prop = QStringList{cpus_suffix, mem_suffix, disk_suffix, bridged_suffix}.join("|");
     const auto prop_pattern = prop_template.arg(either_prop);
 
     const auto key_template = QStringLiteral(R"(%1\.%2\.%3)");
@@ -153,6 +155,27 @@ void update_disk(const QString& key, const QString& val, mp::VirtualMachine& ins
     }
 }
 
+bool is_bridged(const mp::VMSpecs& spec)
+{
+    return std::any_of(spec.extra_interfaces.cbegin(), spec.extra_interfaces.cend(),
+                       [](const auto& network) -> bool { return network.auto_mode; });
+}
+
+void update_bridged(const QString& key, const QString& val, mp::VirtualMachine& instance, mp::VMSpecs& spec)
+{
+    auto bridged = mp::BoolSettingSpec{key, "false"}.interpret(val) == "true";
+
+    if (!bridged && is_bridged(spec))
+    {
+        throw mp::InvalidSettingException{key, val, "Bridged interface cannot be removed"};
+    }
+
+    if (bridged)
+    {
+        // TODO: add the interface
+    }
+}
+
 } // namespace
 
 mp::InstanceSettingsException::InstanceSettingsException(const std::string& reason, const std::string& instance,
@@ -180,7 +203,7 @@ std::set<QString> mp::InstanceSettingsHandler::keys() const
 
     std::set<QString> ret;
     for (const auto& item : vm_instance_specs)
-        for (const auto& suffix : {cpus_suffix, mem_suffix, disk_suffix})
+        for (const auto& suffix : {cpus_suffix, mem_suffix, disk_suffix, bridged_suffix})
             ret.insert(key_template.arg(item.first.c_str()).arg(suffix));
 
     return ret;
@@ -191,6 +214,8 @@ QString mp::InstanceSettingsHandler::get(const QString& key) const
     auto [instance_name, property] = parse_key(key);
     const auto& spec = find_spec(instance_name);
 
+    if (property == bridged_suffix)
+        return is_bridged(spec) ? "true" : "false";
     if (property == cpus_suffix)
         return QString::number(spec.num_cores);
     if (property == mem_suffix)
@@ -214,6 +239,10 @@ void mp::InstanceSettingsHandler::set(const QString& key, const QString& val)
 
     if (property == cpus_suffix)
         update_cpus(key, val, instance, spec);
+    else if (property == bridged_suffix)
+    {
+        update_bridged(key, val, instance, spec);
+    }
     else
     {
         auto size = get_memory_size(key, val);
