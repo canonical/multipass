9be80f34453946b343c44b0a697e804828dc3b42
Luis Peñaranda <luis.penaranda@canonical.com>
2023-09-18 09:14:56 -0300

    [lxd] Implement function to add interface.

--- src/platform/backends/lxd/lxd_virtual_machine.h
+++ src/platform/backends/lxd/lxd_virtual_machine.h
@@ -53,6 +53,7 @@ public:
     void update_cpus(int num_cores) override;
     void resize_memory(const MemorySize& new_size) override;
     void resize_disk(const MemorySize& new_size) override;
+    void add_network_interface(int index, const NetworkInterface& net) override;
     std::unique_ptr<MountHandler> make_native_mount_handler(const SSHKeyProvider* ssh_key_provider,
                                                             const std::string& target, const VMMount& mount) override;
 
