1b77a164d42fa23ebc9807eb1ebb080820f8b99b
Luis Peñaranda <luis.penaranda@canonical.com>
2023-09-27 12:07:56 -0300

    [daemon] Configure new bridged interfaces at boot.

--- src/daemon/daemon.h
+++ src/daemon/daemon.h
@@ -146,7 +146,7 @@ private:
     void init_mounts(const std::string& name);
     void stop_mounts(const std::string& name);
     MountHandler::UPtr make_mount(VirtualMachine* vm, const std::string& target, const VMMount& mount);
-    void configure_new_interfaces(VirtualMachine& vm, VMSpecs& specs);
+    void configure_new_interfaces(const std::string& name, VirtualMachine& vm, VMSpecs& specs);
 
     struct AsyncOperationStatus
     {
@@ -181,6 +181,7 @@ private:
     QFuture<void> image_update_future;
     SettingsHandler* instance_mod_handler;
     std::unordered_map<std::string, std::unordered_map<std::string, MountHandler::UPtr>> mounts;
+    std::unordered_map<std::string, std::vector<std::string>> run_at_boot;
 };
 } // namespace multipass
 #endif // MULTIPASS_DAEMON_H
