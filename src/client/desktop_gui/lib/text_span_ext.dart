40f82b6bc5d1d54a3c8dca96d8d505f45c60c342
Andrei Toterman <andrei.toterman@gmail.com>
2023-10-02 16:36:45 +0200

    [gui] add utility extensions for text spans

---
+++ src/client/desktop_gui/lib/text_span_ext.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final _hoveredLinkProvider = StateProvider<TextSpan?>((_) => null);
+
+extension TextSpanFromStringExt on String {
+  TextSpan get span => TextSpan(
+        text: this,
+        style: const TextStyle(color: Colors.black, fontFamily: 'Ubuntu'),
+      );
+}
+
+extension TextSpanFromListExt on List<TextSpan> {
+  TextSpan get spans => TextSpan(
+        children: this,
+        style: const TextStyle(color: Colors.black),
+      );
+}
+
+extension TextSpanExt on TextSpan {
+  static const noStyle = TextStyle();
+
+  TextSpan get bold => TextSpan(
+        text: text,
+        children: children,
+        style: (style ?? noStyle).copyWith(fontWeight: FontWeight.bold),
+      );
+
+  TextSpan size(double size) => TextSpan(
+        text: text,
+        children: children,
+        style: (style ?? noStyle).copyWith(fontSize: size),
+      );
+
+  TextSpan link(WidgetRef ref, VoidCallback callback) => TextSpan(
+        text: text,
+        children: children,
+        style: (style ?? noStyle).copyWith(
+          color: Colors.blue,
+          decoration: ref.watch(_hoveredLinkProvider) == this
+              ? TextDecoration.underline
+              : null,
+        ),
+        recognizer: TapGestureRecognizer()..onTap = callback,
+        onEnter: (_) => ref.read(_hoveredLinkProvider.notifier).state = this,
+        onExit: (_) => ref.invalidate(_hoveredLinkProvider),
+      );
+}
