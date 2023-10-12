fbac81dcbc452a83c594168135ccf3fb9832dadb
Gerorge Liao <george.liao@canonical.com>
2023-10-12 14:51:31 +0200

    Formatted legacy code diff on this branch based on the new .clang-format file.

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -2686,7 +2686,8 @@ void mp::Daemon::clone(const CloneRequest* request, grpc::ServerReaderWriterInte
 {
     try
     {
-        mpl::ClientLogger<CloneReply, CloneRequest> logger{mpl::level_from(request->verbosity_level()), *config->logger,
+        mpl::ClientLogger<CloneReply, CloneRequest> logger{mpl::level_from(request->verbosity_level()),
+                                                           *config->logger,
                                                            server};
 
         const auto& source_name = request->source_name();
