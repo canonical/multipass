65d7a7d6cdc9ad878194a34181a590f393923ee2
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-10-20 19:55:05 +0100

    [daemon] Use new VMMount serialize method

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -378,37 +378,8 @@ QJsonObject vm_spec_to_json(const mp::VMSpecs& specs)
     QJsonArray json_mounts;
     for (const auto& mount : specs.mounts)
     {
-        QJsonObject entry;
-        entry.insert("source_path", QString::fromStdString(mount.second.source_path));
+        auto entry = mount.second.serialize();
         entry.insert("target_path", QString::fromStdString(mount.first));
-
-        QJsonArray uid_mappings;
-
-        for (const auto& map : mount.second.uid_mappings)
-        {
-            QJsonObject map_entry;
-            map_entry.insert("host_uid", map.first);
-            map_entry.insert("instance_uid", map.second);
-
-            uid_mappings.append(map_entry);
-        }
-
-        entry.insert("uid_mappings", uid_mappings);
-
-        QJsonArray gid_mappings;
-
-        for (const auto& map : mount.second.gid_mappings)
-        {
-            QJsonObject map_entry;
-            map_entry.insert("host_gid", map.first);
-            map_entry.insert("instance_gid", map.second);
-
-            gid_mappings.append(map_entry);
-        }
-
-        entry.insert("gid_mappings", gid_mappings);
-
-        entry.insert("mount_type", static_cast<int>(mount.second.mount_type));
         json_mounts.append(entry);
     }
 
