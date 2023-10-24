d79aa304d7fbf5e7778ef3132dfd643549391bd8
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-10-24 17:25:46 +0100

    [cli] Fix missing newlines in `delete` description

--- src/client/cli/cmd/delete.cpp
+++ src/client/cli/cmd/delete.cpp
@@ -88,8 +88,8 @@ QString cmd::Delete::short_help() const
 QString cmd::Delete::description() const
 {
     return QStringLiteral(
-        "Delete instances and snapshots. Instances can be purged immediately or later on,"
-        "with the \"purge\" command. Until they are purged, instances can be recovered"
+        "Delete instances and snapshots. Instances can be purged immediately or later on,\n"
+        "with the \"purge\" command. Until they are purged, instances can be recovered\n"
         "with the \"recover\" command. Snapshots cannot be recovered after deletion and must be purged at once.");
 }
 
