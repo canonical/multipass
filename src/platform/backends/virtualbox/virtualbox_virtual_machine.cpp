428dce37c5765810a321e4f114d304eadb5fe7b9
Ricardo Abreu <ricardo.abreu@canonical.com>
2023-10-28 00:04:30 +0100

    [vbox] Adapt to default implementation in base VM

--- src/platform/backends/virtualbox/virtualbox_virtual_machine.cpp
+++ src/platform/backends/virtualbox/virtualbox_virtual_machine.cpp
@@ -368,17 +368,3 @@ void mp::VirtualBoxVirtualMachine::resize_disk(const MemorySize& new_size)
                                 {"modifyhd", image_path, "--resizebyte", QString::number(new_size.in_bytes())},
                                 "Could not resize image: {}", name);
 }
-
-auto mp::VirtualBoxVirtualMachine::make_specific_snapshot(const std::string& /*snapshot_name*/,
-                                                          const std::string& /*comment*/,
-                                                          const VMSpecs& /*specs*/,
-                                                          std::shared_ptr<Snapshot> /*parent*/)
-    -> std::shared_ptr<Snapshot>
-{
-    throw NotImplementedOnThisBackendException{"Snapshots"}; // TODO@snapshots
-}
-
-auto mp::VirtualBoxVirtualMachine::make_specific_snapshot(const QString& /*filename*/) -> std::shared_ptr<Snapshot>
-{
-    throw NotImplementedOnThisBackendException{"Snapshots"}; // TODO@snapshots
-}
