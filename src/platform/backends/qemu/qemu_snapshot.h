30b03f46f6643710c56193071c5371bfe7422d5d
Gerorge Liao <george.liao@canonical.com>
2023-11-20 15:49:02 +0100

    [snapshot] fix the constness of snapshot classes

--- src/platform/backends/qemu/qemu_snapshot.h
+++ src/platform/backends/qemu/qemu_snapshot.h
@@ -40,7 +40,7 @@ public:
                  QemuVirtualMachine& vm,
                  VirtualMachineDescription& desc);
     QemuSnapshot(const QString& filename, QemuVirtualMachine& vm, VirtualMachineDescription& desc);
-    QemuSnapshot(std::shared_ptr<Snapshot> src_snapshot, QemuVirtualMachine& vm, VirtualMachineDescription& desc);
+    QemuSnapshot(std::shared_ptr<const Snapshot> src_snapshot, QemuVirtualMachine& vm, VirtualMachineDescription& desc);
 
 protected:
     void capture_impl() override;
