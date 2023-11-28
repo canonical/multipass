d97be9d8d4f994d6a62d2c22271f6c7a00c4f80f
sharder996 <sharder996@gmail.com>
2023-11-28 13:52:19 -0600

    [sftp] rename parameter to more accurately reflect use

--- src/sshfs_mount/sftp_server.h
+++ src/sshfs_mount/sftp_server.h
@@ -57,8 +57,8 @@ private:
     sftp_attributes_struct attr_from(const QFileInfo& file_info);
     int mapped_uid_for(const int uid);
     int mapped_gid_for(const int gid);
-    int reverse_uid_for(const int uid, const int rev_uid_if_not_found);
-    int reverse_gid_for(const int gid, const int rev_gid_if_not_found);
+    int reverse_uid_for(const int uid, const int default_id);
+    int reverse_gid_for(const int gid, const int default_id);
 
     int handle_close(sftp_client_message msg);
     int handle_fstat(sftp_client_message msg);
