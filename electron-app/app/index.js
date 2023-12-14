dfce3c0af8d5159debfd6bea7a4d6e02a2e4ba61
Andrei Toterman <andrei.toterman@gmail.com>
2023-12-14 13:46:26 +0100

    remove the electron-app directory
    the GUI is being developed in Flutter, so there's no reason to keep this around

--- electron-app/app/index.js
+++
@@ -1,8 +0,0 @@
-const remote = require('electron').remote
-const main = remote.require('./main.js')
-
-let button = document.createElement('button')
-button.textContent = 'Launch!'
-document.body.appendChild(button)
-
-button.addEventListener('click', () => { main.Launch() }, false)
