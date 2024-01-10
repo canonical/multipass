67b8295a00abd4194ad9218bbc793f0246ffc523
Andrei Toterman <andrei.toterman@canonical.com>
2024-01-10 21:24:36 +0100

    [gui] add label to switch widget

--- src/client/desktop_gui/lib/switch.dart
+++ src/client/desktop_gui/lib/switch.dart
@@ -2,22 +2,32 @@ import 'package:flutter/cupertino.dart';
 
 class Switch extends StatelessWidget {
   final bool value;
-  final void Function(bool)? onChanged;
+  final ValueChanged<bool>? onChanged;
+  final String label;
 
-  const Switch({super.key, required this.value, this.onChanged});
+  const Switch({
+    super.key,
+    required this.value,
+    this.onChanged,
+    this.label = '',
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 25,
-      child: FittedBox(
-        child: CupertinoSwitch(
-          activeColor: CupertinoColors.activeBlue,
-          trackColor: const Color(0xffd9d9d9),
-          value: value,
-          onChanged: onChanged,
+    return Row(children: [
+      SizedBox(
+        height: 25,
+        child: FittedBox(
+          child: CupertinoSwitch(
+            activeColor: CupertinoColors.activeBlue,
+            trackColor: const Color(0xffd9d9d9),
+            value: value,
+            onChanged: onChanged,
+          ),
         ),
       ),
-    );
+      const SizedBox(width: 8),
+      Text(label, style: const TextStyle(fontSize: 16)),
+    ]);
   }
 }
