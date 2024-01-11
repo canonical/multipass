ecf0b5e48bd62068fbfff251c2e33ba7e73c151d
Andrei Toterman <andrei.toterman@canonical.com>
2024-01-11 13:57:43 +0100

    [gui] add help page base widget

---
+++ src/client/desktop_gui/lib/help.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HelpScreen extends StatelessWidget {
+  static const sidebarKey = 'help';
+
+  const HelpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
