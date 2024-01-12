d5cca6083831ec53220c5c88cf16cf5780ec2210
Andrei Toterman <andrei.toterman@canonical.com>
2024-01-12 14:56:11 +0100

    [gui] add virtualization settings page base

--- src/client/desktop_gui/lib/settings/virtualization_settings.dart
+++ src/client/desktop_gui/lib/settings/virtualization_settings.dart
@@ -1,12 +1,46 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class VirtualizationSettings extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../dropdown.dart';
+import '../providers.dart';
+
+final driverProvider = FutureProvider(
+  (ref) => ref.watch(grpcClientProvider).get('local.driver'),
+);
+
+class VirtualizationSettings extends ConsumerWidget {
   const VirtualizationSettings({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final driver = ref.watch(driverProvider).valueOrNull;
+
     return Column(
-      children: List.generate(50, (index) => Text('$index')),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 60),
+        Dropdown(
+          label: 'Driver',
+          value: driver,
+          items: drivers,
+          onChanged: (_) {},
+        ),
+      ],
     );
   }
 }
+
+final drivers = () {
+  if (Platform.isLinux) {
+    return const {'qemu': 'Qemu', 'lxd': 'LXD', 'libvirt': 'Libvirt'};
+  }
+  if (Platform.isMacOS) {
+    return const {'qemu': 'Qemu', 'virtualbox': 'VirtualBox'};
+  }
+  if (Platform.isWindows) {
+    return const {'hyperv': 'Hyper-V', 'virtualbox': 'VirtualBox'};
+  }
+  throw const OSError('Unsupported OS');
+}();
