00408dd3edcc87034085f774d93eac3ad9d08bba
Andrei Toterman <andrei.toterman@canonical.com>
2024-01-18 16:20:24 +0100

    [gui] allow modifying the bridged-network

--- src/client/desktop_gui/lib/settings/virtualization_settings.dart
+++ src/client/desktop_gui/lib/settings/virtualization_settings.dart
@@ -13,6 +13,20 @@ final driverProvider = FutureProvider((ref) {
   return ref.watch(grpcClientProvider).get(driverKey);
 });
 
+const bridgedNetworkKey = 'local.bridged-network';
+
+final bridgedNetworkProvider = FutureProvider((ref) {
+  ref.watch(daemonAvailableProvider);
+  return ref.watch(grpcClientProvider).get(bridgedNetworkKey);
+});
+
+final networksProvider = FutureProvider((ref) {
+  ref.watch(daemonAvailableProvider);
+  return ref
+      .watch(grpcClientProvider)
+      .networks()
+      .then((networks) => networks.map((n) => n.name).toList());
+});
 
 class VirtualizationSettings extends ConsumerWidget {
   const VirtualizationSettings({super.key});
@@ -20,22 +34,32 @@ class VirtualizationSettings extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final driver = ref.watch(driverProvider).valueOrNull;
+    final networks = ref.watch(networksProvider).valueOrNull ?? const [];
+    final bridgedNetwork = ref.watch(bridgedNetworkProvider).valueOrNull;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 60),
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const SizedBox(height: 60),
+      Dropdown(
+        label: 'Driver',
+        value: driver,
+        items: drivers,
+        onChanged: (value) => ref
+            .read(grpcClientProvider)
+            .set(driverKey, value!)
+            .onError((_, __) {}),
+      ),
+      const SizedBox(height: 48),
+      if (networks.isNotEmpty)
         Dropdown(
-          label: 'Driver',
-          value: driver,
-          items: drivers,
+          label: 'Virtual interface',
+          value: networks.contains(bridgedNetwork) ? bridgedNetwork : null,
+          items: Map.fromIterable(networks),
           onChanged: (value) => ref
               .read(grpcClientProvider)
-              .set(driverKey, value!)
+              .set(bridgedNetworkKey, value!)
               .onError((_, __) {}),
         ),
-      ],
-    );
+    ]);
   }
 }
 
