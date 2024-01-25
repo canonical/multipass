ba172178985f84654fd6ffc96914a7450696ff45
Andrei Toterman <andrei.toterman@canonical.com>
2024-01-25 15:11:19 +0100

    [gui] refactor setting pages to use new providers

--- src/client/desktop_gui/lib/settings/general_settings.dart
+++ src/client/desktop_gui/lib/settings/general_settings.dart
@@ -2,33 +2,28 @@ import 'package:basics/basics.dart';
 import 'package:flutter/material.dart' hide Switch;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../providers.dart';
 import '../switch.dart';
 
-final updateProvider = FutureProvider.autoDispose<UpdateInfo>((ref) {
-  return ref.watch(grpcClientProvider).updateInfo();
+final updateProvider = Provider.autoDispose((ref) {
+  ref.watch(grpcClientProvider).updateInfo().then((value) => ref.state = value);
+  return UpdateInfo();
 });
 
-const privilegedMountsKey = 'local.privileged-mounts';
-
-final privilegedMountsProvider = FutureProvider((ref) {
-  ref.watch(daemonAvailableProvider);
-  return ref
-      .watch(grpcClientProvider)
-      .get(privilegedMountsKey)
-      .then((value) => bool.tryParse(value) ?? false);
-});
+final privilegedMountsProvider = daemonSettingProvider(privilegedMountsKey);
 
 class GeneralSettings extends ConsumerWidget {
   const GeneralSettings({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final update = ref.watch(updateProvider).valueOrNull ?? UpdateInfo();
-    final privilegedMounts =
-        ref.watch(privilegedMountsProvider).valueOrNull ?? false;
+    final update = ref.watch(updateProvider);
+    final privilegedMounts = ref.watch(privilegedMountsProvider.select((value) {
+      return value?.toBoolOption.toNullable() ?? false;
+    }));
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -46,10 +41,9 @@ class GeneralSettings extends ConsumerWidget {
         Switch(
           label: 'Allow privileged mounts',
           value: privilegedMounts,
-          onChanged: (value) => ref
-              .read(grpcClientProvider)
-              .set(privilegedMountsKey, value.toString())
-              .onError((_, __) {}),
+          onChanged: (value) {
+            ref.read(privilegedMountsProvider.notifier).set(value.toString());
+          },
         ),
       ],
     );
@@ -63,42 +57,39 @@ class UpdateAvailable extends StatelessWidget {
 
   static final installUrl = Uri.parse('https://multipass.run/install');
 
+  static void launchInstallUrl() => launchUrl(installUrl);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text('Update available', style: TextStyle(fontSize: 16)),
-        const SizedBox(height: 8),
-        Container(
-          color: const Color(0xffF7F7F7),
-          padding: const EdgeInsets.all(12),
-          width: 480,
-          child: Row(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                color: const Color(0xffE95420),
-                height: 48,
-                width: 48,
-                child: SvgPicture.asset('assets/multipass.svg', width: 30),
-              ),
-              const SizedBox(width: 12),
-              Text(
-                'Multipass ${updateInfo.version}\nis available',
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(width: 12),
-              const Spacer(),
-              TextButton(
-                onPressed: () => launchUrl(installUrl),
-                child: const Text('Upgrade now'),
-              ),
-            ],
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text('Update available', style: TextStyle(fontSize: 16)),
+      const SizedBox(height: 8),
+      Container(
+        color: const Color(0xffF7F7F7),
+        padding: const EdgeInsets.all(12),
+        width: 480,
+        child: Row(children: [
+          Container(
+            alignment: Alignment.center,
+            color: const Color(0xffE95420),
+            height: 48,
+            width: 48,
+            child: SvgPicture.asset('assets/multipass.svg', width: 30),
           ),
-        ),
-        const SizedBox(height: 28),
-      ],
-    );
+          const SizedBox(width: 12),
+          Text(
+            'Multipass ${updateInfo.version}\nis available',
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(width: 12),
+          const Spacer(),
+          const TextButton(
+            onPressed: launchInstallUrl,
+            child: Text('Upgrade now'),
+          ),
+        ]),
+      ),
+      const SizedBox(height: 28),
+    ]);
   }
 }
