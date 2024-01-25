34627d80fc888072c8c3e507c603648e01d50072
Andrei Toterman <andrei.toterman@canonical.com>
2024-01-25 15:18:08 +0100

    [gui] add shared prefs dep

--- src/client/desktop_gui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/client/desktop_gui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import hotkey_manager
 import path_provider_foundation
 import screen_retriever
+import shared_preferences_foundation
 import tray_menu
 import url_launcher_macos
 import window_manager
@@ -16,6 +17,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   HotkeyManagerPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayMenuPlugin.register(with: registry.registrar(forPlugin: "TrayMenuPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
