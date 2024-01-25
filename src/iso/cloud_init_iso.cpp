f97695594c71fd3c6810c6b587cc6b3e07b381b9
Gerorge Liao <george.liao@canonical.com>
2024-01-25 16:55:36 +0100

    [cloud init] small change to integer literal to align with the legacy code.

--- src/iso/cloud_init_iso.cpp
+++ src/iso/cloud_init_iso.cpp
@@ -106,7 +106,7 @@ std::array<uint8_t, 4> to_lsb(uint32_t value)
 // std::array<uint8_t, 4> -> std::span<uint8_t, 4> when c++20 arrives
 uint32_t from_lsb(const std::array<uint8_t, 4>& bytes)
 {
-    return to_u32(bytes[0]) | to_u32(bytes[1]) << 8 | to_u32(bytes[2]) << 16 | to_u32(bytes[3]) << 24;
+    return to_u32(bytes[0]) | to_u32(bytes[1]) << 8u | to_u32(bytes[2]) << 16u | to_u32(bytes[3]) << 24u;
 }
 
 template <typename T, typename SizeType, typename V>
@@ -570,7 +570,7 @@ void mp::CloudInitIso::read_from(const std::filesystem::path& fs_path)
         throw std::runtime_error("The Joliet descriptor is not in place. ");
     }
 
-    const std::array<uint8_t, 5> volume_identifier = readBytesToArray<5>(iso_file, joliet_des_start_pos + 1);
+    const std::array<uint8_t, 5> volume_identifier = readBytesToArray<5>(iso_file, joliet_des_start_pos + 1u);
     if (std::string_view(reinterpret_cast<const char*>(volume_identifier.data()), volume_identifier.size()) != "CD001")
     {
         throw std::runtime_error("The Joliet descriptor is malformed. ");
@@ -587,10 +587,10 @@ void mp::CloudInitIso::read_from(const std::filesystem::path& fs_path)
     // Use std::span when C++20 arrives to avoid the copy of the std::array<uint8_t, 4>
     std::array<uint8_t, 4> root_dir_record_data_location_lsb_bytes;
     // location lsb bytes starts from 2
-    std::copy_n(root_dir_record_data.cbegin() + 2, 4, root_dir_record_data_location_lsb_bytes.begin());
+    std::copy_n(root_dir_record_data.cbegin() + 2u, 4, root_dir_record_data_location_lsb_bytes.begin());
     const uint32_t root_dir_record_data_location_by_blocks = from_lsb(root_dir_record_data_location_lsb_bytes);
     const uint32_t file_records_start_pos = root_dir_record_data_location_by_blocks * logical_block_size +
-                                            2 * sizeof(RootDirRecord); // total size of root dir and root dir parent
+                                            2u * sizeof(RootDirRecord); // total size of root dir and root dir parent
 
     uint32_t current_file_record_start_pos = file_records_start_pos;
     while (true)
@@ -604,14 +604,14 @@ void mp::CloudInitIso::read_from(const std::filesystem::path& fs_path)
         // file record holds the size and the location of the extent. In each loop, we first utilize these info to jump
         // to file content and read it. Later on, we jump back to file record and extract the file name.
         const uint32_t file_content_location_by_blocks =
-            from_lsb(readBytesToArray<4>(iso_file, current_file_record_start_pos + 2));
-        const uint32_t file_content_size = from_lsb(readBytesToArray<4>(iso_file, current_file_record_start_pos + 10));
+            from_lsb(readBytesToArray<4>(iso_file, current_file_record_start_pos + 2u));
+        const uint32_t file_content_size = from_lsb(readBytesToArray<4>(iso_file, current_file_record_start_pos + 10u));
         const std::vector<uint8_t> file_content =
             readBytesToVec(iso_file, file_content_location_by_blocks * logical_block_size, file_content_size);
 
-        const uint32_t file_name_length_start_pos = current_file_record_start_pos + 32;
+        const uint32_t file_name_length_start_pos = current_file_record_start_pos + 32u;
         const uint8_t encoded_file_name_length = readSingleByte(iso_file, file_name_length_start_pos);
-        const uint32_t file_name_start_pos = file_name_length_start_pos + 1;
+        const uint32_t file_name_start_pos = file_name_length_start_pos + 1u;
         const std::vector<uint8_t> encoded_file_name =
             readBytesToVec(iso_file, file_name_start_pos, to_u32(encoded_file_name_length));
 
