59fecfd1764b1bf0fa838d2ce4b927d2ba3af4eb
Gerorge Liao <george.liao@canonical.com>
2024-02-02 15:16:40 +0100

    [unit test] added test for read_single_bytes throwing case.

--- tests/test_cloud_init_iso.cpp
+++ tests/test_cloud_init_iso.cpp
@@ -67,6 +67,25 @@ TEST_F(CloudInitIso, reads_iso_file_failed_to_open_file)
                          mpt::match_what(HasSubstr("Failed to open file")));
 }
 
+TEST_F(CloudInitIso, reads_iso_file_failed_to_read_single_bytes)
+{
+    mp::CloudInitIso orignal_iso;
+    orignal_iso.write_to(iso_path);
+
+    const auto [mock_file_ops, _] = mpt::MockFileOps::inject();
+    EXPECT_CALL(*mock_file_ops, is_open(testing::An<const std::ifstream&>())).WillOnce(Return(true));
+    std::ifstream file;
+    file.setstate(std::ios::failbit);
+    EXPECT_CALL(*mock_file_ops, read(testing::An<std::ifstream&>(), testing::A<char*>(), testing::A<std::streamsize>()))
+        .WillOnce(ReturnRef(file));
+
+    // failed on the first readSingleByte call
+    mp::CloudInitIso new_iso;
+    MP_EXPECT_THROW_THAT(new_iso.read_from(std::filesystem::path(iso_path.toStdString())),
+                         std::runtime_error,
+                         mpt::match_what(HasSubstr("Can not read the next byte data from file at")));
+}
+
 TEST_F(CloudInitIso, reads_iso_file_with_random_string_data)
 {
     mp::CloudInitIso orignal_iso;
