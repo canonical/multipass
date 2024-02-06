1d19cca95569e3d8e8b3d99f790b03fe4526251d
Andrei Toterman <andrei.toterman@canonical.com>
2024-02-06 13:19:15 +0100

    [gui] refactor vm actions into enhanced enum and custom widget

--- src/client/desktop_gui/lib/vm_table/bulk_actions.dart
+++ src/client/desktop_gui/lib/vm_table/bulk_actions.dart
@@ -6,60 +6,50 @@ import '../providers.dart';
 import '../vm_action.dart';
 import 'vms.dart';
 
+extension on Iterable<Widget> {
+  Iterable<Widget> gap({double? width, double? height}) sync* {
+    final thisIterator = iterator;
+    final gapBox = SizedBox(width: width, height: height);
+    if (thisIterator.moveNext()) {
+      yield thisIterator.current;
+      while (thisIterator.moveNext()) {
+        yield gapBox;
+        yield thisIterator.current;
+      }
+    }
+  }
+}
+
 class BulkActionsBar extends ConsumerWidget {
   const BulkActionsBar({super.key});
-
-  Iterable<VmAction> actions(GrpcClient client, Iterable<String> vmNames) {
-    return [
-      VmAction(name: 'Start', vmNames: vmNames, function: client.start),
-      VmAction(name: 'Stop', vmNames: vmNames, function: client.stop),
-      VmAction(name: 'Suspend', vmNames: vmNames, function: client.suspend),
-      VmAction(name: 'Delete', vmNames: vmNames, function: client.delete),
-    ];
-  }
-
-  Widget buildButton(VmAction action, Set<Status> statuses) {
-    final child = Text(action.name);
-    final onPressed = action.allowedStatuses.intersection(statuses).isNotEmpty
-        ? () => action.function(action.vmNames)
-        : null;
-
-    final button = action.name == 'Delete'
-        ? TextButton(
-            onPressed: onPressed,
-            style: const ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(Color(0xffC7162B)),
-            ),
-            child: child,
-          )
-        : OutlinedButton(
-            onPressed: onPressed,
-            style: ButtonStyle(
-              side: MaterialStateBorderSide.resolveWith(
-                (states) => BorderSide(
-                  color: const Color(0xff333333).withOpacity(
-                    states.contains(MaterialState.disabled) ? 0.5 : 1,
-                  ),
-                ),
-              ),
-            ),
-            child: child,
-          );
-
-    return Container(margin: const EdgeInsets.only(right: 8), child: button);
-  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final client = ref.watch(grpcClientProvider);
-    final vmStatuses = ref.watch(vmStatusesProvider);
     final selectedVms = ref.watch(selectedVmsProvider);
-    final statuses = vmStatuses.asMap().whereKey(selectedVms.contains).values;
+    final statuses = ref
+        .watch(vmStatusesProvider)
+        .asMap()
+        .whereKey(selectedVms.contains)
+        .values
+        .toSet();
 
-    return Row(
-      children: actions(client, selectedVms)
-          .map((action) => buildButton(action, statuses.toSet()))
-          .toList(),
-    );
+    final actions = [
+      (VmAction.start, client.start),
+      (VmAction.stop, client.stop),
+      (VmAction.suspend, client.suspend),
+      (VmAction.delete, client.delete),
+    ];
+
+    final actionButtons = [
+      for (final (action, function) in actions)
+        VmActionButton(
+          action: action,
+          currentStatuses: statuses,
+          function: () => function(selectedVms),
+        ),
+    ];
+
+    return Row(children: actionButtons.gap(width: 8).toList());
   }
 }
