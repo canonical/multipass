660a2fc300fa9ddd93542c256790d7ff6829d4a3
Andrei Toterman <andrei.toterman@canonical.com>
2024-02-23 16:04:09 +0200

    [gui] add more extensive logging

--- src/client/desktop_gui/lib/logger.dart
+++ src/client/desktop_gui/lib/logger.dart
@@ -1,7 +1,33 @@
-import 'package:logger/logger.dart';
+import 'dart:io';
 
-final logger = Logger(
-  output: MultiOutput([
-    ConsoleOutput(),
-  ]),
-);
+import 'package:logger/logger.dart';
+import 'package:path_provider/path_provider.dart';
+
+late final Logger logger;
+
+Future<void> setupLogger() async {
+  final logFilePath = await getApplicationSupportDirectory();
+
+  logger = Logger(
+    printer: PrettyPrinter(
+      colors: false,
+      lineLength: 20,
+      printTime: true,
+      methodCount: 0,
+      levelEmojis: {
+        Level.debug: 'DEBUG',
+        Level.error: 'ERROR',
+        Level.info: 'INFO',
+        Level.warning: 'WARN',
+      },
+      excludePaths: [
+        'dart:',
+        'package:flutter',
+      ],
+    ),
+    output: MultiOutput([
+      ConsoleOutput(),
+      FileOutput(file: File('${logFilePath.path}/multipass_desktop.log')),
+    ]),
+  );
+}
