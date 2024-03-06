3770fd869d2917e0288d533abc31733bdf3c1cad
Gerorge Liao <george.liao@canonical.com>
2024-03-06 18:56:53 +0100

    [unit test] added unit test empty file path input test for add_extra_interfaces_to_cloud_init

--- tests/test_base_virtual_machine.cpp
+++ tests/test_base_virtual_machine.cpp
@@ -1174,4 +1174,16 @@ TEST_F(BaseVM, rollsbackFailedRestore)
     EXPECT_EQ(vm.take_snapshot(current_specs, "", "")->get_parent().get(), &last_snapshot);
 }
 
+TEST(BaseVMStub, addExtraInterfacesToCloudInit)
+{
+    StubBaseVirtualMachine base_vm(mp::VirtualMachine::State::off);
+    mpt::TempDir temp_dir;
+    QString iso_path = QDir{temp_dir.path()}.filePath("");
+    const std::string& default_mac_addr = "52:54:00:56:78:90";
+    const std::vector<mp::NetworkInterface> extra_interfaces = {{"id", "52:54:00:56:78:91", true},
+                                                                {"id", "52:54:00:56:78:92", true}};
+    EXPECT_THROW(base_vm.add_extra_interfaces_to_cloud_init(default_mac_addr, extra_interfaces, ""),
+                 std::runtime_error);
+}
+
 } // namespace
