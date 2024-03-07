2f71adb3b7ed94c91df45378b5c946da3e85f0ae
Andrei Toterman <andrei.toterman@canonical.com>
2024-03-07 21:50:10 +0100

    [gui] remove logging filter

--- src/client/desktop_gui/lib/logger.dart
+++ src/client/desktop_gui/lib/logger.dart
@@ -5,10 +5,16 @@ import 'package:path_provider/path_provider.dart';
 
 late final Logger logger;
 
+class NoFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) => true;
+}
+
 Future<void> setupLogger() async {
   final logFilePath = await getApplicationSupportDirectory();
 
   logger = Logger(
+    filter: NoFilter(),
     printer: PrettyPrinter(
       colors: false,
       lineLength: 20,
