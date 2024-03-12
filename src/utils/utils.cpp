e751e1c639c1135f33985b07a28df0bf0400d309
Ricardo Abreu <ricardo.abreu@canonical.com>
2024-03-12 23:22:24 +0000

    [utils] Reduce logging level for ssh cmd failures
    Shell commands can be intended or expected to exit with non-0 code.
    There is no inherent problem in that, so reduce the logging level.

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -283,7 +283,7 @@ std::string mp::utils::run_in_ssh_session(mp::SSHSession& session, const std::st
     if (auto ec = proc.exit_code() != 0)
     {
         auto error_msg = mp::utils::trim_end(proc.read_std_error());
-        mpl::log(mpl::Level::warning, category, fmt::format("failed to run '{}', error message: '{}'", cmd, error_msg));
+        mpl::log(mpl::Level::debug, category, fmt::format("failed to run '{}', error message: '{}'", cmd, error_msg));
         throw mp::SSHExecFailure(error_msg, ec);
     }
 
