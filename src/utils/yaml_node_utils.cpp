bff59ba661aa40a223ad6890deb277e08d53af78
Gerorge Liao <george.liao@canonical.com>
2024-03-13 15:34:38 +0100

    [unit test] restore exitlessSshProcessExceptionDoesNotShowMessage unit test which was accidentally deleted.

--- src/utils/yaml_node_utils.cpp
+++ src/utils/yaml_node_utils.cpp
@@ -13,8 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Authored by: Alberto Aguirre <alberto.aguirre@canonical.com>
- *
  */
 
 #include <multipass/format.h>
