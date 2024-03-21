c6a8a6818adb1ac48d3dc7621d1220f3623d4d5b
Gerorge Liao <george.liao@canonical.com>
2024-03-21 12:02:04 +0100

    [cloud init] added add_extra_interface_to_cloud_init function.

--- include/multipass/cloud_init_iso.h
+++ include/multipass/cloud_init_iso.h
@@ -66,6 +66,9 @@ namespace cloudInitIsoUtils
 void update_cloud_init_with_new_extra_interfaces(const std::string& default_mac_addr,
                                                  const std::vector<NetworkInterface>& extra_interfaces,
                                                  const std::filesystem::path& cloud_init_path);
+void add_extra_interface_to_cloud_init(const std::string& default_mac_addr,
+                                       const NetworkInterface& extra_interfaces,
+                                       const std::filesystem::path& cloud_init_path);
 }
 }
 #endif // MULTIPASS_CLOUD_INIT_ISO_H
