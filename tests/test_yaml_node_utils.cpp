c1d83c8f8afaf38d7a82a54401cd13c5b512531a
Gerorge Liao <george.liao@canonical.com>
2024-03-21 11:11:10 +0100

    [unit test][yaml utils] added some unit tests for add_extra_interface_to_network_config method.

--- tests/test_yaml_node_utils.cpp
+++ tests/test_yaml_node_utils.cpp
@@ -15,6 +15,7 @@
  *
  */
 
+#include <multipass/network_interface.h>
 #include <multipass/yaml_node_utils.h>
 
 #include <gtest/gtest.h>
@@ -40,4 +41,81 @@ cloud-name: multipass)";
     EXPECT_EQ(meta_data_node["instance-id"].as<std::string>(), "vm1");
     EXPECT_EQ(meta_data_node["local-hostname"].as<std::string>(), "vm1");
     EXPECT_EQ(meta_data_node["cloud-name"].as<std::string>(), "multipass");
+}
+
+TEST(UtilsTests, addOneExtraInterfaceNonEmptyNetworkFileContent)
+{
+    constexpr std::string_view original_network_config_file_content = R"(#cloud-config
+version: 2
+ethernets:
+  default:
+    match:
+      macaddress: "52:54:00:51:84:0c"
+    dhcp4: true
+  extra0:
+    match:
+      macaddress: "52:54:00:d8:12:9b"
+    dhcp4: true
+    dhcp4-overrides:
+      route-metric: 200
+    optional: true)";
+
+    constexpr std::string_view expected_new_network_config_file_content = R"(#cloud-config
+version: 2
+ethernets:
+  default:
+    match:
+      macaddress: "52:54:00:51:84:0c"
+    dhcp4: true
+  extra0:
+    match:
+      macaddress: "52:54:00:d8:12:9b"
+    dhcp4: true
+    dhcp4-overrides:
+      route-metric: 200
+    optional: true
+  extra1:
+    match:
+      macaddress: "52:54:00:d8:12:9c"
+    dhcp4: true
+    dhcp4-overrides:
+      route-metric: 200
+    optional: true
+)";
+
+    const mp::NetworkInterface extra_interface{"id", "52:54:00:d8:12:9c", true};
+    const std::string& default_mac_addr = "52:54:00:56:78:90";
+
+    const auto new_network_node =
+        mpu::add_extra_interface_to_network_config(default_mac_addr,
+                                                   extra_interface,
+                                                   std::string(original_network_config_file_content));
+
+    EXPECT_EQ(mpu::emit_cloud_config(new_network_node), expected_new_network_config_file_content);
+}
+
+TEST(UtilsTests, addOneExtraInterfaceEmptyNetworkFileContent)
+{
+    constexpr std::string_view expected_new_network_config_file_content = R"(#cloud-config
+version: 2
+ethernets:
+  default:
+    match:
+      macaddress: "52:54:00:56:78:90"
+    dhcp4: true
+  extra0:
+    match:
+      macaddress: "52:54:00:d8:12:9c"
+    dhcp4: true
+    dhcp4-overrides:
+      route-metric: 200
+    optional: true
+)";
+
+    const mp::NetworkInterface extra_interface{"id", "52:54:00:d8:12:9c", true};
+    const std::string default_mac_addr = "52:54:00:56:78:90";
+
+    const auto new_network_node = mpu::add_extra_interface_to_network_config(default_mac_addr, extra_interface, "");
+
+    EXPECT_EQ(mpu::emit_cloud_config(new_network_node), expected_new_network_config_file_content);
 }
