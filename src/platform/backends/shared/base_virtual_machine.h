ec3841e3856821c0db5281428d6721e91d20228e
Gerorge Liao <george.liao@canonical.com>
2024-03-27 19:19:36 +0100

    [virtual machine] added instance id parameter to make_specific_snapshot function and changed all the corresponding callers.

--- src/platform/backends/shared/base_virtual_machine.h
+++ src/platform/backends/shared/base_virtual_machine.h
@@ -81,6 +81,7 @@ protected:
     virtual std::shared_ptr<Snapshot> make_specific_snapshot(const QString& filename);
     virtual std::shared_ptr<Snapshot> make_specific_snapshot(const std::string& snapshot_name,
                                                              const std::string& comment,
+                                                             const std::string& instance_id,
                                                              const VMSpecs& specs,
                                                              std::shared_ptr<Snapshot> parent);
 
