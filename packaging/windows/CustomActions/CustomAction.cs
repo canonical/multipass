d2ff461f2c2e77cf86e8a19dd27ad8c38d1908a8
Scott Harder <sharder996@gmail.com>
2024-04-11 14:10:59 -0500

    [packaging] don't fail install on hyperv failure and remove C# action

--- packaging/windows/CustomActions/CustomAction.cs
+++ packaging/windows/CustomActions/CustomAction.cs
@@ -6,59 +6,6 @@ namespace CustomActions
     public class CustomActions
     {
         [CustomAction]
-        public static ActionResult CheckHyperV(Session session)
-        {
-            session.Log("Begin CheckHyperV");
-
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = "powershell",
-                Arguments = "Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -ErrorAction SilentlyContinue | Select-Object -ExpandProperty State",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            Process process = Process.Start(psi);
-            process.WaitForExit();
-
-            if (process.ExitCode != 0)
-            {
-                session.Log($"Error checking Hyper-V status: {process.StandardError.ReadToEnd().Trim()}");
-                return ActionResult.Failure;
-            }
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult EnableHyperV(Session session)
-        {
-            session.Log("Begin EnableHyperV");
-
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = "powershell",
-                Arguments = "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -All -NoRestart",
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            Process process = Process.Start(psi);
-            process.WaitForExit();
-
-            if (process.ExitCode != 0)
-            {
-                session.Log($"Error enabling Hyper-V: {process.StandardError.ReadToEnd().Trim()}");
-                return ActionResult.Failure;
-            }
-
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
         public static ActionResult AskRemoveData(Session session)
         {
             session.Log("Begin AskRemoveData");
