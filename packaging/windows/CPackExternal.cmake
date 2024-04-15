30e94adc879ad69c05fe6914a9defd6a04831cc6
Scott Harder <sharder996@gmail.com>
2024-04-15 11:07:27 -0700

    [cpack] use multipass version to appropriately name the installer

--- packaging/windows/CPackExternal.cmake
+++ packaging/windows/CPackExternal.cmake
@@ -37,6 +37,7 @@ execute_process(
     /p:PublishDir=\"${PRIMING_DIR}\"
     /p:OutputPath=\"${CPACK_OUTPUT_FILE_PREFIX}\\packages\"
     /p:BuildVersion=${CPACK_PACKAGE_VERSION}
+    /p:OutputName="${CPACK_PACKAGE_FILE_NAME}"
   WORKING_DIRECTORY "${CPACK_SOURCE_DIR}\\packaging\\windows\\wix"
   OUTPUT_VARIABLE build_output
   RESULT_VARIABLE result)
