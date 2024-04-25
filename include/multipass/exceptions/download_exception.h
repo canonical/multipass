70a1c8de8d1189a437b323f6b605c899af4e29de
Gerorge Liao <george.liao@canonical.com>
2024-04-25 11:03:20 +0200

    [exception] added CRTP BaseQException class and use it in DownloadException.

--- include/multipass/exceptions/download_exception.h
+++ include/multipass/exceptions/download_exception.h
@@ -20,33 +20,17 @@
 
 #include <fmt/format.h>
 
-#include <QException>
-#include <string>
+#include "base_qexception.h"
 
 namespace multipass
 {
-class DownloadException : public QException
+class DownloadException : public BaseQException<DownloadException>
 {
 public:
     DownloadException(const std::string& url, const std::string& cause)
-        : error_string{fmt::format("failed to download from '{}': {}", url, cause)}
+        : BaseQException{fmt::format("failed to download from '{}': {}", url, cause)}
     {
     }
-    void raise() const override
-    {
-        throw *this;
-    }
-    DownloadException* clone() const override
-    {
-        return new DownloadException(*this);
-    }
-    const char* what() const noexcept override
-    {
-        return error_string.c_str();
-    }
-
-private:
-    std::string error_string;
 };
 } // namespace multipass
 #endif // MULTIPASS_DOWNLOAD_H
