501d8a7255994877102f4f789e2b0426b19156a7
Scott Harder <scottharder@Scotts-MacBook-Pro.local>
2024-05-16 12:15:16 +0200

    [packaging] don't fix up qemu binaries if rpath is already included

--- packaging/macos/fixup-qemu-and-deps.sh
+++ packaging/macos/fixup-qemu-and-deps.sh
@@ -5,12 +5,15 @@ set -euo pipefail
 CMAKE_BINARY_DIR=$1
 EXECUTABLE_PATH="@executable_path/../lib"
 
-if ! otool -L ${CMAKE_BINARY_DIR}/bin/qemu-system-* | grep -q ${EXECUTABLE_PATH}; then
+otool_qemu_system=$(otool -L ${CMAKE_BINARY_DIR}/bin/qemu-system-*)
+otool_qemu_img=$(otool -L ${CMAKE_BINARY_DIR}/bin/qemu-img)
+
+if ! echo $otool_qemu_system | grep -q @rpath && ! echo $otool_qemu_system | grep -q ${EXECUTABLE_PATH}; then
     dylibbundler -of -x ${CMAKE_BINARY_DIR}/bin/qemu-system-* -b -d ${CMAKE_BINARY_DIR}/lib/ -p @rpath -i /usr/lib
     install_name_tool -add_rpath "@executable_path/../lib" ${CMAKE_BINARY_DIR}/bin/qemu-system-*
 fi
 
-if ! otool -L "${CMAKE_BINARY_DIR}/bin/qemu-img" | grep -q "${EXECUTABLE_PATH}"; then
+if ! echo $otool_qemu_img | grep -q @rpath && ! echo $otool_qemu_img | grep -q ${EXECUTABLE_PATH}; then
     dylibbundler -of -x ${CMAKE_BINARY_DIR}/bin/qemu-img -b -d ${CMAKE_BINARY_DIR}/lib/ -p @rpath -i /usr/lib
     install_name_tool -add_rpath "@executable_path/../lib" "${CMAKE_BINARY_DIR}/bin/qemu-img"
 fi
