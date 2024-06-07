b8702ca50baae5e7f80c8e2880e64e287260136e
Ricardo Abreu <ricardo.abreu@canonical.com>
2024-06-07 19:28:09 +0100

    [tests] Adapt test to new error message
    And loosen it a little.

--- tests/test_instance_settings_handler.cpp
+++ tests/test_instance_settings_handler.cpp
@@ -520,7 +520,7 @@ TEST_F(TestInstanceSettingsHandler, setRefusesToUnbridge)
 
     MP_EXPECT_THROW_THAT(make_handler().set(make_key(target_instance_name, "bridged"), "false"),
                          mp::InvalidSettingException,
-                         mpt::match_what(HasSubstr("Bridged interface cannot be removed")));
+                         mpt::match_what(HasSubstr("not supported")));
 }
 
 TEST_F(TestInstanceSettingsHandler, setAddsInterface)
