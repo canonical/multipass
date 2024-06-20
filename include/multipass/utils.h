b9ba4199fe02f187a619028b813d35dd56ad8123
Ricardo Abreu <ricardo.abreu@canonical.com>
2024-06-20 20:18:16 +0100

    [utils] Replace template with regular function

--- include/multipass/utils.h
+++ include/multipass/utils.h
@@ -98,18 +98,10 @@ void validate_server_address(const std::string& value);
 bool valid_hostname(const std::string& name_string);
 std::string generate_mac_address();
 bool valid_mac_address(const std::string& mac);
-template <typename NetworkContainer>
-std::optional<NetworkInterfaceInfo> find_bridge_with(const NetworkContainer& networks,
+
+std::optional<NetworkInterfaceInfo> find_bridge_with(const std::vector<NetworkInterfaceInfo>& networks,
                                                      const std::string& target_network,
-                                                     const std::string& bridge_type)
-{
-    const auto it = std::find_if(std::cbegin(networks),
-                                 std::cend(networks),
-                                 [&target_network, &bridge_type](const NetworkInterfaceInfo& info) {
-                                     return info.type == bridge_type && info.has_link(target_network);
-                                 });
-    return it == std::cend(networks) ? std::nullopt : std::make_optional(*it);
-}
+                                                     const std::string& bridge_type);
 
 // string helpers
 bool has_only_digits(const std::string& value);
