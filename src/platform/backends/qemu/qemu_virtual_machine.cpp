96e8820f29e9451698319ef84cfb2dda73037c32
Gerorge Liao <george.liao@canonical.com>
2024-06-24 17:52:15 +0200

    [qemu] remove the state check and added the missing lock on state variable during the previous refactor commit.

--- src/platform/backends/qemu/qemu_virtual_machine.cpp
+++ src/platform/backends/qemu/qemu_virtual_machine.cpp
@@ -371,16 +371,14 @@ void mp::QemuVirtualMachine::shutdown(const bool force)
             mpl::log(mpl::Level::debug, vm_name, "No process to kill");
         }
 
+        std::scoped_lock lock{state_mutex};
         if (state == State::suspended || mp::backend::instance_image_has_snapshot(desc.image.image_path, suspend_tag))
         {
             mpl::log(mpl::Level::info, vm_name, "Deleting suspend image");
             mp::backend::delete_instance_suspend_image(desc.image.image_path, suspend_tag);
         }
 
-        if (state != State::off)
-        {
-            state = State::off;
-        }
+        state = State::off;
     }
     else
     {
