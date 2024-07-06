3f2454823dd4e714af2bd8dfcbe83f40d36e4a1c
Scott Harder <scott.harder@canonical.com>
2024-07-05 19:16:23 -0700

    [qemu] align tests with bridged networks on linux

--- tests/qemu/test_qemu_backend.cpp
+++ tests/qemu/test_qemu_backend.cpp
@@ -801,7 +801,7 @@ TEST_F(QemuBackend, createsQemuSnapshotsFromJsonFile)
     EXPECT_EQ(snapshot->get_parent(), parent);
 }
 
-TEST_F(QemuBackend, lists_no_networks)
+TEST_F(QemuBackend, networks_does_not_throw)
 {
     EXPECT_CALL(*mock_qemu_platform_factory, make_qemu_platform(_)).WillOnce([this](auto...) {
         return std::move(mock_qemu_platform);
@@ -809,7 +809,7 @@ TEST_F(QemuBackend, lists_no_networks)
 
     mp::QemuVirtualMachineFactory backend{data_dir.path()};
 
-    EXPECT_THROW(backend.networks(), mp::NotImplementedOnThisBackendException);
+    EXPECT_NO_THROW(backend.networks());
 }
 
 TEST_F(QemuBackend, remove_resources_for_calls_qemu_platform)
