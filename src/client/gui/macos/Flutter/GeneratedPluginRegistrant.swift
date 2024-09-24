d456c07efd03a650a1797fb47eb50638457a07b2
Andrei Toterman <andrei.toterman@canonical.com>
2024-09-24 11:18:47 +0200

    [gui] add file selector and extended text deps

--- src/client/gui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/client/gui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import hotkey_manager_macos
 import path_provider_foundation
 import screen_retriever
@@ -15,6 +16,7 @@ import window_manager
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
