d46ce1a9a5a0884d05dfa736974abe2e4f83c031
Trevor Shoe <trevor.schupbach@canonical.com>
2024-10-03 17:34:58 -0400

    [daemon] Change permissions for instances to root only

--- src/platform/platform_unix.cpp
+++ src/platform/platform_unix.cpp
@@ -63,6 +63,11 @@ bool mp::platform::Platform::set_permissions(const Path& path, const Perms permi
     return QFile::setPermissions(path, permissions);
 }
 
+bool mp::platform::Platform::set_root_as_owner(const mp::Path& path) const
+{
+    return this->chown(path.toStdString().c_str(), 0, 0) == 0;
+}
+
 bool mp::platform::Platform::symlink(const char* target, const char* link, bool is_dir) const
 {
     return ::symlink(target, link) == 0;
