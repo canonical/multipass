92c5f4025982b448068a01d2350badd274d80034
Trevor Shoe <trevor.schupbach@canonical.com>
2024-10-03 17:34:58 -0400

    [daemon] Change permissions for instances to root only
    # Conflicts:
    #	src/utils/vm_image_vault_utils.cpp

--- src/daemon/default_vm_image_vault.cpp
+++ src/daemon/default_vm_image_vault.cpp
@@ -675,7 +675,9 @@ QString mp::DefaultVMImageVault::extract_image_from(const VMImage& source_image,
                                                     const ProgressMonitor& monitor,
                                                     const mp::Path& dest_dir)
 {
-    MP_UTILS.make_dir(dest_dir);
+    MP_UTILS.make_dir(dest_dir, QFile::ReadOwner | QFile::WriteOwner | QFile::ExeOwner);
+    MP_PLATFORM.set_root_as_owner(dest_dir);
+
     QFileInfo file_info{source_image.image_path};
     const auto image_name = file_info.fileName().remove(".xz");
     const auto image_path = QDir(dest_dir).filePath(image_name);
@@ -685,7 +687,8 @@ QString mp::DefaultVMImageVault::extract_image_from(const VMImage& source_image,
 
 mp::VMImage mp::DefaultVMImageVault::image_instance_from(const VMImage& prepared_image, const mp::Path& dest_dir)
 {
-    MP_UTILS.make_dir(dest_dir);
+    MP_UTILS.make_dir(dest_dir, QFile::ReadOwner | QFile::WriteOwner | QFile::ExeOwner);
+    MP_PLATFORM.set_root_as_owner(dest_dir);
 
     return {MP_IMAGE_VAULT_UTILS.copy_to_dir(prepared_image.image_path, dest_dir),
             prepared_image.id,
