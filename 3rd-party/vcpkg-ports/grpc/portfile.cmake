27c8bf28ea843fa439e34dbbe56c815d8d53fbd6
Gerorge Liao <george.liao@canonical.com>
2024-10-04 10:43:15 +0200

    [3rd-party] added vcpkg and grpc update

--- 3rd-party/vcpkg-ports/grpc/portfile.cmake
+++ 3rd-party/vcpkg-ports/grpc/portfile.cmake
@@ -5,20 +5,33 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO canonical/grpc
-    REF e3acf245a91630fe4d464091ba5446f6a638d82f
-    SHA512 18574197f4a5070de07c39c096ead2175c150a2b790adbb3d9639b0637641015fb91f5cffa916b50863d6ee62203ad2a6964ce87566b6ae7b41716594c445c06
+    REF bfcc6af27db9de9dc0a5edb3bc8ab46731956ba1
+    SHA512 e84e0ec81981ec33adc191fd06ece6e8ebac02c1a0dc612e689809015a171aab276330be952f7b052ad8b213820c0a29f03b5d2f25c2d236b311d0e420efeff8
     HEAD_REF master
     PATCHES
+        00001-fix-uwp.patch
         00002-static-linking-in-linux.patch
         00003-undef-base64-macro.patch
         00004-link-gdi32-on-windows.patch
         00005-fix-uwp-error.patch
-        00006-remove-unused-libraries.patch
-        snprintf.patch
+        00009-use-system-upb.patch
         00015-disable-download-archive.patch
+        00016-fix-plugin-targets.patch
+        00017-abseil.patch
 )
 
-if(NOT TARGET_TRIPLET STREQUAL HOST_TRIPLET)
+# Ensure de-vendoring
+file(REMOVE_RECURSE
+    "${SOURCE_PATH}/third_party/abseil-cpp"
+    "${SOURCE_PATH}/third_party/cares"
+    "${SOURCE_PATH}/third_party/protobuf"
+    "${SOURCE_PATH}/third_party/re2"
+    "${SOURCE_PATH}/third_party/upb"
+    "${SOURCE_PATH}/third_party/utf8_range"
+    "${SOURCE_PATH}/third_party/zlib"
+)
+
+if(VCPKG_CROSSCOMPILING)
     vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/grpc")
 endif()
 
@@ -49,19 +62,12 @@ vcpkg_cmake_configure(
         -DgRPC_PROTOBUF_PROVIDER=package
         -DgRPC_ABSL_PROVIDER=package
         -DgRPC_RE2_PROVIDER=package
-        -DgRPC_PROTOBUF_PACKAGE_TYPE=CONFIG
         -DgRPC_CARES_PROVIDER=${cares_CARES_PROVIDER}
         -DgRPC_BENCHMARK_PROVIDER=none
         -DgRPC_INSTALL_BINDIR:STRING=bin
         -DgRPC_INSTALL_LIBDIR:STRING=lib
         -DgRPC_INSTALL_INCLUDEDIR:STRING=include
         -DgRPC_INSTALL_CMAKEDIR:STRING=share/grpc
-	-DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF
-	-DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF
-	-DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF
-	-DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF
-	-DgRPC_BUILD_GRPC_PYTHON_PLUGIN=OFF
-	-DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF
         "-D_gRPC_PROTOBUF_PROTOC_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf/protoc${VCPKG_HOST_EXECUTABLE_SUFFIX}"
         "-DProtobuf_PROTOC_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf/protoc${VCPKG_HOST_EXECUTABLE_SUFFIX}"
     MAYBE_UNUSED_VARIABLES
@@ -72,11 +78,21 @@ vcpkg_cmake_install(ADD_BIN_TO_PATH)
 
 vcpkg_cmake_config_fixup()
 
-vcpkg_copy_tools(
-    AUTO_CLEAN
-    TOOL_NAMES
-        grpc_cpp_plugin
-)
+if (gRPC_BUILD_CODEGEN)
+    vcpkg_copy_tools(
+        AUTO_CLEAN
+        TOOL_NAMES
+            grpc_php_plugin
+            grpc_python_plugin
+            grpc_node_plugin
+            grpc_objective_c_plugin
+            grpc_csharp_plugin
+            grpc_cpp_plugin
+            grpc_ruby_plugin
+    )
+else()
+    configure_file("${CMAKE_CURRENT_LIST_DIR}/gRPCTargets-vcpkg-tools.cmake" "${CURRENT_PACKAGES_DIR}/share/grpc/gRPCTargets-vcpkg-tools.cmake" @ONLY)
+endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/debug/include")
 
@@ -84,7 +100,7 @@ vcpkg_copy_pdbs()
 if (VCPKG_TARGET_IS_WINDOWS)
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
 else()
-    vcpkg_fixup_pkgconfig(SKIP_CHECK)
+    vcpkg_fixup_pkgconfig()
 endif()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
