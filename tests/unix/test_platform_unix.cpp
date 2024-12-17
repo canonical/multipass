d6d61cbe78f8f5e80780160780fd34bc596039ff
Trevor Shoe <trevor.schupbach@canonical.com>
2024-12-17 14:50:29 -0500

    [sshfs] Add platform unix tests

--- tests/unix/test_platform_unix.cpp
+++ tests/unix/test_platform_unix.cpp
@@ -21,6 +21,7 @@
 #include <tests/temp_file.h>
 
 #include "mock_libc_functions.h"
+#include "mock_signal_wrapper.h"
 
 #include <multipass/constants.h>
 #include <multipass/format.h>
@@ -153,4 +154,139 @@ TEST_F(TestPlatformUnix, get_total_ram_returns_greater_than_zero)
 {
     // On any real system, there should be some RAM
     EXPECT_GT(MP_PLATFORM.get_total_ram(), 0LL);
+}
+
+void test_sigset_empty(const sigset_t& set)
+{
+    // there is no standard empty check to try a few different signals
+    EXPECT_EQ(sigismember(&set, SIGABRT), 0);
+    EXPECT_EQ(sigismember(&set, SIGALRM), 0);
+    EXPECT_EQ(sigismember(&set, SIGCHLD), 0);
+    EXPECT_EQ(sigismember(&set, SIGINT), 0);
+    EXPECT_EQ(sigismember(&set, SIGSEGV), 0);
+    EXPECT_EQ(sigismember(&set, SIGKILL), 0);
+    EXPECT_EQ(sigismember(&set, SIGQUIT), 0);
+    EXPECT_EQ(sigismember(&set, SIGTERM), 0);
+    EXPECT_EQ(sigismember(&set, SIGUSR1), 0);
+    EXPECT_EQ(sigismember(&set, SIGUSR2), 0);
+}
+
+bool test_sigset_has(const sigset_t& set, const std::vector<int>& sigs)
+{
+    bool good = true;
+    for (auto sig : sigs)
+    {
+        auto has_sig = sigismember(&set, sig) == 1;
+        if (!has_sig)
+            good = false;
+
+        EXPECT_TRUE(has_sig);
+    }
+
+    return good;
+}
+
+TEST_F(TestPlatformUnix, make_sigset_returns_emptyset)
+{
+    auto set = mp::platform::make_sigset({});
+    test_sigset_empty(set);
+}
+
+TEST_F(TestPlatformUnix, make_sigset_makes_sigset)
+{
+    auto set = mp::platform::make_sigset({SIGINT, SIGUSR2});
+
+    // check signals are set
+    test_sigset_has(set, {SIGINT, SIGUSR2});
+
+    // unset set signals
+    sigdelset(&set, SIGUSR2);
+    sigdelset(&set, SIGINT);
+
+    // check other signals aren't set
+    test_sigset_empty(set);
+}
+
+TEST_F(TestPlatformUnix, make_and_block_signals_works)
+{
+    auto [mock_signals, guard] = mpt::MockSignalWrapper::inject<StrictMock>();
+
+    EXPECT_CALL(
+        *mock_signals,
+        mask_signals(SIG_BLOCK, Pointee(Truly([](const auto& set) { return test_sigset_has(set, {SIGINT}); })), _));
+
+    auto set = mp::platform::make_and_block_signals({SIGINT});
+
+    test_sigset_has(set, {SIGINT});
+
+    sigdelset(&set, SIGINT);
+    test_sigset_empty(set);
+}
+
+TEST_F(TestPlatformUnix, make_quit_watchdog_blocks_signals)
+{
+    auto [mock_signals, guard] = mpt::MockSignalWrapper::inject<StrictMock>();
+
+    EXPECT_CALL(*mock_signals,
+                mask_signals(SIG_BLOCK,
+                             Pointee(Truly([](const auto& set) {
+                                 return test_sigset_has(set, {SIGQUIT, SIGTERM, SIGHUP, SIGUSR2});
+                             })),
+                             _));
+
+    mp::platform::make_quit_watchdog(std::chrono::milliseconds{1});
+}
+
+TEST_F(TestPlatformUnix, quit_watchdog_quits_on_condition)
+{
+    auto [mock_signals, guard] = mpt::MockSignalWrapper::inject<StrictMock>();
+
+    EXPECT_CALL(*mock_signals, mask_signals(SIG_BLOCK, _, _));
+    EXPECT_CALL(*mock_signals, wait(_, _)).WillRepeatedly(DoAll(SetArgReferee<1>(SIGUSR2), Return(0)));
+    ON_CALL(*mock_signals, send(pthread_self(), SIGUSR2)).WillByDefault(Return(0));
+
+    auto watchdog = mp::platform::make_quit_watchdog(std::chrono::milliseconds{1});
+    EXPECT_EQ(watchdog([] { return false; }), std::nullopt);
+}
+
+TEST_F(TestPlatformUnix, quit_watchdog_quits_on_signal)
+{
+    auto [mock_signals, guard] = mpt::MockSignalWrapper::inject<StrictMock>();
+
+    EXPECT_CALL(*mock_signals, mask_signals(SIG_BLOCK, _, _));
+    EXPECT_CALL(*mock_signals, wait(_, _))
+        .WillOnce(DoAll(SetArgReferee<1>(SIGUSR2), Return(0)))
+        .WillOnce(DoAll(SetArgReferee<1>(SIGTERM), Return(0)));
+    ON_CALL(*mock_signals, send(pthread_self(), SIGUSR2)).WillByDefault(Return(0));
+
+    auto watchdog = mp::platform::make_quit_watchdog(std::chrono::milliseconds{1});
+    EXPECT_EQ(watchdog([] { return true; }), SIGTERM);
+}
+
+TEST_F(TestPlatformUnix, quit_watchdog_signals_itself_asynchronously)
+{
+    auto [mock_signals, guard] = mpt::MockSignalWrapper::inject<StrictMock>();
+
+    std::atomic<bool> signaled = false;
+    std::atomic<int> times = 0;
+
+    EXPECT_CALL(*mock_signals, mask_signals(SIG_BLOCK, _, _));
+    EXPECT_CALL(*mock_signals, wait(_, _))
+        .WillRepeatedly(DoAll(
+            [&signaled, &times] {
+                while (!signaled.load(std::memory_order_acquire))
+                {
+                }
+                times.fetch_add(1, std::memory_order_release);
+                signaled.store(false, std::memory_order_release);
+            },
+            SetArgReferee<1>(SIGUSR2),
+            Return(0)));
+
+    EXPECT_CALL(*mock_signals, send(pthread_self(), SIGUSR2))
+        .WillRepeatedly(DoAll([&signaled] { signaled.store(true, std::memory_order_release); }, Return(0)));
+
+    auto watchdog = mp::platform::make_quit_watchdog(std::chrono::milliseconds{1});
+    EXPECT_EQ(watchdog([&times] { return times.load(std::memory_order_acquire) < 10; }), std::nullopt);
+    EXPECT_GE(times.load(std::memory_order_acquire), 10);
 }
