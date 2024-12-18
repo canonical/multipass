2951a8e2087a9664d3186b38ce0becbcaed5d8de
Scott Harder <scott.harder@canonical.com>
2024-12-18 12:00:37 -0600

    [cmake] sync cmake req with project level cmake

--- packaging/macos/post_build.cmake
+++ packaging/macos/post_build.cmake
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-cmake_minimum_required(VERSION 3.19)
+cmake_minimum_required(VERSION 3.20)
 
 foreach(PACKAGE IN LISTS CPACK_PACKAGE_FILES)
     cmake_path(GET PACKAGE PARENT_PATH PACKAGE_DIR)
