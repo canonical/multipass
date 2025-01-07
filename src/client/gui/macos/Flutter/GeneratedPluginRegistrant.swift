07e215aa2e48e9e371d87fedb7db044cc0267931
Andrei Toterman <andrei.toterman@canonical.com>
2025-01-07 17:26:40 +0100

    upgrade flutter dependencies

--- src/client/gui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/client/gui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import file_selector_macos
 import hotkey_manager_macos
 import local_notifier
 import path_provider_foundation
-import screen_retriever
+import screen_retriever_macos
 import shared_preferences_foundation
 import tray_menu
 import url_launcher_macos
@@ -21,7 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   LocalNotifierPlugin.register(with: registry.registrar(forPlugin: "LocalNotifierPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayMenuPlugin.register(with: registry.registrar(forPlugin: "TrayMenuPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
--- src/client/gui/windows/flutter/generated_plugin_registrant.cc
+++ src/client/gui/windows/flutter/generated_plugin_registrant.cc
@@ -9,7 +9,7 @@
 #include <file_selector_windows/file_selector_windows.h>
 #include <hotkey_manager_windows/hotkey_manager_windows_plugin_c_api.h>
 #include <local_notifier/local_notifier_plugin.h>
-#include <screen_retriever/screen_retriever_plugin.h>
+#include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
 #include <tray_menu/tray_menu_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_manager/window_manager_plugin.h>
@@ -22,8 +22,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("HotkeyManagerWindowsPluginCApi"));
   LocalNotifierPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("LocalNotifierPlugin"));
-  ScreenRetrieverPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
+  ScreenRetrieverWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenRetrieverWindowsPluginCApi"));
   TrayMenuPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("TrayMenuPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
