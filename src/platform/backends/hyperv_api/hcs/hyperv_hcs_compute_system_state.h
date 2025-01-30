6962d6f96aabf68128b59fd814d66f0ded45ed72
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-01-30 22:49:16 +0300

    HCS/HCN API wrapper implementation [wip]

---
+++ src/platform/backends/hyperv_api/hcs/hyperv_hcs_compute_system_state.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_HYPERV_API_HCS_CREATE_COMPUTE_SYSTEM_STATE_H
+#define MULTIPASS_HYPERV_API_HCS_CREATE_COMPUTE_SYSTEM_STATE_H
+
+#include <algorithm>
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+
+namespace multipass::hyperv::hcs
+{
+
+/**
+ * Enum values representing a compute system's possible state
+ *
+ * @ref https://learn.microsoft.com/en-us/virtualization/api/hcs/schemareference#State
+ */
+enum class ComputeSystemState : std::uint8_t
+{
+    created,
+    running,
+    paused,
+    stopped,
+    saved_as_template,
+    unknown
+};
+
+inline ComputeSystemState compute_system_state_from_string(std::string str)
+{
+    std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) { return std::tolower(c); });
+    // std::unordered_map
+    static const std::unordered_map<std::string, ComputeSystemState> translation_map{
+        {"created", ComputeSystemState::created},
+        {"running", ComputeSystemState::running},
+        {"paused", ComputeSystemState::paused},
+        {"stopped", ComputeSystemState::stopped},
+        {"savedastemplate", ComputeSystemState::saved_as_template},
+        {"unknown", ComputeSystemState::unknown}};
+
+    const auto itr = translation_map.find(str);
+    if (translation_map.end() == itr)
+    {
+        throw std::domain_error{""};
+    }
+
+    return itr->second;
+}
+
+} // namespace multipass::hyperv::hcs
+
+#endif
