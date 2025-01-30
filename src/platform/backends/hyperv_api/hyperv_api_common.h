9c9518880c87c5128f60f45c2a46e580ec3ebe6e
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-01-30 22:49:16 +0300

    HCS/HCN API wrapper implementation [wip]

---
+++ src/platform/backends/hyperv_api/hyperv_api_common.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_HYPERV_API_COMMON_H
+#define MULTIPASS_HYPERV_API_COMMON_H
+
+#include <string>
+#include <windows.h>
+
+namespace multipass::hyperv
+{
+
+/**
+ * Parse given GUID string into a GUID struct.
+ *
+ * @param guid_wstr GUID in string form, either 36 characters
+ *                  (without braces) or 38 characters (with braces.)
+ *
+ * @return GUID The parsed GUID
+ */
+auto guid_from_string(const std::wstring& guid_wstr) -> GUID;
+
+// ---------------------------------------------------------
+
+/**
+ * Convert a multi-byte string to a wide-character string.
+ *
+ * @param str Multi-byte string
+ * @return Wide-character equivalent of the given multi-byte string.
+ */
+auto string_to_wstring(const std::string& str) -> std::wstring;
+
+// ---------------------------------------------------------
+
+/**
+ * Parse given GUID string into a GUID struct.
+ *
+ * @param guid_str GUID in string form, either 36 characters
+ *                  (without braces) or 38 characters (with braces.)
+ *
+ * @return GUID The parsed GUID
+ */
+auto guid_from_string(const std::string& guid_str) -> GUID;
+
+} // namespace multipass::hyperv
+
+#endif
