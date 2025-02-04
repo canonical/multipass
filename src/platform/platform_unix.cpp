d9044d10a750e8091640ccc004390dd39dfd71ef
Gerorge Liao <george.liao@canonical.com>
2025-02-04 17:34:12 +0100

    [platform] added snap root cert file path

--- src/platform/platform_unix.cpp
+++ src/platform/platform_unix.cpp
@@ -18,6 +18,7 @@
 #include <multipass/format.h>
 #include <multipass/platform.h>
 #include <multipass/platform_unix.h>
+#include <multipass/snap_utils.h>
 #include <multipass/timer.h>
 #include <multipass/utils.h>
 
@@ -244,5 +245,10 @@ long long mp::platform::Platform::get_total_ram() const
 
 std::filesystem::path mp::platform::Platform::get_root_cert_path() const
 {
-    return {"/usr/local/share/ca-certificates/multipass_root_cert.pem"};
+    constexpr auto* root_cert_file_name = "multipass_root_cert.pem";
+
+    return mp::utils::in_multipass_snap()
+               ? std::filesystem::path{QString{mp::utils::snap_common_dir()}.toStdString().c_str()} /
+                     "data/multipassd/certificates" / root_cert_file_name
+               : std::filesystem::path{"/usr/local/share/ca-certificates"} / root_cert_file_name;
 }
