ce7095466ff5602fe39011c74f019055d986813c
Gerorge Liao <george.liao@canonical.com>
2025-02-11 23:41:59 +0100

    [platform][macos] fixed the root certificate sharing directory.
    /System/Library/Keychains/ is not writable even for root user.

--- src/platform/platform_osx.cpp
+++ src/platform/platform_osx.cpp
@@ -417,5 +417,5 @@ std::string mp::platform::reinterpret_interface_id(const std::string& ux_id)
 
 std::filesystem::path mp::platform::Platform::get_root_cert_path() const
 {
-    return std::filesystem::path{"/System/Library/Keychains/multipass_root_cert"};
+    return std::filesystem::path{"/Library/Keychains/multipass_root_cert.pem"};
 }
