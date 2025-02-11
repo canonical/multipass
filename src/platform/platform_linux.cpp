ed9111c6907c44217fb9ee9689bdda689ad9b196
Gerorge Liao <george.liao@canonical.com>
2025-02-11 15:14:59 +0100

    [platform][linux] fix the snap path mistake.

--- src/platform/platform_linux.cpp
+++ src/platform/platform_linux.cpp
@@ -183,8 +183,9 @@ std::string get_alias_script_path(const std::string& alias)
 std::filesystem::path multipass_final_storage_location()
 {
     const auto user_specified_mp_storage = MP_PLATFORM.multipass_storage_location();
-    const auto mp_final_storage = user_specified_mp_storage.isEmpty() ? mp::utils::snap_common_dir() + "data/multipassd"
-                                                                      : user_specified_mp_storage;
+    const auto mp_final_storage = user_specified_mp_storage.isEmpty()
+                                      ? mp::utils::snap_common_dir() + "/data/multipassd"
+                                      : user_specified_mp_storage;
     return std::filesystem::path{mp_final_storage.toStdString()};
 }
 } // namespace
