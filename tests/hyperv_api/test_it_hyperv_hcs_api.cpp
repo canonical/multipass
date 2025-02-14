99a4b3988c162cf6290a235630fcfdd9da2010e5
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-02-14 08:53:18 +0300

    [hyperv-hcs] add update_cpu_count to API

--- tests/hyperv_api/test_it_hyperv_hcs_api.cpp
+++ tests/hyperv_api/test_it_hyperv_hcs_api.cpp
@@ -18,6 +18,8 @@
 #include "tests/common.h"
 #include "tests/mock_logger.h"
 
+#include <fmt/xchar.h>
+
 #include <src/platform/backends/hyperv_api/hcs/hyperv_hcs_api_wrapper.h>
 
 namespace multipass::test
@@ -76,7 +78,7 @@ TEST_F(HyperVHCSAPI, enumerate_properties)
     params.vhdx_path = "";
 
     const auto c_result = uut.create_compute_system(params);
-    
+
     ASSERT_TRUE(c_result);
     ASSERT_TRUE(c_result.status_msg.empty());
 
@@ -87,6 +89,47 @@ TEST_F(HyperVHCSAPI, enumerate_properties)
     const auto p_result = uut.get_compute_system_properties(params.name);
     EXPECT_TRUE(p_result);
     std::wprintf(L"%s\n", p_result.status_msg.c_str());
+
+    // const auto e_result = uut.enumerate_all_compute_systems();
+    // EXPECT_TRUE(e_result);
+    // std::wprintf(L"%s\n", e_result.status_msg.c_str());
+
+    const auto d_result = uut.terminate_compute_system(params.name);
+    ASSERT_TRUE(d_result);
+    std::wprintf(L"%s\n", d_result.status_msg.c_str());
+    ASSERT_FALSE(d_result.status_msg.empty());
+}
+
+TEST_F(HyperVHCSAPI, DISABLED_update_cpu_count)
+{
+
+    uut_t uut{};
+
+    hyperv::hcs::CreateComputeSystemParameters params{};
+    params.name = "test";
+    params.memory_size_mb = 1024;
+    params.processor_count = 1;
+    params.cloudinit_iso_path = "";
+    params.vhdx_path = "";
+
+    const auto c_result = uut.create_compute_system(params);
+
+    ASSERT_TRUE(c_result);
+    ASSERT_TRUE(c_result.status_msg.empty());
+
+    const auto s_result = uut.start_compute_system(params.name);
+    ASSERT_TRUE(s_result);
+    ASSERT_TRUE(s_result.status_msg.empty());
+
+    const auto p_result = uut.get_compute_system_properties(params.name);
+    EXPECT_TRUE(p_result);
+    std::wprintf(L"%s\n", p_result.status_msg.c_str());
+
+    const auto u_result = uut.update_cpu_count(params.name, 8);
+    EXPECT_TRUE(u_result);
+    auto v = fmt::format("{}", u_result.code);
+    std::wprintf(L"%s\n", u_result.status_msg.c_str());
+    std::printf("%s \n", v.c_str());
 
     // const auto e_result = uut.enumerate_all_compute_systems();
     // EXPECT_TRUE(e_result);
