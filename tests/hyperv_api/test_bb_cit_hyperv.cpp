b8cdeb8badf99677a18c0865cc22a1849917b968
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-02-18 19:21:37 +0300

    [hyperv-hcn] remove the "IpConfigurations" section from create endpoint settings
    It's a remnant from the original WSL2 traces. It works just as fine without the IpConfigurations settings.

--- tests/hyperv_api/test_bb_cit_hyperv.cpp
+++ tests/hyperv_api/test_bb_cit_hyperv.cpp
@@ -58,7 +58,6 @@ TEST_F(HyperV_ComponentIntegrationTests, spawn_empty_test_vm)
         hyperv::hcn::CreateEndpointParameters endpoint_parameters{};
         endpoint_parameters.network_guid = network_parameters.guid;
         endpoint_parameters.endpoint_guid = "aee79cf9-54d1-4653-81fb-8110db97029f";
-        endpoint_parameters.endpoint_ipvx_addr = "10.99.99.10";
         return endpoint_parameters;
     }();
 
@@ -71,7 +70,104 @@ TEST_F(HyperV_ComponentIntegrationTests, spawn_empty_test_vm)
         return create_disk_parameters;
     }();
 
-    const auto create_vm_parameters = []() {
+    const auto add_endpoint_parameters = [&endpoint_parameters]() {
+        hyperv::hcs::AddEndpointParameters add_endpoint_parameters{};
+        add_endpoint_parameters.endpoint_guid = endpoint_parameters.endpoint_guid;
+        add_endpoint_parameters.target_compute_system_name = "multipass-hyperv-cit-vm";
+        add_endpoint_parameters.nic_mac_address = "00-15-5D-9D-CF-69";
+        return add_endpoint_parameters;
+    }();
+
+    const auto create_vm_parameters = [&add_endpoint_parameters]() {
+        hyperv::hcs::CreateComputeSystemParameters vm_parameters{};
+        vm_parameters.name = "multipass-hyperv-cit-vm";
+        vm_parameters.processor_count = 1;
+        vm_parameters.memory_size_mb = 512;
+        vm_parameters.endpoints.push_back(add_endpoint_parameters);
+        return vm_parameters;
+    }();
+
+    (void)hcs.terminate_compute_system(create_vm_parameters.name);
+
+    // Create the test network
+    {
+        const auto& [status, status_msg] = hcn.create_network(network_parameters);
+        ASSERT_TRUE(status);
+    }
+
+    // Create the test endpoint
+    {
+        const auto& [status, status_msg] = hcn.create_endpoint(endpoint_parameters);
+        ASSERT_TRUE(status);
+    }
+
+    // Create the test VHDX (empty)
+    {
+        const auto& [status, status_msg] = virtdisk.create_virtual_disk(create_disk_parameters);
+        ASSERT_TRUE(status);
+    }
+
+    // Create test VM
+    {
+        const auto& [status, status_msg] = hcs.create_compute_system(create_vm_parameters);
+        ASSERT_TRUE(status);
+    }
+
+    // Start test VM
+    {
+        const auto& [status, status_msg] = hcs.start_compute_system(create_vm_parameters.name);
+        ASSERT_TRUE(status);
+    }
+
+    (void)hcs.terminate_compute_system(create_vm_parameters.name);
+    (void)hcn.delete_endpoint(endpoint_parameters.endpoint_guid);
+    (void)hcn.delete_network(network_parameters.guid);
+}
+
+TEST_F(HyperV_ComponentIntegrationTests, spawn_empty_test_vm_attach_nic_after_boot)
+{
+    hcn_wrapper_t hcn{};
+    hcs_wrapper_t hcs{};
+    virtdisk_wrapper_t virtdisk{};
+    // 10.0. 0.0 to 10.255. 255.255.
+    const auto network_parameters = []() {
+        hyperv::hcn::CreateNetworkParameters network_parameters{};
+        network_parameters.name = "multipass-hyperv-cit";
+        network_parameters.guid = "b4d77a0e-2507-45f0-99aa-c638f3e47486";
+        network_parameters.subnet = "10.99.99.0/24";
+        network_parameters.gateway = "10.99.99.1";
+        return network_parameters;
+    }();
+
+    const auto endpoint_parameters = [&network_parameters]() {
+        hyperv::hcn::CreateEndpointParameters endpoint_parameters{};
+        endpoint_parameters.network_guid = network_parameters.guid;
+        endpoint_parameters.endpoint_guid = "aee79cf9-54d1-4653-81fb-8110db97029f";
+        return endpoint_parameters;
+    }();
+
+    // Remove remnants from previous tests, if any.
+    (void)hcn.delete_endpoint(endpoint_parameters.endpoint_guid);
+    (void)hcn.delete_network(network_parameters.guid);
+
+    const auto temp_path = make_tempfile_path(".vhdx");
+
+    const auto create_disk_parameters = [&temp_path]() {
+        hyperv::virtdisk::CreateVirtualDiskParameters create_disk_parameters{};
+        create_disk_parameters.path = temp_path;
+        create_disk_parameters.size_in_bytes = (1024 * 1024) * 512; // 512 MiB
+        return create_disk_parameters;
+    }();
+
+    const auto add_endpoint_parameters = [&endpoint_parameters]() {
+        hyperv::hcs::AddEndpointParameters add_endpoint_parameters{};
+        add_endpoint_parameters.endpoint_guid = endpoint_parameters.endpoint_guid;
+        add_endpoint_parameters.target_compute_system_name = "multipass-hyperv-cit-vm";
+        add_endpoint_parameters.nic_mac_address = "00-15-5D-9D-CF-69";
+        return add_endpoint_parameters;
+    }();
+
+    const auto create_vm_parameters = [&add_endpoint_parameters]() {
         hyperv::hcs::CreateComputeSystemParameters vm_parameters{};
         vm_parameters.name = "multipass-hyperv-cit-vm";
         vm_parameters.processor_count = 1;
@@ -95,14 +191,6 @@ TEST_F(HyperV_ComponentIntegrationTests, spawn_empty_test_vm)
             GTEST_LOG_(WARNING) << "The test system was already present, terminated it.";
         }
     }
-
-    const auto add_endpoint_parameters = [&create_vm_parameters, &endpoint_parameters]() {
-        hyperv::hcs::AddEndpointParameters add_endpoint_parameters{};
-        add_endpoint_parameters.endpoint_guid = endpoint_parameters.endpoint_guid;
-        add_endpoint_parameters.target_compute_system_name = create_vm_parameters.name;
-        add_endpoint_parameters.nic_mac_address = "00-15-5D-9D-CF-69";
-        return add_endpoint_parameters;
-    }();
 
     // Create the test network
     {
