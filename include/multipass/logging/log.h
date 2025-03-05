514b3486025e202d883abfa804583b5b19bc0891
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-03-05 11:41:03 +0300

    [log] introduce mpl::<level-name>(...) log functions
    Added the following templated log functions: error, warn, info,
    debug,trace.
    
    Added a templated log overload, which takes the log level as a
    template parameter. This function acts as a dispatcher for the
    with/without format calls.
    
    Moved non-templated function declarations to the top.
    
    Added unit tests for the new log level functions.
    
    Prefixed all log() calls with logging:: namespace so log() from math
    library does not get pulled into overload resolution candidates.
    
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

--- include/multipass/logging/log.h
+++ include/multipass/logging/log.h
@@ -30,6 +30,11 @@ namespace multipass
 namespace logging
 {
 
+void log(Level level, CString category, CString message);
+void set_logger(std::shared_ptr<Logger> logger);
+Level get_logging_level();
+Logger* get_logger(); // for tests, don't rely on it lasting
+
 /**
  * Log with formatting support
  *
@@ -54,13 +59,105 @@ constexpr void
 log(Level level, const std::string& category, fmt::format_string<Arg0, Args...> fmt, Arg0&& arg0, Args&&... args)
 {
     const auto formatted_log_msg = fmt::format(fmt, std::forward<Arg0>(arg0), std::forward<Args>(args)...);
-    log(level, category, formatted_log_msg);
+    logging::log(level, category, formatted_log_msg);
 }
 
-void log(Level level, CString category, CString message);
-void set_logger(std::shared_ptr<Logger> logger);
-Level get_logging_level();
-Logger* get_logger(); // for tests, don't rely on it lasting
+/**
+ * Log function with templated log level.
+ *
+ * This function acts as an dispatch point for the templated
+ * and non-templated log overloads, based on argument count.
+ * We cannot simply use the templated overload since it requires
+ * at least 1 arguments.
+ *
+ * @tparam level Log level
+ * @tparam Args Format argument types
+ * @param category Log category
+ * @param fmt Format string
+ * @param args Format arguments, if any
+ */
+template <Level level, typename... Args>
+constexpr void log(const std::string& category, fmt::format_string<Args...> fmt, Args&&... args)
+{
+    if constexpr (sizeof...(Args) > 0)
+    {
+        // Needs formatting
+        logging::log(level, category, fmt, std::forward<Args>(args)...);
+        return;
+    }
+    logging::log(level, category, CString{fmt.get().data()});
+}
+
+/**
+ * Log an error message.
+ *
+ * @tparam Args Format argument types
+ * @param category Log category
+ * @param fmt Format string
+ * @param args Format arguments
+ */
+template <typename... Args>
+constexpr void error(const std::string& category, fmt::format_string<Args...> fmt, Args&&... args)
+{
+    logging::log<Level::error>(category, fmt, std::forward<Args>(args)...);
+}
+
+/**
+ * Log a warning message.
+ *
+ * @tparam Args List of types for format arguments
+ * @param category Log category
+ * @param fmt Format string
+ * @param args Format arguments
+ */
+template <typename... Args>
+constexpr void warn(const std::string& category, fmt::format_string<Args...> fmt, Args&&... args)
+{
+    logging::log<Level::warning>(category, fmt, std::forward<Args>(args)...);
+}
+
+/**
+ * Log a info message.
+ *
+ * @tparam Args List of types for format arguments
+ * @param category Log category
+ * @param fmt Format string
+ * @param args Format arguments
+ */
+template <typename... Args>
+constexpr void info(const std::string& category, fmt::format_string<Args...> fmt, Args&&... args)
+{
+    logging::log<Level::info>(category, fmt, std::forward<Args>(args)...);
+}
+
+/**
+ * Log a debug message.
+ *
+ * @tparam Args List of types for format arguments
+ * @param category Log category
+ * @param fmt Format string
+ * @param args Format arguments
+ */
+template <typename... Args>
+constexpr void debug(const std::string& category, fmt::format_string<Args...> fmt, Args&&... args)
+{
+    logging::log<Level::debug>(category, fmt, std::forward<Args>(args)...);
+}
+
+/**
+ * Log a trace message.
+ *
+ * @tparam Args List of types for format arguments
+ * @param category Log category
+ * @param fmt Format string
+ * @param args Format arguments
+ */
+template <typename... Args>
+constexpr void trace(const std::string& category, fmt::format_string<Args...> fmt, Args&&... args)
+{
+    logging::log<Level::trace>(category, fmt, std::forward<Args>(args)...);
+}
+
 } // namespace logging
 } // namespace multipass
 #endif // MULTIPASS_LOG_H
