757a525b5274e4a9ad30a98c9e0b34fbf231d7ff
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-03-07 14:07:22 +0300

    [virtual-machine] add fmt formatter for state enum

--- include/multipass/virtual_machine.h
+++ include/multipass/virtual_machine.h
@@ -34,6 +34,8 @@
 #include <string>
 #include <vector>
 
+#include <fmt/format.h>
+
 namespace multipass
 {
 class MemorySize;
@@ -78,7 +80,7 @@ public:
     virtual std::string ssh_hostname()
     {
         return ssh_hostname(std::chrono::minutes(2));
-    };
+    }
     virtual std::string ssh_hostname(std::chrono::milliseconds timeout) = 0;
     virtual std::string ssh_username() = 0;
     virtual std::string management_ipv4() = 0;
@@ -136,7 +138,7 @@ protected:
     VirtualMachine(VirtualMachine::State state, const std::string& vm_name, const Path& instance_dir)
         : state{state}, vm_name{vm_name}, instance_dir{QDir{instance_dir}} {};
     VirtualMachine(const std::string& vm_name, const Path& instance_dir)
-        : VirtualMachine(State::off, vm_name, instance_dir){};
+        : VirtualMachine(State::off, vm_name, instance_dir) {};
 };
 } // namespace multipass
 
@@ -144,5 +146,55 @@ inline QDir multipass::VirtualMachine::instance_directory() const
 {
     return instance_dir; // TODO this should probably only be known at the level of the base VM
 }
+
+/**
+ * Formatter type specialization for CreateComputeSystemParameters
+ */
+template <typename Char>
+struct fmt::formatter<multipass::VirtualMachine::State, Char>
+{
+    constexpr auto parse(basic_format_parse_context<Char>& ctx)
+    {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(multipass::VirtualMachine::State state, FormatContext& ctx) const
+    {
+        std::string_view v = "(undefined)";
+        switch (state)
+        {
+        case multipass::VirtualMachine::State::off:
+            v = "off";
+            break;
+        case multipass::VirtualMachine::State::stopped:
+            v = "stopped";
+            break;
+        case multipass::VirtualMachine::State::starting:
+            v = "starting";
+            break;
+        case multipass::VirtualMachine::State::restarting:
+            v = "restarting";
+            break;
+        case multipass::VirtualMachine::State::running:
+            v = "running";
+            break;
+        case multipass::VirtualMachine::State::delayed_shutdown:
+            v = "delayed_shutdown";
+            break;
+        case multipass::VirtualMachine::State::suspending:
+            v = "suspending";
+            break;
+        case multipass::VirtualMachine::State::suspended:
+            v = "suspended";
+            break;
+        case multipass::VirtualMachine::State::unknown:
+            v = "unknown";
+            break;
+        }
+
+        return format_to(ctx.out(), "{}", v);
+    }
+};
 
 #endif // MULTIPASS_VIRTUAL_MACHINE_H
