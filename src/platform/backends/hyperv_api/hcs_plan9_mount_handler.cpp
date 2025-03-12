f43cd095e2a881ae778733b7c1c74e5631e6b48c
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-03-12 19:59:07 +0300

    [hyperv-api-vm] Implement Plan9 shares
    Implement the host-side of the Plan9 shares. It needs an agent to be
    running on the guest system, which will be implemented later.
    
    Add support for the graceful shutdown using guest hv_utils linux kernel
    driver, if present.

---
+++ src/platform/backends/hyperv_api/hcs_plan9_mount_handler.cpp
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <hyperv_api/hcs_plan9_mount_handler.h>
+
+#include <hyperv_api/hcs/hyperv_hcs_wrapper_interface.h>
+#include <multipass/ssh/ssh_session.h>
+#include <multipass/utils.h>
+#include <multipass/virtual_machine.h>
+
+#include <fmt/format.h>
+
+namespace multipass::hyperv::hcs
+{
+
+namespace mpu = utils;
+
+constexpr auto kLogCategory = "hcs-plan9-mount-handler";
+
+Plan9MountHandler::Plan9MountHandler(VirtualMachine* vm,
+                                     const SSHKeyProvider* ssh_key_provider,
+                                     VMMount mount_spec,
+                                     const std::string& target,
+                                     hcs_sptr_t hcs_w)
+    : MountHandler(vm, ssh_key_provider, mount_spec, target), hcs{hcs_w}
+{
+    // No need to do anything special.
+    if (nullptr == hcs)
+    {
+        throw std::invalid_argument{"HCS API wrapper object cannot be null."};
+    }
+    if (nullptr == vm)
+    {
+        throw std::invalid_argument{"VM pointer cannot be null."};
+    }
+}
+
+Plan9MountHandler::~Plan9MountHandler() = default;
+
+void Plan9MountHandler::activate_impl(ServerVariant server, std::chrono::milliseconds timeout)
+{
+    const auto params = [this] {
+        hcs::Plan9ShareParameters params{};
+        params.access_name = mpu::make_uuid(target).remove("-").left(30).prepend('m').toStdString();
+        params.name = mpu::make_uuid(target).remove("-").left(30).prepend('m').toStdString();
+        params.host_path = mount_spec.get_source_path();
+        return params;
+    }();
+
+    const auto result = hcs->add_plan9_share(vm->vm_name, params);
+
+    if (!result)
+    {
+        throw std::runtime_error{"Failed to create a Plan9 share for the mount"};
+    }
+
+    try
+    {
+        // The host side 9P share setup is done. Let's handle the guest side.
+        SSHSession session{vm->ssh_hostname(), vm->ssh_port(), vm->ssh_username(), *ssh_key_provider};
+
+        // Split the path in existing and missing parts
+        // We need to create the part of the path which does not still exist, and set then the correct ownership.
+        if (const auto& [leading, missing] = mpu::get_path_split(session, target); missing != ".")
+        {
+            const auto default_uid = std::stoi(MP_UTILS.run_in_ssh_session(session, "id -u"));
+            mpl::log(mpl::Level::debug,
+                     kLogCategory,
+                     fmt::format("{}:{} {}(): `id -u` = {}", __FILE__, __LINE__, __FUNCTION__, default_uid));
+            const auto default_gid = std::stoi(MP_UTILS.run_in_ssh_session(session, "id -g"));
+            mpl::log(mpl::Level::debug,
+                     kLogCategory,
+                     fmt::format("{}:{} {}(): `id -g` = {}", __FILE__, __LINE__, __FUNCTION__, default_gid));
+
+            mpu::make_target_dir(session, leading, missing);
+            mpu::set_owner_for(session, leading, missing, default_uid, default_gid);
+        }
+
+        // fmt::format("sudo mount -t 9p {} {} -o trans=virtio,version=9p2000.L,msize=536870912", tag, target));
+        constexpr std::string_view mount_command_fmtstr =
+            "sudo mount -t 9p -o trans=virtio,version=9p2000.L,port={} {} {}";
+
+        const auto mount_command = fmt::format(mount_command_fmtstr, params.port, params.access_name, target);
+
+        auto mount_command_result = session.exec(mount_command);
+
+        if (mount_command_result.exit_code() == 0)
+        {
+            mpl::info(kLogCategory, "Successfully mounted 9P share `{}` to VM `{}`", params, vm->vm_name);
+        }
+        else
+        {
+            mpl::error(kLogCategory,
+                       "stdout: {} stderr: {}",
+                       mount_command_result.read_std_output(),
+                       mount_command_result.read_std_error());
+            throw std::runtime_error{"Failed to mount the Plan9 share"};
+        }
+    }
+    catch (...)
+    {
+        if (!hcs->remove_plan9_share(vm->vm_name, params))
+        {
+            // TODO: Warn here
+        }
+    }
+}
+void Plan9MountHandler::deactivate_impl(bool force)
+{
+    SSHSession session{vm->ssh_hostname(), vm->ssh_port(), vm->ssh_username(), *ssh_key_provider};
+    constexpr std::string_view umount_command_fmtstr = "mountpoint -q {0}; then sudo umount {0}; else true; fi";
+    const auto umount_command = fmt::format(umount_command_fmtstr, target);
+
+    if (!(session.exec(umount_command).exit_code() == 0))
+    {
+        // TODO: Include output?
+        mpl::warn(kLogCategory, "Plan9 share unmount failed.");
+
+        if (!force)
+        {
+            return;
+        }
+    }
+
+    const auto params = [this] {
+        hcs::Plan9ShareParameters params{};
+        params.access_name = mpu::make_uuid(target).remove("-").left(30).prepend('m').toStdString();
+        params.name = mpu::make_uuid(target).remove("-").left(30).prepend('m').toStdString();
+        params.host_path = mount_spec.get_source_path();
+        return params;
+    }();
+
+    if (!hcs->remove_plan9_share(vm->vm_name, params))
+    {
+        mpl::warn(kLogCategory, "Plan9 share removal failed.");
+    }
+}
+
+// No need for custom active logic.
+
+} // namespace multipass::hyperv::hcs
