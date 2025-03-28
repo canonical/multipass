90099bd4716b2f12caf00a9d1f77ae50f3e56c2e
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-03-28 23:01:49 +0300

    [test_daemon] use mpt::Signal instead of raw condition variable
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

--- tests/test_daemon.cpp
+++ tests/test_daemon.cpp
@@ -38,6 +38,7 @@
 #include "mock_vm_blueprint_provider.h"
 #include "mock_vm_image_vault.h"
 #include "path.h"
+#include "signal.h"
 #include "stub_virtual_machine.h"
 #include "tracking_url_downloader.h"
 
@@ -386,15 +387,12 @@ TEST_F(Daemon, ensure_that_on_restart_future_completes)
         .WillOnce(Return(mp::VirtualMachine::State::stopped));
     EXPECT_CALL(*mock_vm, start).Times(1);
 
-    std::mutex mtx;
-    std::condition_variable cv;
-    bool ready = false;
+    mpt::Signal signal;
     // update_state is called by the finished() handler of the future. If it's called, then
     // everything's ok.
-    EXPECT_CALL(*mock_vm, update_state).WillOnce([&cv, &mtx, &ready] {
+    EXPECT_CALL(*mock_vm, update_state).WillOnce([&signal] {
         // Ensure that update_state is delayed until daemon's destructor call.
-        std::unique_lock lock(mtx);
-        cv.wait(lock, [&] { return ready; });
+        signal.wait();
         // Wait a bit to ensure that daemon's destructor has been run
         std::this_thread::sleep_for(std::chrono::milliseconds{50});
     });
@@ -403,11 +401,7 @@ TEST_F(Daemon, ensure_that_on_restart_future_completes)
 
     {
         mp::Daemon daemon{config_builder.build()};
-        {
-            std::lock_guard lock(mtx);
-            ready = true;
-        }
-        cv.notify_one();
+        signal.signal();
     }
 }
 
