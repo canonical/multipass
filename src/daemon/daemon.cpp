48ec025724b44a093c86053811829a86c504316e
Andrei Toterman <andrei.toterman@canonical.com>
2025-04-01 20:42:45 +0200

    [az] handle az not found in daemon

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -2870,6 +2870,10 @@ try // clang-format on
 
     status_promise->set_value(grpc::Status{});
 }
+catch (const AvailabilityZoneNotFound& e)
+{
+    status_promise->set_value(grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, e.what(), ""));
+}
 catch (const std::exception& e)
 {
     status_promise->set_value(grpc::Status(grpc::StatusCode::FAILED_PRECONDITION, e.what(), ""));
