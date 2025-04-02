77f33e706e3132f5300745eb8f8f20d5eda317af
Andrei Toterman <andrei.toterman@canonical.com>
2025-04-02 14:03:01 +0200

    [az] refactor base az implementation

--- src/platform/backends/shared/base_availability_zone.cpp
+++ src/platform/backends/shared/base_availability_zone.cpp
@@ -15,10 +15,11 @@
  *
  */
 
-#include "multipass/base_availability_zone.h"
-#include "multipass/exceptions/availability_zone_exceptions.h"
-#include "multipass/file_ops.h"
-#include "multipass/logging/log.h"
+#include <multipass/base_availability_zone.h>
+#include <multipass/exceptions/availability_zone_exceptions.h>
+#include <multipass/file_ops.h>
+#include <multipass/json_utils.h>
+#include <multipass/logging/log.h>
 
 #include <fmt/format.h>
 
@@ -26,64 +27,34 @@
 
 namespace multipass
 {
-
 namespace mpl = logging;
 
 constexpr auto subnet_key = "subnet";
 constexpr auto available_key = "available";
 
-BaseAvailabilityZone::data BaseAvailabilityZone::make(const std::string& name, const fs::path& az_directory)
+BaseAvailabilityZone::data BaseAvailabilityZone::read_from_file(const std::string& name, const fs::path& file_path)
 {
-    const auto file_path{az_directory / (name + ".json")};
+    mpl::trace(name, "reading AZ from file '{}'", file_path);
 
-    mpl::log(mpl::Level::info, name, "creating zone");
+    const auto read_json = [&] {
+        try
+        {
+            return MP_JSONUTILS.read_object_from_file(file_path);
+        }
+        catch (std::exception& e)
+        {
+            mpl::debug(name, "failed to read AZ file': {}", e.what());
+            return QJsonObject{};
+        }
+    };
 
-    std::error_code err;
-    const auto file_type = MP_FILEOPS.status(file_path, err).type();
-    if (err && file_type != fs::file_type::not_found)
-    {
-        throw AvailabilityZoneDeserializationError{
-            "AZ file {:?} is not accessible: {}.",
-            file_path.string(),
-            err.message(),
-        };
-    }
-
-    if (file_type == fs::file_type::not_found)
-    {
-        mpl::log(mpl::Level::info, name, fmt::format("AZ file {:?} not found, using defaults", file_path.string()));
-        return {
-            .name = name,
-            .file_path = file_path,
-            // TODO GET ACTUAL SUBNET
-            .subnet = "",
-            .available = true,
-        };
-    }
-
-    if (file_type != fs::file_type::regular)
-        throw AvailabilityZoneDeserializationError{"AZ file {:?} is not a regular file.", file_path.string()};
-
-    mpl::log(mpl::Level::info, name, fmt::format("reading AZ from file {:?}", file_path.string()));
-    const auto file = MP_FILEOPS.open_read(file_path);
-    const auto data = QString::fromStdString(std::string{std::istreambuf_iterator{*file}, {}}).toUtf8();
-    if (file->fail())
-    {
-        throw AvailabilityZoneDeserializationError{
-            "failed to read AZ file {:?}: {}",
-            file_path.string(),
-            std::strerror(errno),
-        };
-    }
-    const auto json = QJsonDocument::fromJson(data).object();
+    const auto json = read_json();
 
     const auto deserialize_subnet = [&] {
-        if (auto json_subnet = json[subnet_key].toString().toStdString(); !json_subnet.empty())
+        if (const auto json_subnet = json[subnet_key].toString().toStdString(); !json_subnet.empty())
             return json_subnet;
 
-        mpl::log(mpl::Level::warning,
-                 name,
-                 fmt::format("subnet missing from AZ file {:?}, using default", file_path.string()));
+        mpl::debug(name, "subnet missing from AZ file '{}', using default", file_path);
         // TODO GET ACTUAL SUBNET
         return std::string{};
     };
@@ -92,22 +63,21 @@ BaseAvailabilityZone::data BaseAvailabilityZone::make(const std::string& name, c
         if (const auto json_available = json[available_key]; json_available.isBool())
             return json_available.toBool();
 
-        mpl::log(mpl::Level::warning,
-                 name,
-                 fmt::format("availability missing from AZ file {:?}, using default", file_path.string()));
+        mpl::debug(name, "availability missing from AZ file '{}', using default", file_path);
         return true;
     };
 
     return {
-        .name = name,
-        .file_path = file_path,
-        .subnet = deserialize_subnet(),
-        .available = deserialize_available(),
+        // TODO remove these comments in C++20
+        /*.name = */ name,
+        /*.file_path = */ file_path,
+        /*.subnet = */ deserialize_subnet(),
+        /*.available = */ deserialize_available(),
     };
 }
 
 BaseAvailabilityZone::BaseAvailabilityZone(const std::string& name, const fs::path& az_directory)
-    : m{make(name, az_directory)}
+    : m{read_from_file(name, az_directory / (name + ".json"))}
 {
     serialize();
 }
@@ -131,11 +101,11 @@ bool BaseAvailabilityZone::is_available() const
 void BaseAvailabilityZone::set_available(const bool new_available)
 {
 
+    mpl::debug(m.name, "making AZ {}available", new_available ? "" : "un");
     const std::unique_lock lock{m.mutex};
     if (m.available == new_available)
         return;
 
-    mpl::log(mpl::Level::info, m.name, fmt::format("making AZ {}available", new_available ? "" : "un"));
     m.available = new_available;
     serialize();
 
@@ -145,15 +115,15 @@ void BaseAvailabilityZone::set_available(const bool new_available)
 
 void BaseAvailabilityZone::add_vm(VirtualMachine& vm)
 {
+    mpl::debug(m.name, "adding vm '{}' to AZ", vm.vm_name);
     const std::unique_lock lock{m.mutex};
-    mpl::log(mpl::Level::info, m.name, fmt::format("adding vm {:?} to AZ", vm.vm_name));
     m.vms.emplace_back(vm);
 }
 
 void BaseAvailabilityZone::remove_vm(VirtualMachine& vm)
 {
+    mpl::debug(m.name, "removing vm '{}' from AZ", vm.vm_name);
     const std::unique_lock lock{m.mutex};
-    mpl::log(mpl::Level::info, m.name, fmt::format("removing vm {:?} from AZ", vm.vm_name));
     // as of now, we use vm names to uniquely identify vms, so we can do the same here
     const auto to_remove = std::remove_if(m.vms.begin(), m.vms.end(), [&](const auto& some_vm) {
         return some_vm.get().vm_name == vm.vm_name;
@@ -163,22 +133,14 @@ void BaseAvailabilityZone::remove_vm(VirtualMachine& vm)
 
 void BaseAvailabilityZone::serialize() const
 {
+    mpl::trace(m.name, "writing AZ to file '{}'", m.file_path);
     const std::unique_lock lock{m.mutex};
-    mpl::log(mpl::Level::info, m.name, fmt::format("writing AZ to file {:?}", m.file_path.string()));
 
     const QJsonObject json{
         {subnet_key, QString::fromStdString(m.subnet)},
         {available_key, m.available},
     };
-    const auto json_bytes = QJsonDocument{json}.toJson();
 
-    if (MP_FILEOPS.open_write(m.file_path)->write(json_bytes.data(), json_bytes.size()).fail())
-    {
-        throw AvailabilityZoneSerializationError{
-            "failed to write AZ file {:?}: {}",
-            m.file_path.string(),
-            std::strerror(errno),
-        };
-    }
+    MP_JSONUTILS.write_json(json, QString::fromStdString(m.file_path.u8string()));
 }
 } // namespace multipass
