200b974253389869b90a74c47848264e2dfb89cf
Gerorge Liao <george.liao@canonical.com>
2025-04-04 11:12:56 +0200

    [daemon] added the deprecation warning to launch command.

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -1564,6 +1564,7 @@ void mp::Daemon::launch(const LaunchRequest* request,
                         std::promise<grpc::Status>* status_promise) // clang-format off
 try // clang-format on
 {
+    lxd_and_libvirt_deprecation_warning(*server); // TODO lxd and libvirt migration, remove
     mpl::ClientLogger<LaunchReply, LaunchRequest> logger{mpl::level_from(request->verbosity_level()), *config->logger,
                                                          server};
 
