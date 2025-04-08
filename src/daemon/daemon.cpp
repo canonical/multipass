0e6b69f95f5b474801169f3d5bcbf2e7f986f38b
Gerorge Liao <george.liao@canonical.com>
2025-04-08 11:51:09 +0200

    [daemon] changed the doc link to the specific page.

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -2923,7 +2923,9 @@ void mp::Daemon::create_vm(const CreateRequest* request,
             "* Warning! Blueprints are deprecated and will be removed in a future release. *\n\n"
             "You can achieve similar effects with cloud-init and other launch options.\n"
             "Run `multipass help launch` for help, or find out more at:\n"
-            "- https://canonical.com/multipass/docs\n"
+            "- "
+            "https://documentation.ubuntu.com/multipass/en/latest/how-to-guides/manage-instances/"
+            "launch-customized-virtual-machines-with-multipass-and-cloud-init/\n"
             "- https://cloudinit.readthedocs.io/en/latest/index.html\n\n";
         CreateReply reply;
         reply.set_log_line(deprecation_warning);
