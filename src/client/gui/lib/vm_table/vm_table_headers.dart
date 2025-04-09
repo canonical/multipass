dd97aa39a3b9b21e5891fc437283a941a4b6046c
levkropp <lev.kropp@canonical.com>
2025-04-09 13:45:33 -0400

    Implement availability zones support in GUI - Add ZONE column to VM instances table
    - Add zone controls in bulk actions and filters
    
    - Add zone selection to instance configuration
    
    - Add Launch & Configure Next functionality

--- src/client/gui/lib/vm_table/vm_table_headers.dart
+++ src/client/gui/lib/vm_table/vm_table_headers.dart
@@ -44,6 +44,16 @@ final headers = <TableHeader<VmInfo>>[
     ),
   ),
   TableHeader(
+    name: 'ZONE',
+    width: 100,
+    minWidth: 70,
+    sortKey: (info) => info.zone.name,
+    cellBuilder: (info) => Text(
+      info.zone.name.nonBreaking,
+      overflow: TextOverflow.ellipsis,
+    ),
+  ),
+  TableHeader(
     name: 'CPU USAGE',
     width: 130,
     minWidth: 100,
