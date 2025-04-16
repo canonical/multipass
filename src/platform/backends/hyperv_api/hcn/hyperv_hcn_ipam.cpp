2c8f17a6eb0dddaf31f6cbe145b20c9ac89130ac
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-04-16 20:52:12 +0300

    [hyperv-hcn] unify formatting for HCN data structures
    Formatting HCN data types with fmt will now yield their JSON representation
    for both `char` and `wchar_t`.
    
    - Added bitwise | operator for HcnNetworkFlags
    - Moved most of the HCN formatter implementations to source
    - Added `universal_string_literal_helper` and `maybe_widen` helpers
    - Added unit tests for format results
    - Added more tests for create_network

---
+++ src/platform/backends/hyperv_api/hcn/hyperv_hcn_ipam.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <hyperv_api/hcn/hyperv_hcn_ipam.h>
+
+#include <hyperv_api/hyperv_api_string_conversion.h>
+
+namespace hv = multipass::hyperv;
+namespace hcn = hv::hcn;
+
+template <typename Char>
+template <typename FormatContext>
+auto fmt::formatter<hcn::HcnIpam, Char>::format(const hcn::HcnIpam& ipam, FormatContext& ctx) const ->
+    typename FormatContext::iterator
+{
+    constexpr static auto subnet_template = MULTIPASS_UNIVERSAL_LITERAL(R"json(
+        {{
+            "Type": "{}",
+            "Subnets": [
+                {}
+            ]
+        }}
+    )json");
+
+    constexpr static auto comma = MULTIPASS_UNIVERSAL_LITERAL(",");
+
+    return format_to(ctx.out(),
+                     subnet_template.as<Char>(),
+                     hv::maybe_widen{ipam.type},
+                     fmt::join(ipam.subnets, comma.as<Char>()));
+}
+
+template auto fmt::formatter<hcn::HcnIpam, char>::format<fmt::format_context>(const hcn::HcnIpam&,
+                                                                              fmt::format_context&) const
+    -> fmt::format_context::iterator;
+
+template auto fmt::formatter<hcn::HcnIpam, wchar_t>::format<fmt::wformat_context>(const hcn::HcnIpam&,
+                                                                                  fmt::wformat_context&) const
+    -> fmt::wformat_context::iterator;
