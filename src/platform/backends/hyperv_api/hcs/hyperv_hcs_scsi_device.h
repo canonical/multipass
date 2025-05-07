d99f2e945ef121ba435b4731e625660335f72f0a
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-05-07 17:35:21 +0300

    [hyperv-hcs] extract SCSI device to its own entity
    - Introduce two new types, HcsScsiDevice & HcsScsiDeviceType
    - Provide proper formatting for the HcsScsiDevice
    - Remove vhdx_path & cloudinit_iso_path variables from create params
    - Introduce scsi_devices paraneter to create params
    - Update the code that refers to the vhdx_path & cloudinit_iso_path

---
+++ src/platform/backends/hyperv_api/hcs/hyperv_hcs_scsi_device.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MULTIPASS_HYPERV_API_HCS_SCSI_DEVICE_H
+#define MULTIPASS_HYPERV_API_HCS_SCSI_DEVICE_H
+
+#include <hyperv_api/hcs/hyperv_hcs_scsi_device_type.h>
+
+#include <fmt/xchar.h>
+
+#include <filesystem>
+#include <string>
+
+namespace multipass::hyperv::hcs
+{
+
+struct HcsScsiDevice
+{
+    HcsScsiDeviceType type;
+    std::string name;
+    std::filesystem::path path;
+    bool read_only;
+};
+
+} // namespace multipass::hyperv::hcs
+
+/**
+ * Formatter type specialization for HcnNetworkPolicy
+ */
+template <typename Char>
+struct fmt::formatter<multipass::hyperv::hcs::HcsScsiDevice, Char> : formatter<basic_string_view<Char>, Char>
+{
+    template <typename FormatContext>
+    auto format(const multipass::hyperv::hcs::HcsScsiDevice& policy, FormatContext& ctx) const ->
+        typename FormatContext::iterator;
+};
+
+#endif // MULTIPASS_HYPERV_API_HCS_SCSI_DEVICE_H
