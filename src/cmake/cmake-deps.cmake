12d17a5bbe6fdddee46b074d704a42c6a26c75e5
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-05-09 16:52:59 +0300

    [out_ptr] switch to P1132 reference implementation
    - update one more location that has been missed

---
+++ src/cmake/cmake-deps.cmake
@@ -0,0 +1,24 @@
+# Copyright (C) Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+include(FetchContent)
+
+# Declare and fetch fmt
+FetchContent_Declare(
+  out_ptr
+  GIT_REPOSITORY https://github.com/soasis/out_ptr.git
+  GIT_TAG 02a577edfcf25e2519e380a95c16743b7e5878a1
+)
+
+FetchContent_MakeAvailable(out_ptr)
