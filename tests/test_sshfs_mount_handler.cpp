a2ed07a31858f16f0b9a6cd6c153b8a24c752d42
Sploder12 <trevorschupbach@gmail.com>
2025-05-12 07:44:03 -0400

    [az] Integrate macOS

--- tests/test_sshfs_mount_handler.cpp
+++ tests/test_sshfs_mount_handler.cpp
@@ -122,9 +122,9 @@ TEST_F(SSHFSMountHandlerTest, mountCreatesSshfsProcess)
     factory->register_callback(sshfs_server_callback(sshfs_prints_connected));
 
     mpt::MockVirtualMachine mock_vm{"my_instance"};
-    EXPECT_CALL(mock_vm, ssh_port()).Times(3);
-    EXPECT_CALL(mock_vm, ssh_hostname()).Times(3);
-    EXPECT_CALL(mock_vm, ssh_username()).Times(3);
+    EXPECT_CALL(mock_vm, ssh_port()).Times(2);
+    EXPECT_CALL(mock_vm, ssh_hostname()).Times(2);
+    EXPECT_CALL(mock_vm, ssh_username()).Times(2);
 
     mp::SSHFSMountHandler sshfs_mount_handler{&mock_vm, &key_provider, target_path, mount};
     sshfs_mount_handler.activate(&server);
