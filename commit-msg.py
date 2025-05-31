7b33a9661594bc7c051c875811cb0f272666cd5d
Ricardo Abreu <ricardo.abreu@canonical.com>
2025-05-31 01:23:44 +0100

    [git] Add function to distinguish merge commits

--- commit-msg.py
+++ commit-msg.py
@@ -2,3 +2,12 @@
 """
 Git commit-msg hook to validate commit messages according to team guidelines.
 """
+
+
+def is_merge_commit(message):
+    """
+    Check if this is a merge commit.
+
+    Commits are identified as merges iff they start with the word "Merge"
+    """
+    return message.startswith("Merge")
