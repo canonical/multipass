01142a1ee91a88e72f75420b9ef97d1f0e320803
Trevor Shoe <trevor.schupbach@canonical.com>
2025-07-16 06:31:25 -0400

    [cert] Ensure there is always access to the root cert

--- src/daemon/daemon_config.cpp
+++ src/daemon/daemon_config.cpp
@@ -133,11 +133,16 @@ std::unique_ptr<const mp::DaemonConfig> mp::DaemonConfigBuilder::build()
     auto multiplexing_logger = std::make_shared<mpl::MultiplexingLogger>(std::move(logger));
     mpl::set_logger(multiplexing_logger);
 
+    MP_UTILS.make_dir(
+        QString::fromStdU16String(MP_PLATFORM.get_root_cert_path().parent_path().u16string()),
+        fs::perms::owner_all | fs::perms::group_exec | fs::perms::others_exec);
+
     MP_PLATFORM.setup_permission_inheritance(true);
 
     auto storage_path = MP_PLATFORM.multipass_storage_location();
     if (!storage_path.isEmpty())
-        MP_UTILS.make_dir(storage_path);
+        MP_UTILS.make_dir(storage_path,
+                          fs::perms::owner_all | fs::perms::group_exec | fs::perms::others_exec);
 
     if (cache_directory.isEmpty())
     {
