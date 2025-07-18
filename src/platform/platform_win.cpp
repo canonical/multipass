646e1e827d7a9ed060f8154f4c74ca4e120c3ed0
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-07-18 12:26:40 +0300

    [hyperv-hcs] implement get_os_supported_schema_version() function
    Implemented get_os_supported_schema_version() to check which HCS schema
    version is supported by the OS. Added a new Windows-specific platform function
    called `get_windows_version()` to retrieve major, minor and build number of
    the OS.
    
    Added schema version to HCSWrapper constructor log.

--- src/platform/platform_win.cpp
+++ src/platform/platform_win.cpp
@@ -67,6 +67,7 @@
 #include <secext.h>
 // clang-format on
 #include <windows.h>
+#include <winternl.h>
 
 #include <algorithm>
 #include <cerrno>
@@ -551,6 +552,49 @@ mp::platform::wsa_init_wrapper::~wsa_init_wrapper()
         WSACleanup();
     }
     delete wsa_data;
+}
+
+auto multipass::platform::get_windows_version() -> std::optional<windows_version>
+{
+    struct HModuleDeleter
+    {
+        void operator()(HMODULE handle)
+        {
+            if (handle)
+            {
+                FreeLibrary(handle);
+            }
+        }
+    };
+
+    using unique_hmodule = std::unique_ptr<std::remove_pointer<HMODULE>::type, HModuleDeleter>;
+    using RtlGetVersionPtr = NTSTATUS(WINAPI*)(PRTL_OSVERSIONINFOW);
+
+    static std::optional<windows_version> cached_version = []() -> std::optional<windows_version> {
+        unique_hmodule hNtdll(LoadLibraryA("ntdll.dll"));
+        if (hNtdll)
+        {
+            RtlGetVersionPtr RtlGetVersion =
+                (RtlGetVersionPtr)GetProcAddress(hNtdll.get(), "RtlGetVersion");
+            if (RtlGetVersion)
+            {
+                // Initialize the version info structure
+                RTL_OSVERSIONINFOW osVersionInfo = {0};
+                osVersionInfo.dwOSVersionInfoSize = sizeof(RTL_OSVERSIONINFOW);
+                // Call RtlGetVersion
+                if (const auto status = RtlGetVersion(&osVersionInfo); status == 0)
+                {
+                    windows_version ver{};
+                    ver.major = osVersionInfo.dwMajorVersion;
+                    ver.minor = osVersionInfo.dwMinorVersion;
+                    ver.build = osVersionInfo.dwBuildNumber;
+                    return ver;
+                }
+            }
+        }
+        return {};
+    }();
+    return cached_version;
 }
 
 struct GetNetworkInterfacesInfoException : public multipass::FormattedExceptionBase<>
