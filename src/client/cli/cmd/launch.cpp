4862c9737ec41ac141700b19edefa120e549bb00
Scott Harder <scott.harder@canonical.com>
2025-07-29 02:44:16 -0500

    [cli] Remove references to Ubuntu within CLI

--- src/client/cli/cmd/launch.cpp
+++ src/client/cli/cmd/launch.cpp
@@ -173,7 +173,7 @@ std::string cmd::Launch::name() const
 
 QString cmd::Launch::short_help() const
 {
-    return QStringLiteral("Create and start an Ubuntu instance");
+    return QStringLiteral("Create and start a cloud instance");
 }
 
 QString cmd::Launch::description() const
@@ -185,7 +185,7 @@ mp::ParseCode cmd::Launch::parse_args(mp::ArgParser* parser)
 {
     parser->addPositionalArgument(
         "image",
-        "Optional image to launch. If omitted, then the default Ubuntu LTS "
+        "Optional image to launch. If omitted, then the latest Ubuntu LTS "
         "will be used.\n"
         "<remote> can be either ‘release’ or ‘daily‘. If <remote> is omitted, "
         "‘release’ will be used.\n"
@@ -252,7 +252,7 @@ mp::ParseCode cmd::Launch::parse_args(mp::ArgParser* parser)
     QCommandLineOption mountOption(
         "mount",
         "Mount a local directory inside the instance. If <target> is omitted, the "
-        "mount point will be under /home/ubuntu/<source-dir>, where <source-dir> is "
+        "mount point will be under /home/<user>/<source-dir>, where <source-dir> is "
         "the name of the <source> directory.",
         "source>:<target");
 
