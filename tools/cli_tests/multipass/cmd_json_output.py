6e54c3eb399fb61c8e8b3192af691aa35fe737be
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-08-05 20:59:21 +0300

    [cli-tests] refactor boilerplate into bite-sized chunks
    Refactored utils.py into a new module, called utilities:
    - functional
    - mathutils
    - pexpectutils
    - privutils
    - procutils
    - temputils
    - textutils
    - threadutils
    - treeutils
    
    Moved all non-generic util code from utils.py into a new module
    called "multipass":
    - basics
    - certutils
    - cmd_json_output
    - cmd_output
    - helpers
    - multipass_cmd
    - snapshot
    - validate
    
    mount: Renamed expected_mount_gid and expected_mount_uid to
    default_mount_gid and default_mount_uid respectively. Also moved
    these functions to the new "multipass/helpers" module.
    
    Renamed is_size_in_ballpark to is_within_tolerance to make it more
    generic.
    
    Added wait_for_{shutdown|start|restart} commands to multipassd
    controller. Tests can now wait for daemon lifecycle events.
    
    Implemented BooleanLatch class.
    
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

---
+++ tools/cli_tests/multipass/cmd_json_output.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) Canonical, Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import jq
+import json
+
+
+class JsonOutput(dict):
+    """A type to store JSON command output."""
+
+    def __init__(self, content, exitstatus):
+        self.content = content.strip()
+        self.exitstatus = exitstatus
+        try:
+            super().__init__(json.loads(content))
+        except json.JSONDecodeError:
+            super().__init__()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return False
+
+    def __bool__(self):
+        return self.exitstatus == 0
+
+    def jq(self, query):
+        """Real jq queries using the jq library"""
+        return jq.compile(query).input(dict(self)).all()
