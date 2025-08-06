00d1ea316183a3dbcdd58f0c2cf6e62f4015c1c0
Scott Harder <scott.harder@canonical.com>
2025-08-06 12:00:05 -0500

    [daemon] Use pure bash implementation
    `awk` on Debian cloud images uses 32-bit `mawk` causing an integer
    overflow when counting disk space

--- src/daemon/runtime_instance_info_helper.cpp
+++ src/daemon/runtime_instance_info_helper.cpp
@@ -55,25 +55,39 @@ private:
         std::pair{Keys::mem_total_key, R"(free -b | grep 'Mem:' | awk '{printf \$2}')"},
         std::pair{Keys::disk_usage_key,
                   R"(
-                    df -B1 -t ext4 -t btrfs -t vfat --output=source,used,target \
-                    | tail -n +2 \
-                    | while read src used mp; do
-                        fsid=\$(stat -f -c '%d' \$mp)
-                        echo \$fsid \$used
-                    done \
-                    | sort -u -k1,1 \
-                    | awk '{ sum += \$2 } END { printf(\"%d\n\", sum) }'
+                    bash -c '
+                      declare -A seen
+                      sum=0
+                      while read -r src size mp; do
+                        fsid=\$(stat -f -c \"%d\" \$mp)
+                        if [[ -z \${seen[\$fsid]} ]]; then
+                          sum=\$(( sum + size ))
+                          seen[\$fsid]=1
+                        fi
+                      done < <(
+                        df -B1 -t ext4 -t btrfs -t vfat --output=source,used,target \
+                        | tail -n +2
+                      )
+                      printf \"%d\" \$sum
+                    '
                   )"},
         std::pair{Keys::disk_total_key,
                   R"(
-                    df -B1 -t ext4 -t btrfs -t vfat --output=source,size,target \
-                    | tail -n +2 \
-                    | while read src size mp; do
-                        fsid=\$(stat -f -c '%d' \$mp)
-                        echo \$fsid \$size
-                    done \
-                    | sort -u -k1,1 \
-                    | awk '{ sum += \$2 } END { printf(\"%d\n\", sum) }'
+                    bash -c '
+                      declare -A seen
+                      sum=0
+                      while read -r src size mp; do
+                        fsid=\$(stat -f -c \"%d\" \$mp)
+                        if [[ -z \${seen[\$fsid]} ]]; then
+                          sum=\$(( sum + size ))
+                          seen[\$fsid]=1
+                        fi
+                      done < <(
+                        df -B1 -t ext4 -t btrfs -t vfat --output=source,size,target \
+                        | tail -n +2
+                      )
+                      printf \"%d\" \$sum
+                    '
                   )"},
         std::pair{Keys::cpus_key, "nproc"},
         std::pair{Keys::cpu_times_key, "head -n1 /proc/stat"},
