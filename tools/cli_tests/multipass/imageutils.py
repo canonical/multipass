b18e2ab37c9bc3af8b737c03c8325cc5243df447
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-08-07 15:36:43 +0300

    [cli-tests] add image_name_to_version util function
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

---
+++ tools/cli_tests/multipass/imageutils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) Canonical, Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+
+def image_name_to_version(image):
+    return {"noble": "24.04", "jammy": "22.04", "focal": "20.04", "bionic": "18.04"}[
+        image
+    ]
