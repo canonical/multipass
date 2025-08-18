8d0fdefe46b67b6a5c9ab2e9717bcd996186a4a5
Trevor Shoe <trevor.schupbach@canonical.com>
2025-08-18 17:55:30 -0400

    [az] Fixup rebase

--- src/client/gui/lib/vm_table/vms.dart
+++ src/client/gui/lib/vm_table/vms.dart
@@ -174,8 +174,8 @@ class Vms extends ConsumerWidget {
             finalRow: infos.isNotEmpty ? totalUsageRow : null,
             emptyContent: emptyTableContent,
           ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 }
