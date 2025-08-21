3db4741662472316cb2000190e85d100c57521a9
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-08-21 20:07:32 +0300

    [linux/process_factory] use aa_change_onexec_forksafe
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

--- src/platform/backends/shared/linux/process_factory.cpp
+++ src/platform/backends/shared/linux/process_factory.cpp
@@ -20,12 +20,11 @@
 #include <multipass/exceptions/snap_environment_exception.h>
 #include <multipass/format.h>
 #include <multipass/logging/log.h>
+#include <multipass/platform_unix.h>
 #include <multipass/process/basic_process.h>
 #include <multipass/process/process_spec.h>
 #include <multipass/process/simple_process_spec.h>
 #include <multipass/snap_utils.h>
-
-#include <signal.h>
 
 namespace mp = multipass;
 namespace mpl = multipass::logging;
@@ -36,7 +35,10 @@ class AppArmoredProcess : public mp::BasicProcess
 {
 public:
     AppArmoredProcess(const mp::AppArmor& aa, std::shared_ptr<mp::ProcessSpec> spec)
-        : mp::BasicProcess{spec}, apparmor{aa}
+        : mp::BasicProcess{spec},
+          apparmor{aa},
+          aa_exec_str(fmt::format("exec {0}",
+                                  process_spec->apparmor_profile_name().toLatin1().toStdString()))
     {
         apparmor.load_policy(process_spec->apparmor_profile().toLatin1());
 
@@ -53,18 +55,36 @@ public:
 
     void setup_child_process() final
     {
-        mp::BasicProcess::setup_child_process();
-
         // This function runs after fork, but before exec, which is a perfect
         // place to reset the signal masks.
+        //
+        // BEWARE: This function is called right in between fork and exec, which is a *very* special
+        // state. This is a critical section where only a handful of functions can be called:
+        //
+        // https://man7.org/linux/man-pages/man7/signal-safety.7.html
+        //
+        // No heap allocations, no exceptions, basically treat this as a signal handler and adhere
+        // by all the things you can't do in a signal handler. Further info:
+        //
+        // https://doc.qt.io/qt-6/qprocess.html#setChildProcessModifier
+        //
+        // Also, be careful about the stack allocations, too. The clone() call is being
+        // made with CLONE_VM and the older QT versions has only 4096 bytes of reserved
+        // stack space for the child:
+        //
+        // https://github.com/qt/qtbase/commit/993db5a12227b1e4067714ddc626d64a14474a54
+
+        mp::BasicProcess::setup_child_process();
+
         // Unblock all signals for the child process
         {
-            sigset_t set;
-            sigemptyset(&set);
-            sigprocmask(SIG_SETMASK, &set, nullptr);
+            sigset_t sigset;
+            sigemptyset(&sigset);
+            sigprocmask(SIG_SETMASK, &sigset, nullptr);
         }
 
-        apparmor.next_exec_under_policy(process_spec->apparmor_profile_name().toLatin1());
+        // In the same spirit above, use our fork-friendly aa_change_onexec implementation
+        mp::AppArmor::aa_change_onexec_forksafe(aa_exec_str.c_str());
     }
 
     ~AppArmoredProcess()
@@ -82,6 +102,9 @@ public:
 
 private:
     const mp::AppArmor& apparmor;
+
+    // This has to be pre-populated since we can't allocate memory in fork() state.
+    const std::string aa_exec_str;
 };
 
 std::optional<mp::AppArmor> create_apparmor()
