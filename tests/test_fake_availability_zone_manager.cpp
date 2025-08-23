f7bd0d152bd30b6744448d90454f1ef15f44823e
levkropp <lev.kropp@canonical.com>
2025-08-23 14:40:08 -0400

    [az] Add test coverage for fake manager

--- tests/test_fake_availability_zone_manager.cpp
+++ tests/test_fake_availability_zone_manager.cpp
@@ -17,6 +17,7 @@
 
 #include "common.h"
 #include "mock_logger.h"
+#include "mock_virtual_machine.h"
 
 #include <multipass/exceptions/not_implemented_on_this_backend_exception.h>
 #include <multipass/fake_availability_zone_manager.h>
@@ -83,4 +84,53 @@ TEST_F(FakeAvailabilityZoneManagerTest, ZoneSetAvailableThrowsNotImplemented)
     // Setting zone availability should throw NotImplementedOnThisBackendException
     EXPECT_THROW(zone.set_available(false), mp::NotImplementedOnThisBackendException);
     EXPECT_THROW(zone.set_available(true), mp::NotImplementedOnThisBackendException);
+}
+
+TEST_F(FakeAvailabilityZoneManagerTest, ZoneGetNameReturnsCorrectName)
+{
+    mp::FakeAvailabilityZoneManager manager;
+    auto& zone = manager.get_zone("zone1");
+
+    EXPECT_EQ(zone.get_name(), "zone1");
+}
+
+TEST_F(FakeAvailabilityZoneManagerTest, ZoneGetSubnetReturnsSubnet)
+{
+    mp::FakeAvailabilityZoneManager manager;
+    auto& zone = manager.get_zone("zone1");
+
+    EXPECT_EQ(zone.get_subnet(), "10.0.0.0/24");
+}
+
+TEST_F(FakeAvailabilityZoneManagerTest, ZoneIsAvailableReturnsTrue)
+{
+    mp::FakeAvailabilityZoneManager manager;
+    auto& zone = manager.get_zone("zone1");
+
+    // The fake zone should always report as available
+    EXPECT_TRUE(zone.is_available());
+}
+
+TEST_F(FakeAvailabilityZoneManagerTest, ZoneAddVmDoesNotThrow)
+{
+    mp::FakeAvailabilityZoneManager manager;
+    auto& zone = manager.get_zone("zone1");
+
+    // Create a mock VM for testing
+    auto mock_vm = std::make_unique<NiceMock<mpt::MockVirtualMachine>>("test-vm");
+
+    // add_vm should be a no-op and not throw
+    EXPECT_NO_THROW(zone.add_vm(*mock_vm));
+}
+
+TEST_F(FakeAvailabilityZoneManagerTest, ZoneRemoveVmDoesNotThrow)
+{
+    mp::FakeAvailabilityZoneManager manager;
+    auto& zone = manager.get_zone("zone1");
+
+    // Create a mock VM for testing
+    auto mock_vm = std::make_unique<NiceMock<mpt::MockVirtualMachine>>("test-vm");
+
+    // remove_vm should be a no-op and not throw
+    EXPECT_NO_THROW(zone.remove_vm(*mock_vm));
 }
