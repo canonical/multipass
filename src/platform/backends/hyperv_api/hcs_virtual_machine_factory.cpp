8db901d1e80e33e1a83792843d6ac8a6a19bd505
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-09-16 17:42:48 +0300

    [platform/win] List IEEE 802.11 devices in get_network_interfaces_info

--- src/platform/backends/hyperv_api/hcs_virtual_machine_factory.cpp
+++ src/platform/backends/hyperv_api/hcs_virtual_machine_factory.cpp
@@ -301,7 +301,7 @@ std::vector<NetworkInterfaceInfo> HCSVirtualMachineFactory::get_adapters()
     for (auto& item : MP_PLATFORM.get_network_interfaces_info())
     {
         auto& net = item.second;
-        if (const auto& type = net.type; type == "Ethernet")
+        if (const auto& type = net.type; type == "Ethernet" || type == "IEEE 802.11")
         {
             net.needs_authorization = true;
             ret.emplace_back(std::move(net));
