c3f26ef3c908196f97dde341688cefdda1c99655
geoffreynyaga <geoffreynyagak@gmail.com>
2025-09-19 14:32:23 +0300

    [doc] Add packer docs to ignore list

--- docs/conf.py
+++ docs/conf.py
@@ -209,6 +209,7 @@ linkcheck_ignore = [
     "https://sourceforge.net/projects/xming/",
     "http://www.straightrunning.com/XmingNotes/",
     "https://unix.stackexchange.com",  # it seems stackexchange is now blocking bots
+    "https://developer.hashicorp.com/packer"
 ]
 
 linkcheck_retries = 3
