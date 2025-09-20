74b4b2ce1e490d099b065fd2cfc20ec5633203ac
Jim Porter <jim.porter@canonical.com>
2025-09-19 19:10:53 -0700

    [fmt] Mark runtime format strings as such

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -796,9 +796,9 @@ grpc::StatusCode react_to_component(const SelectionComponent& selection_componen
                     const auto& instance_name = get_instance_name(instance_element);
 
                     if (status_code)
-                        add_fmt_to(errors, msg, instance_name);
+                        add_fmt_to(errors, fmt::runtime(msg), instance_name);
                     else
-                        mpl::debug(category, msg, instance_name);
+                        mpl::debug(category, fmt::runtime(msg), instance_name);
                 }
             }
         }
@@ -880,7 +880,7 @@ grpc::Status grpc_status_for_instance_trail(const InstanceTrail& trail,
     const auto& status_code = relevant_reaction_component->status_code;
     if (const auto& msg_opt = relevant_reaction_component->message_template; msg_opt)
     {
-        const auto& msg = fmt::format(*msg_opt, *instance_name);
+        const auto& msg = fmt::format(fmt::runtime(*msg_opt), *instance_name);
         if (status_code)
             return grpc::Status{status_code, msg, ""};
 
