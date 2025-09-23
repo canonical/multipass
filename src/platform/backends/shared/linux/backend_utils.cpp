127f17860c00e48c898da24bab95fe7dfbe1c550
Scott Harder <scott.harder@canonical.com>
2025-09-23 13:55:35 -0500

    [utils] Add missing include

--- src/platform/backends/shared/linux/backend_utils.cpp
+++ src/platform/backends/shared/linux/backend_utils.cpp
@@ -25,6 +25,7 @@
 #include <multipass/process/basic_process.h>
 #include <multipass/top_catch_all.h>
 #include <multipass/utils.h>
+#include <shared/shared_backend_utils.h>
 
 #include <scope_guard.hpp>
 
