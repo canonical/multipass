152577b9d20a1fcb681b4cb2d732fdcf26038bc6
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2025-09-24 16:31:38 +0200

    [backend] Add comment for posterior LXD removal
    On LXD removal the qemu_img_utils can be moved to the qemu backend since
    it is the only caller.

--- src/platform/backends/shared/qemu_img_utils/qemu_img_utils.h
+++ src/platform/backends/shared/qemu_img_utils/qemu_img_utils.h
@@ -24,6 +24,9 @@
 
 namespace multipass
 {
+
+// TODOn on LXD removal: Move all these functions to the qemu backend, since the only remaining
+// calling backend is LXD.
 class MemorySize;
 class QemuImgProcessSpec;
 
