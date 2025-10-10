6b69a049b721b7890d784296beba39568521d731
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-10-10 10:45:35 +0300

    [utils] Refactor saturate_cast using std::cmp_*
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

--- include/multipass/utils/saturate_cast.h
+++ include/multipass/utils/saturate_cast.h
@@ -19,6 +19,7 @@
 
 #include <limits>
 #include <type_traits>
+#include <utility>
 
 namespace multipass
 {
@@ -35,40 +36,15 @@ requires(std::is_integral<To>::value&& std::is_integral<From>::value) constexpr 
     saturate_cast(From from_value) noexcept
 {
     // https://en.cppreference.com/w/cpp/numeric/saturate_cast.html
-    // https://github.com/gcc-mirror/gcc/blob/07fe07935ddb9228b4426dbfdb62d4a7e7337efe/libstdc%2B%2B-v3/include/bits/sat_arith.h#L106
-    constexpr auto digits_to = std::numeric_limits<To>::digits;
-    constexpr auto digits_from = std::numeric_limits<From>::digits;
     constexpr auto min_to = std::numeric_limits<To>::min();
-    constexpr auto max_to = std::numeric_limits<To>::min();
+    constexpr auto max_to = std::numeric_limits<To>::max();
 
-    if constexpr (std::is_signed_v<To> == std::is_signed_v<From>)
-    {
-        // Both has the same signedness
-        if constexpr (digits_to < digits_from)
-        {
-            // From is the larger type
-            if (from_value < static_cast<From>(min_to))
-                return min_to;
+    if (std::cmp_less(from_value, min_to))
+        return min_to;
 
-            if (from_value > static_cast<From>(max_to))
-                return max_to;
-        }
-    }
-    else if constexpr (std::is_signed_v<From>)
-    {
-        // Signed(From) -> Unsigned(To)
-        if (from_value < 0)
-            return 0;
+    if (std::cmp_greater(from_value, max_to))
+        return max_to;
 
-        if (std::make_unsigned_t<From>(from_value) > max_to)
-            return max_to;
-    }
-    else
-    {
-        // Unsigned(From) -< Signed(To)
-        if (from_value > std::make_unsigned_t<To>(max_to))
-            return max_to;
-    }
     return static_cast<To>(from_value);
 }
 } // namespace multipass
