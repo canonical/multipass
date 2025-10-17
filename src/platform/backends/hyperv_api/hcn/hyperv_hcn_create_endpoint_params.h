ec3d40a27d9c13eb95a8c1200234ed0038388d73
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-10-17 21:45:35 +0300

    [hyperv-hcs] Inherit from formatter<string_view> in all formatters

--- src/platform/backends/hyperv_api/hcn/hyperv_hcn_create_endpoint_params.h
+++ src/platform/backends/hyperv_api/hcn/hyperv_hcn_create_endpoint_params.h
@@ -60,11 +60,8 @@ struct CreateEndpointParameters
  */
 template <typename Char>
 struct fmt::formatter<multipass::hyperv::hcn::CreateEndpointParameters, Char>
+    : formatter<basic_string_view<Char>, Char>
 {
-    constexpr auto parse(basic_format_parse_context<Char>& ctx)
-    {
-        return ctx.begin();
-    }
 
     template <typename FormatContext>
     auto format(const multipass::hyperv::hcn::CreateEndpointParameters& params,
