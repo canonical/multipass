17c6ae5ce82c1b2f0789b65a8fd39444303e581e
Jim Porter <jim.porter@canonical.com>
2025-11-13 14:18:14 -0800

    [image-host] Use Boost.JSON

--- include/multipass/vm_image_info.h
+++ include/multipass/vm_image_info.h
@@ -20,11 +20,14 @@
 #include <QString>
 #include <QStringList>
 
+#include <boost/json.hpp>
+
+#include <multipass/json_utils.h>
+
 namespace multipass
 {
-class VMImageInfo
+struct VMImageInfo
 {
-public:
     QStringList aliases;
     QString os;
     QString release;
@@ -40,4 +43,32 @@ public:
 
     friend inline bool operator==(const VMImageInfo& a, const VMImageInfo& b) = default;
 };
+
+struct for_arch
+{
+    std::string arch;
+};
+
+inline VMImageInfo tag_invoke(const boost::json::value_to_tag<VMImageInfo>&,
+                              const boost::json::value& json,
+                              const for_arch& arch)
+{
+    QStringList aliases = value_to<QString>(json.at("aliases")).split(",", Qt::SkipEmptyParts);
+    for (QString& alias : aliases)
+        alias = alias.trimmed();
+
+    const auto& arch_json = json.at("items").at(arch.arch);
+    return {aliases,
+            value_to<QString>(json.at("os")),
+            value_to<QString>(json.at("release")),
+            value_to<QString>(json.at("release_codename")),
+            value_to<QString>(json.at("release_title")),
+            true,
+            value_to<QString>(arch_json.at("image_location")),
+            value_to<QString>(arch_json.at("id")),
+            "",
+            value_to<QString>(arch_json.at("version")),
+            lookup_or<int>(arch_json, "size", -1),
+            true};
+}
 } // namespace multipass
