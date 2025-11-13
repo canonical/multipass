1f1714266766ba9d60cc8b19860ae514ea616f15
Jim Porter <jim.porter@canonical.com>
2025-11-12 17:57:31 -0800

    [daemon] Use Boost.JSON in the image vault

--- include/multipass/query.h
+++ include/multipass/query.h
@@ -17,7 +17,11 @@
 
 #pragma once
 
+#include <multipass/json_utils.h>
+
 #include <string>
+
+#include <boost/json.hpp>
 
 namespace multipass
 {
@@ -38,4 +42,25 @@ public:
     Type query_type;
     bool allow_unsupported{false};
 };
+
+inline void tag_invoke(const boost::json::value_from_tag&,
+                       boost::json::value& json,
+                       const Query& query)
+{
+    json = {{"release", query.release},
+            {"persistent", query.persistent},
+            {"remote_name", query.remote_name},
+            {"query_type", static_cast<int>(query.query_type)}};
+}
+
+inline Query tag_invoke(const boost::json::value_to_tag<Query>&, const boost::json::value& json)
+{
+    return {
+        "",
+        value_to<std::string>(json.at("release")),
+        value_to<bool>(json.at("persistent")),
+        lookup_or<std::string>(json, "remote_name", ""),
+        static_cast<Query::Type>(lookup_or<int>(json, "query_type", 0)),
+    };
+}
 } // namespace multipass
