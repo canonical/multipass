4a63de3e4d2b228f08d2dedaa2f1a55826299e2a
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2025-12-02 15:04:16 +0100

    [exceptions] Add FFI boundary error checking
    Also added comments and file headers

--- rxx/petnamers/src/lib.rs
+++ rxx/petnamers/src/lib.rs
@@ -1,11 +1,30 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 use std::os::raw::c_char;
 use std::string::ParseError;
+
 #[cxx::bridge(namespace = "petnamers")]
 pub mod ffi {
     pub enum NumWords {
         One,
         Two,
         Three,
+        Max,
     }
 
     extern "Rust" {
@@ -16,12 +35,6 @@ pub mod ffi {
         type Level = rust_logger::ffi::Level;
     }
 }
-//use cxx::{ExternType, type_id};
-//
-//unsafe impl cxx::ExternType for ffi::Level {
-//    type Id = cxx::type_id!("multipass::logging::Level");
-//    type Kind = cxx::kind::Trivial; // enum is trivially copyable
-//}
 
 fn generate_petname(n_w: ffi::NumWords, sep: c_char) -> Result<String, ParseError> {
     rust_logger::log_message(
--- rxx/petnamers/src/name_generator_rs.cpp
+++ rxx/petnamers/src/name_generator_rs.cpp
@@ -1,2 +1,46 @@
 
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "petnamers/src/lib.rs.h"
+#include <multipass/exceptions/rust_exceptions.h>
 #include <multipass/name_generator_rs.h>
+
+namespace mpp = multipass::petname;
+namespace mp = multipass;
+
+std::string mpp::generate_petname(petnamers::NumWords word_count, char sep)
+{
+    if (word_count >= petnamers::NumWords::Max || word_count < petnamers::NumWords::One)
+        throw mp::FaultyFFIArgument("Invalid argument: word_count is outside the enum's bounds");
+    try
+    {
+        return std::string(petnamers::generate_petname(word_count, sep));
+    }
+    catch (const rust::Error& e)
+    {
+        std::string error{e.what()};
+        if (error.find("some_error"))
+        {
+            throw mp::UnknownRustError(error);
+        }
+        else
+        {
+            throw mp::UnknownRustError(error);
+        }
+    }
+}
