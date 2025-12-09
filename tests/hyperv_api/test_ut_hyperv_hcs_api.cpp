1bb5ba03b78d26bbc0a2a60c07f31748c153289f
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-12-09 16:14:06 +0300

    [hyperv-api] Remove _table suffix from API decls

--- tests/hyperv_api/test_ut_hyperv_hcs_api.cpp
+++ tests/hyperv_api/test_ut_hyperv_hcs_api.cpp
@@ -18,7 +18,7 @@
 #include "hyperv_api/hcs/hyperv_hcs_create_compute_system_params.h"
 #include "hyperv_api/hcs/hyperv_hcs_wrapper.h"
 #include "hyperv_test_utils.h"
-#include "mock_hyperv_hcs_api_table.h"
+#include "mock_hyperv_hcs_api.h"
 #include "mock_schema_version.h"
 #include "tests/mock_logger.h"
 #include "gmock/gmock.h"
