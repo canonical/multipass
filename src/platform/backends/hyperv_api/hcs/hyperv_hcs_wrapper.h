68d6d31e658b49e3a3f76b00b516d07d6d7bf860
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2025-12-09 15:21:03 +0300

    [hyperv-api] Remove function comments

--- src/platform/backends/hyperv_api/hcs/hyperv_hcs_wrapper.h
+++ src/platform/backends/hyperv_api/hcs/hyperv_hcs_wrapper.h
@@ -34,197 +34,42 @@ namespace multipass::hyperv::hcs
 using HcsSystemHandle = std::shared_ptr<void>;
 
 /**
- * A high-level wrapper class that defines
- * the common operations that Host Compute System
- * API provide.
+ * A high-level wrapper class that defines the common operations that Host Compute System API
+ * provide.
  */
 struct HCSWrapper : public Singleton<HCSWrapper>
 {
-
-    /**
-     * Construct a new HCNWrapper
-     *
-     */
     HCSWrapper(const Singleton<HCSWrapper>::PrivatePass&) noexcept;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Open an existing Host Compute System
-     *
-     * @param api The HCS API table
-     * @param name Target Host Compute System's name
-     *
-     * @return auto UniqueHcsSystem non-nullptr on success.
-     */
     [[nodiscard]] virtual OperationResult open_compute_system(
         const std::string& compute_system_name,
         HcsSystemHandle& out_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Create a new Host Compute System
-     *
-     * @param [in] params Parameters for the new compute system
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult create_compute_system(
         const CreateComputeSystemParameters& params,
         HcsSystemHandle& out_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Start a compute system.
-     *
-     * @param [in] compute_system_name Target compute system's name
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult start_compute_system(
         const HcsSystemHandle& target_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Gracefully shutdown the compute system
-     *
-     * @param [in] compute_system_name Target compute system's name
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult shutdown_compute_system(
         const HcsSystemHandle& target_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Forcefully shutdown the compute system
-     *
-     * @param [in] compute_system_name Target compute system's name
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult terminate_compute_system(
         const HcsSystemHandle& target_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Pause the execution of a running compute system
-     *
-     * @param [in] compute_system_name Target compute system's name
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult pause_compute_system(
         const HcsSystemHandle& target_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Resume the execution of a previously paused compute system
-     *
-     * @param [in] compute_system_name Target compute system's name
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult resume_compute_system(
         const HcsSystemHandle& target_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Retrieve a Host Compute System's properties
-     *
-     * @param [in] compute_system_name Target compute system's name
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult get_compute_system_properties(
         const HcsSystemHandle& target_hcs_system) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Grant a compute system access to a file path.
-     *
-     * @param [in] compute_system_name Target compute system's name
-     * @param [in] file_path File path to grant access to
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult grant_vm_access(
         const std::string& compute_system_name,
         const std::filesystem::path& file_path) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Revoke a compute system's access to a file path.
-     *
-     * @param [in] compute_system_name Target compute system's name
-     * @param [in] file_path File path to revoke access to
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult revoke_vm_access(
         const std::string& compute_system_name,
         const std::filesystem::path& file_path) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Retrieve the current state of the compute system.
-     *
-     * @param [in] compute_system_name Target compute system's name
-     * @param [out] state_out Variable to write the compute system's state
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult get_compute_system_state(
         const HcsSystemHandle& target_hcs_system,
         ComputeSystemState& state_out) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Modify a Host Compute System request.
-     *
-     * @param compute_system_name Target compute system name
-     * @param request The request
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult modify_compute_system(
         const HcsSystemHandle& target_hcs_system,
         const HcsRequest& request) const;
-
-    // ---------------------------------------------------------
-
-    /**
-     * Set compute system callback for a HCS.
-     *
-     * @param compute_system_name Target compute system name
-     * @param context Context, which will be passed to the callback
-     * @param callback Callback function
-     *
-     * @return An object that evaluates to true on success, false otherwise.
-     * message() may contain details of failure when result is false.
-     */
     [[nodiscard]] virtual OperationResult set_compute_system_callback(
         const HcsSystemHandle& target_hcs_system,
         void* context,
