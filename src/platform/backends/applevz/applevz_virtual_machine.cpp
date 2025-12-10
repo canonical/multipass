5c6a7ec01178df317f012a1117cc3c1feda70675
Scott Harder <scott.harder@canonical.com>
2025-12-10 14:42:39 -0600

    [apple-vm] Use proper state functions before stop

--- src/platform/backends/applevz/applevz_virtual_machine.cpp
+++ src/platform/backends/applevz/applevz_virtual_machine.cpp
@@ -136,12 +136,12 @@ void AppleVZVirtualMachine::shutdown(ShutdownPolicy shutdown_policy)
 
     CFError error;
 
-    if (shutdown_policy == ShutdownPolicy::Poweroff)
+    if (shutdown_policy == ShutdownPolicy::Poweroff && MP_APPLEVZ.can_stop(vm_handle))
     {
         mpl::debug(log_category, "shutdown() -> Forcing shutdown of VM `{}`", vm_name);
         error = MP_APPLEVZ.stop_vm(vm_handle, true);
     }
-    else if (MP_APPLEVZ.can_stop(vm_handle))
+    else if (MP_APPLEVZ.can_request_stop(vm_handle))
     {
         mpl::debug(log_category, "shutdown() -> Requesting shutdown of VM `{}`", vm_name);
         error = MP_APPLEVZ.stop_vm(vm_handle);
