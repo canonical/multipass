f973fbcbd18bf8bcad89e968382630c441535687
Ricardo Abreu <ricardo.abreu@canonical.com>
2025-12-12 15:58:56 +0000

    [tests] Test arp output with incomplete MAC

--- tests/macos/test_backend_utils.cpp
+++ tests/macos/test_backend_utils.cpp
@@ -34,6 +34,20 @@ const QByteArray mock_arp_output_stream = QByteArray{R"(
 ? (192.168.64.5) at 50:eb:f6:7f:39:a7 on bridge100 ifscope [bridge]
 ? (192.168.64.6) at 50:eb:f6:7f:39:a7 on bridge100 ifscope [bridge]
 ? (192.168.64.255) at ff:ff:ff:ff:ff:ff on bridge100 ifscope [bridge]
+? (192.168.2.1) at 18:58:80:a:4a:1c on en0 ifscope [ethernet]
+? (192.168.2.1) at be:d0:74:27:1c:64 on bridge100 ifscope permanent [bridge]
+? (192.168.2.2) at (incomplete) on en0 ifscope [ethernet]
+? (192.168.2.2) at 52:54:0:55:1a:c8 on bridge100 ifscope [bridge]
+? (192.168.2.43) at c8:99:b2:77:72:f0 on en0 ifscope [ethernet]
+? (192.168.2.100) at 40:6c:8f:20:be:f2 on en0 ifscope [ethernet]
+? (192.168.2.105) at 90:48:9a:16:df:14 on en0 ifscope [ethernet]
+? (192.168.2.130) at be:c7:5:e7:50:38 on en0 ifscope [ethernet]
+? (192.168.2.143) at f6:ac:38:42:84:bd on en0 ifscope [ethernet]
+? (192.168.2.154) at 7e:1:62:5a:3f:ab on en0 ifscope [ethernet]
+? (192.168.2.182) at e:a2:3d:ee:2a:2e on en0 ifscope permanent [ethernet]
+? (192.168.2.185) at da:bd:76:7e:4c:98 on en0 ifscope [ethernet]
+? (192.168.2.212) at c8:99:b2:75:25:b0 on en0 ifscope [ethernet]
+? (192.168.2.255) at ff:ff:ff:ff:ff:ff on en0 ifscope [ethernet]
 ? (224.0.0.251) at 1:0:5e:0:0:fb on en0 ifscope permanent [ethernet])"};
 }
 
@@ -83,6 +97,7 @@ INSTANTIATE_TEST_SUITE_P(GetNeighbourIPTestsInstantiation,
                                 std::make_pair("52:54:00:85:72:55", "192.168.64.3"),
                                 std::make_pair("52:54:00:e1:cd:ab", "192.168.64.4"),
                                 std::make_pair("50:eb:f6:7f:39:a7", "192.168.64.6"),
+                                std::make_pair("52:54:00:55:1a:c8", "192.168.2.2"),
                                 std::make_pair("01:00:5e:00:00:fb", "224.0.0.251")));
 
 struct GetNeighbourIPInvalidInputsTests : public GetNeighbourIpFixture,
