dc58353e4268004f580ebd01b9e86e4f8eadc583
Jim Porter <jim.porter@canonical.com>
2025-12-15 14:11:10 -0800

    [cmake] Add support for defining feature flags

---
+++ src/cmake/feature-flag.cmake
@@ -0,0 +1,27 @@
+# Copyright (C) Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+include(CMakeDependentOption)
+
+option(MP_ALLOW_OPTIONAL_FEATURES "allow enabling optional features" ON)
+
+# Define a feature flag named `option`, with optional requirements as the third argument (see
+# `cmake_dependent_option` for details).
+macro(feature_flag option doc)
+  if(${ARGC} GREATER 2)
+    cmake_dependent_option(${option} "${doc}" ON "MP_ALLOW_OPTIONAL_FEATURES;${ARGV2}" OFF)
+  else()
+    cmake_dependent_option(${option} "${doc}" ON MP_ALLOW_OPTIONAL_FEATURES OFF)
+  endif()
+endmacro()
