de1929c4cc98cd21890c3c92b52900c0a66402cb
Jim Porter <jim.porter@canonical.com>
2025-12-16 16:47:07 -0800

    [cmake] Add `is_release_branch`
    We can use this to determine the default value of
    `MP_ALLOW_OPTIONAL_FEATURES`.

--- src/cmake/environment-utils.cmake
+++ src/cmake/environment-utils.cmake
@@ -19,3 +19,24 @@ function(is_running_in_ci OUT_VAR)
         set(${OUT_VAR} FALSE PARENT_SCOPE)
     endif()
 endfunction()
+
+function(is_release_branch OUTPUT_VARIABLE)
+  # use upstream repo as the authoritative reference when checking for release status
+  # set -DMULTIPASS_UPSTREAM="" to use the local repository
+  if(MULTIPASS_UPSTREAM)
+    set(MULTIPASS_UPSTREAM_PREFIX "${MULTIPASS_UPSTREAM}/")
+  endif()
+
+  execute_process(COMMAND git describe --all --exact --match "${MULTIPASS_UPSTREAM_PREFIX}release/*"
+                  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                  OUTPUT_VARIABLE GIT_RELEASE_BRANCH
+                  OUTPUT_STRIP_TRAILING_WHITESPACE
+                  ERROR_QUIET)
+
+  string(REGEX MATCH "release/[0-9]+.[0-9]+" GIT_RELEASE_MATCH "${GIT_RELEASE_BRANCH}")
+  if(GIT_RELEASE_MATCH)
+    set(${OUTPUT_VARIABLE} TRUE PARENT_SCOPE)
+  else()
+    set(${OUTPUT_VARIABLE} FALSE PARENT_SCOPE)
+  endif()
+endfunction()
