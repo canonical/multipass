9ac91d067693e3d82bcfd8180520d1d09cc6d1d0
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2026-01-08 10:32:26 +0300

    [hyperv-hcs] Move default_ssh_port to constants

--- include/multipass/constants.h
+++ include/multipass/constants.h
@@ -78,4 +78,5 @@ constexpr auto home_in_instance = "/home/ubuntu";
 
 constexpr std::chrono::milliseconds vm_shutdown_timeout =
     300000ms; // unit: ms, 5 minute timeout for shutdown/suspend
+constexpr auto default_ssh_port = 22;
 } // namespace multipass
