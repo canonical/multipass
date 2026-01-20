0c04b04b1ccf17615d79e56772127f5cb9d35b73
ScottH <59572507+sharder996@users.noreply.github.com>
2026-01-20 15:46:25 +0000

    [s390x] Compile Multipass on s390x architecture (#4548)
    Gets Multipass compiling on s390x again. Changes are categorized into
    several areas:
    
    - Enabled s390x compilation for vcpkg
    - Moved check for `MULTIPASS_ENABLE_FLUTTER_GUI` in cmake up one level
    so we can avoid unnecessarily compiling the Dart FFI interface
    
    - Included additional firmware for QEMU on s390x
    - Fixed bug in Linux CI where we install `ccache` interactively
    
    - Adjusted QEMU process arguments when running on s390x
    - Added s390x QEMU process arguments in unit test
    
    - Fixed bug in `CustomImageHost` where we incorrectly assume that the
    current CPU architecture will be in the manifest
    - Made tests in `test_custom_image_host.cpp` more architecture agnostic
    so they continue to function currently even if the current CPU
    architecture is not in the list of supported architectures in the
    manifest
    - Included s390x images from the Fedora scraper
    
    ~~From this PR, there are still a few outstanding unknowns:~~
    
    ~~Firstly, I've carefully based this branch to just before #4530 was
    merged into `main`. vcpkg fetches Qt from a domain that for whatever
    reason doesn't play nice with the proxy that my s390x development
    machine is behind. I'm still using the same version of Qt, just from a
    different source, so fingers-crossed this doesn't cause an issue when it
    runs in Launchpad. Also, the Canonical self-hosted s390x runners are
    incredibly flaky. After many re-runs I was only able to get to the
    packaging stage once when the runner subsequently failed due to a
    networking issue. The
    [documentation](https://canonical-self-hosted-github-runner-docs.readthedocs-hosted.com/en/latest/usage/available_runners/#available-runners)
    itself lists the runners as **experimental** so I gave up on them.~~
    
    ~~See code comments on adding a few configure options to the compilation
    of dbus and qtbase.~~ **FIXED**
    
    ~~Secondly, while omitting the GUI from snapcraft is simple enough,
    getting the same recipe to work for both GUI and non-GUI packages has
    proven to be quite complex. Turning off the compilation of the GUI on
    s390x can be easily done through CMake, but snapcraft still wants to
    pull in the `gnome` extension which depends on snaps that aren't
    available on s390x. We might be able to get around this requirement by
    painstakingly individually redefining what the `gnome` extension
    automates for us, but I foresee it being error prone and difficult to
    maintain in the future.~~
    
    ~~Instead, what I propose is that we maintain an additional `git`
    branch, say `gui-less-snap`, where we simply omit the `gui` app from
    `snapcraft.yaml` and limit the architectures under `build-on`. More
    components could theoretically be omitted to optimize the package size,
    but would come with the tradeoff of being more difficult to maintain.
    Then, we create an additional snap recipe in Launchpad that uses this
    branch and publishes Multipass for these special architecture.
    Maintenance of this branch could be automated with rebases onto `main`
    whenever merge queues runs with Mattermost/email alerts happening when
    intervention is required.~~
    
    Snap packaging will be handled in Launchpad through the use of another
    snap recipe which will pull from a unique git branch. Will be addressed
    in a separate PR.
    
    Closes #4549
    
    ---
    
    MULTI-2377

--- src/platform/backends/qemu/qemu_vm_process_spec.cpp
+++ src/platform/backends/qemu/qemu_vm_process_spec.cpp
@@ -78,7 +78,11 @@ in `man qemu-system`, under `-m` option; including suffix to avoid relying on de
         // clang-format off
         // The VM image itself
         args << "-device"
+#if defined Q_PROCESSOR_S390
+             << "virtio-scsi-ccw,id=scsi0"
+#else
              << "virtio-scsi-pci,id=scsi0"
+#endif
              << "-drive"
              << QString("file=%1,if=none,format=qcow2,discard=unmap,id=hda")
                     .arg(desc.image.image_path)
