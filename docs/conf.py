27abbd510226052a947c6fe617c4b6c37384131f
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2026-01-21 14:38:36 +0300

    [docs] Try spoofing User-Agent for linkcheck
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -218,6 +218,14 @@ linkcheck_retries = 3
 
 linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*",r"https://matrix\.to/.*"]
 
+linkcheck_request_headers = {
+    "*": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+    }
+}
+
 
 ########################
 # Configuration extras #
