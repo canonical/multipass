dcb7d145c9f0bff73390923ca3d8c292fd898a3d
Ricardo Abreu <ricardo.abreu@canonical.com>
2026-01-22 23:30:37 +0000

    [log] Implement `debug_location` too

--- include/multipass/logging/trace_location.h
+++ include/multipass/logging/trace_location.h
@@ -33,6 +33,29 @@ std::string_view extract_filename(std::string_view path);
 
 // using a struct and deduction guide to accommodate the default argument after the parameter pack
 template <typename... Args>
+struct log_location
+{
+    log_location(Level level,
+                 std::string_view category,
+                 fmt::format_string<Args...> fmt,
+                 Args&&... args,
+                 std::source_location location = std::source_location::current())
+    {
+        log(level,
+            category,
+            "{}:{} {}(): {}",
+            detail::extract_filename(location.file_name()),
+            location.line(),
+            location.function_name(),
+            fmt::format(fmt, std::forward<Args>(args)...));
+    }
+};
+
+template <typename... Args>
+log_location(Level, std::string_view, fmt::format_string<Args...>, Args&&...)
+    -> log_location<Args...>;
+
+template <typename... Args>
 struct trace_location
 {
     trace_location(std::string_view category,
@@ -40,17 +63,27 @@ struct trace_location
                    Args&&... args,
                    std::source_location location = std::source_location::current())
     {
-        trace(category,
-              "{}:{} {}(): {}",
-              detail::extract_filename(location.file_name()),
-              location.line(),
-              location.function_name(),
-              fmt::format(fmt, std::forward<Args>(args)...));
+        log_location<Args...>(Level::trace, category, fmt, std::forward<Args>(args)..., location);
     }
 };
 
 template <typename... Args>
 trace_location(std::string_view, fmt::format_string<Args...>, Args&&...) -> trace_location<Args...>;
+
+template <typename... Args>
+struct debug_location
+{
+    debug_location(std::string_view category,
+                   fmt::format_string<Args...> fmt,
+                   Args&&... args,
+                   std::source_location location = std::source_location::current())
+    {
+        log_location<Args...>(Level::debug, category, fmt, std::forward<Args>(args)..., location);
+    }
+};
+
+template <typename... Args>
+debug_location(std::string_view, fmt::format_string<Args...>, Args&&...) -> debug_location<Args...>;
 
 } // namespace logging
 } // namespace multipass
