e54e5e1d431e1558fe1601ae9679fc514da8ed6a
Scott Harder <scott.harder@canonical.com>
2026-01-24 08:29:19 -0600

    [ppc64le] Force vcpkg compilation with clang

--- 3rd-party/vcpkg-triplets/ppc64le-linux-release.cmake
+++ 3rd-party/vcpkg-triplets/ppc64le-linux-release.cmake
@@ -7,3 +7,7 @@ set(VCPKG_BUILD_TYPE release)
 # Workaround for dbus and qtbase: when vcpkg sets VCPKG_CMAKE_SYSTEM_NAME, CMake may treat
 # native ppc64le builds as cross-compiling.
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CROSSCOMPILING:BOOL=OFF")
+
+# Workaround for GCC 13 internal compiler errors on ppc64le
+# Use Clang which is more stable on this architecture
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "${VCPKG_CMAKE_CONFIGURE_OPTIONS};-DCMAKE_C_COMPILER=clang;-DCMAKE_CXX_COMPILER=clang++")
