3f70c2ec95e57de5049624039f495f6fbad13bb7
Scott Harder <scott.harder@canonical.com>
2026-01-24 08:28:41 -0600

    [ppc64le] Force vcpkg cross compilation off

--- 3rd-party/vcpkg-triplets/ppc64le-linux-release.cmake
+++ 3rd-party/vcpkg-triplets/ppc64le-linux-release.cmake
@@ -3,3 +3,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
+
+# Workaround for dbus and qtbase: when vcpkg sets VCPKG_CMAKE_SYSTEM_NAME, CMake may treat
+# native ppc64le builds as cross-compiling.
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CROSSCOMPILING:BOOL=OFF")
