625438e562b62116f98b6d65d6d7bb644756e3df
Ricardo Abreu <ricardo.abreu@canonical.com>
2026-01-27 13:10:27 +0000

    [log] Simplify location logging approach
    Avoid wrapping each *_location function with a struct + deduction
    guides. Instead, wrap the location around the category argument.
    
    Implements review suggestion.
    
    Co-authored-by: Jim Porter <826865+jimporter@users.noreply.github.com>

--- include/multipass/logging/log_location.h
+++ include/multipass/logging/log_location.h
@@ -29,61 +29,52 @@ namespace logging
 namespace detail
 {
 std::string_view extract_filename(std::string_view path);
+
+template <typename T>
+struct with_source_location
+{
+    template <std::convertible_to<T> U>
+    with_source_location(U&& val, std::source_location loc = std::source_location::current())
+        : value(std::forward<U>(val)), location(loc)
+    {
+    }
+
+    T value;
+    std::source_location location;
+};
+} // namespace detail
+
+template <typename... Args>
+void log_location(Level level,
+                  detail::with_source_location<std::string_view> category,
+                  fmt::format_string<Args...> fmt,
+                  Args&&... args)
+{
+    const auto& location = category.location;
+    log(level,
+        category.value,
+        "{}:{}, `{}`: {}",
+        detail::extract_filename(location.file_name()),
+        location.line(),
+        location.function_name(),
+        fmt::format(fmt, std::forward<Args>(args)...));
 }
 
-// using a struct and deduction guide to accommodate the default argument after the parameter pack
 template <typename... Args>
-struct log_location
+void trace_location(detail::with_source_location<std::string_view> category,
+                    fmt::format_string<Args...> fmt,
+                    Args&&... args)
 {
-    log_location(Level level,
-                 std::string_view category,
-                 fmt::format_string<Args...> fmt,
-                 Args&&... args,
-                 std::source_location location = std::source_location::current())
-    {
-        log(level,
-            category,
-            "{}:{}, `{}`: {}",
-            detail::extract_filename(location.file_name()),
-            location.line(),
-            location.function_name(),
-            fmt::format(fmt, std::forward<Args>(args)...));
-    }
-};
+    log_location(Level::trace, category, fmt, std::forward<Args>(args)...);
+}
 
 template <typename... Args>
-log_location(Level, std::string_view, fmt::format_string<Args...>, Args&&...)
-    -> log_location<Args...>;
-
-template <typename... Args>
-struct trace_location
+void debug_location(detail::with_source_location<std::string_view> category,
+                    fmt::format_string<Args...> fmt,
+                    Args&&... args)
 {
-    trace_location(std::string_view category,
-                   fmt::format_string<Args...> fmt,
-                   Args&&... args,
-                   std::source_location location = std::source_location::current())
-    {
-        log_location<Args...>(Level::trace, category, fmt, std::forward<Args>(args)..., location);
-    }
-};
-
-template <typename... Args>
-trace_location(std::string_view, fmt::format_string<Args...>, Args&&...) -> trace_location<Args...>;
-
-template <typename... Args>
-struct debug_location
-{
-    debug_location(std::string_view category,
-                   fmt::format_string<Args...> fmt,
-                   Args&&... args,
-                   std::source_location location = std::source_location::current())
-    {
-        log_location<Args...>(Level::debug, category, fmt, std::forward<Args>(args)..., location);
-    }
-};
-
-template <typename... Args>
-debug_location(std::string_view, fmt::format_string<Args...>, Args&&...) -> debug_location<Args...>;
+    log_location(Level::debug, category, fmt, std::forward<Args>(args)...);
+}
 
 } // namespace logging
 } // namespace multipass
