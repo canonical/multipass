a4eae3f8f8540df20b331bf88e583baf3576137e
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2026-01-27 23:39:40 +0100

    [petname] Fix c_char ABI incompatibility

--- rxx/petname/tests/test_petname_generator.rs
+++ rxx/petname/tests/test_petname_generator.rs
@@ -2,18 +2,19 @@ use petname::{
     ffi::NumWords, petname_error::PetnameError, petname_generator::make_petname_generator,
 };
 use std::collections::HashSet;
+use std::ffi::c_char;
 
 #[test]
 fn generates_requested_word_number_with_separator() {
-    let petname_1 = make_petname_generator(NumWords::One, '-' as i8)
+    let petname_1 = make_petname_generator(NumWords::One, '-' as c_char)
         .unwrap()
         .make_name()
         .unwrap();
-    let petname_2 = make_petname_generator(NumWords::Two, '-' as i8)
+    let petname_2 = make_petname_generator(NumWords::Two, '-' as c_char)
         .unwrap()
         .make_name()
         .unwrap();
-    let petname_3 = make_petname_generator(NumWords::Three, '-' as i8)
+    let petname_3 = make_petname_generator(NumWords::Three, '-' as c_char)
         .unwrap()
         .make_name()
         .unwrap();
@@ -25,19 +26,19 @@ fn generates_requested_word_number_with_separator() {
 #[test]
 fn filters_out_bad_input() {
     //First we test that failure is not due to CXX enum syntax
-    let result = make_petname_generator(NumWords { repr: 0 }, '-' as i8);
+    let result = make_petname_generator(NumWords { repr: 0 }, '-' as c_char);
     assert!(result.is_ok());
-    let result = make_petname_generator(NumWords { repr: 4 }, '-' as i8);
+    let result = make_petname_generator(NumWords { repr: 4 }, '-' as c_char);
     assert!(matches!(result, Err(PetnameError::InvalidWordNumber(4))));
 
-    let result = make_petname_generator(NumWords::One, '(' as i8);
+    let result = make_petname_generator(NumWords::One, '(' as c_char);
     assert!(matches!(result, Err(PetnameError::InvalidSeparator(_))));
 }
 
 #[test]
 fn can_generate_unique_names() {
     let mut hashset: HashSet<String> = HashSet::new();
-    let petname_generator = make_petname_generator(NumWords::Three, '-' as i8).unwrap();
+    let petname_generator = make_petname_generator(NumWords::Three, '-' as c_char).unwrap();
     const TOTAL_NAMES: usize = 1000;
 
     for i in 0..TOTAL_NAMES {
