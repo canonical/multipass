acb227989ebd3e5bddd1ca97bc3099626b328f6f
Scott Harder <scott.harder@canonical.com>
2026-01-28 08:31:00 -0600

    [cli] Remove colon from non percentage progression

--- src/client/cli/cmd/launch.cpp
+++ src/client/cli/cmd/launch.cpp
@@ -602,7 +602,7 @@ mp::ReturnCode cmd::Launch::request_launch(const ArgParser* parser)
             std::unordered_map<int, std::string> progress_messages{
                 {LaunchProgress_ProgressTypes_IMAGE, "Retrieving image: "},
                 {LaunchProgress_ProgressTypes_EXTRACT, "Extracting image: "},
-                {LaunchProgress_ProgressTypes_VERIFY, "Verifying image: "},
+                {LaunchProgress_ProgressTypes_VERIFY, "Verifying image"},
                 {LaunchProgress_ProgressTypes_WAITING, "Preparing image: "}};
 
             if (!reply.log_line().empty())
