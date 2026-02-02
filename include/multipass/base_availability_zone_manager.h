5ab8911c87ba0d27c1489dacaf5f58132fb149fa
Jim Porter <jim.porter@canonical.com>
2026-02-02 13:23:27 -0800

    [az] Simplify BaseAvailabilityZone(Manager)
    This moves the file path and mutex to the main class instead of the
    nested `Data` class.

--- include/multipass/base_availability_zone_manager.h
+++ include/multipass/base_availability_zone_manager.h
@@ -60,21 +60,14 @@ private:
         mutable std::shared_mutex mutex{};
     };
 
-    // we store all the data in one struct so that it can be created from one function call in the
-    // initializer list
-    struct data
-    {
-        const std::filesystem::path file_path{};
-        ZoneCollection zone_collection;
-        // we don't have designated initializers, so mutex remains last so it doesn't need to be
-        // manually initialized
-        mutable std::recursive_mutex mutex{};
-    } m;
+    mutable std::recursive_mutex mutex;
+    const std::filesystem::path file_path;
+    ZoneCollection zone_collection;
 
     [[nodiscard]] const ZoneCollection::ZoneArray& zones() const;
 
-    static data read_from_file(const std::filesystem::path& file_path,
-                               const std::filesystem::path& zones_directory);
+    static ZoneCollection read_from_file(const std::filesystem::path& file_path,
+                                         const std::filesystem::path& zones_directory);
 };
 } // namespace multipass
 
