8dd9a07eb143239209ac053557bbd0b0eb40d11d
Scott Harder <scott.harder@canonical.com>
2026-02-10 19:43:26 -0600

    [utils] Resize ASIF image with diskutil

--- src/platform/backends/applevz/applevz_utils.cpp
+++ src/platform/backends/applevz/applevz_utils.cpp
@@ -18,12 +18,16 @@
 #include "applevz_utils.h"
 
 #include <applevz/applevz_wrapper.h>
+#include <multipass/format.h>
+#include <multipass/logging/log.h>
 #include <qemu/qemu_img_utils.h>
+#include <shared/macos/process_factory.h>
 
 #include <cstring>
 #include <fcntl.h>
 #include <unistd.h>
 
+namespace mp = multipass;
 namespace mpl = multipass::logging;
 
 namespace
@@ -92,6 +96,19 @@ void resize_image(const MemorySize& disk_space, const Path& image_path)
 
     if (is_asif_image(image_path))
     {
+        auto process = MP_PROCFACTORY.create_process(
+            QStringLiteral("diskutil"),
+            QStringList() << "image" << "resize" << "--size"
+                          << QString::number(disk_space.in_bytes()) << image_path);
+
+        auto exit_state = process->execute();
+
+        if (!exit_state.completed_successfully())
+        {
+            throw std::runtime_error(fmt::format("Failed to resize ASIF device: {}; Output: {}",
+                                                 exit_state.failure_message(),
+                                                 process->read_all_standard_error().toStdString()));
+        }
     }
     else
     {
