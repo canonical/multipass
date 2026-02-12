945f9bad9d5f34ddd14319385340909086919e83
Scott Harder <scott.harder@canonical.com>
2026-02-12 16:14:29 -0600

    [vcpkg] Use chainloading to deduplicated some code

--- 3rd-party/vcpkg-triplets/x64-linux-release.cmake
+++ 3rd-party/vcpkg-triplets/x64-linux-release.cmake
@@ -1,8 +1,3 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
-
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
 
-include("${CMAKE_CURRENT_LIST_DIR}/per-package-overrides.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/x64-linux.cmake")
