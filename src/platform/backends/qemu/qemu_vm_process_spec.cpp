9c9671fe2a796b59722be47c9a546411cd63547f
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2026-02-27 11:47:57 +0100

    [qemu] Use relative path to locate bridge_helper

--- src/platform/backends/qemu/qemu_vm_process_spec.cpp
+++ src/platform/backends/qemu/qemu_vm_process_spec.cpp
@@ -59,6 +59,8 @@ QStringList mp::QemuVMProcessSpec::arguments() const
                       "Cannot determine QEMU machine type. Falling back to system default.");
         }
 
+        args.replaceInStrings(QRegularExpression("helper=.*bridge_helper"),
+                              "helper=./bridge_helper");
         // need to fix old-style vmnet arguments
         // TODO: remove in due time
         args.replaceInStrings("vmnet-macos,mode=shared,", "vmnet-shared,");
@@ -239,6 +241,7 @@ QString mp::QemuVMProcessSpec::identifier() const
 {
     return QString::fromStdString(desc.vm_name);
 }
+
 QString mp::QemuVMProcessSpec::working_directory() const
 {
     return QDir(QCoreApplication::applicationDirPath()).absolutePath();
