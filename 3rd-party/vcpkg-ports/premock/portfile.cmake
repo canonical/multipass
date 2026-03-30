3f727f1ab97c4148df652ed2fb4dc40d67e2d333
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2026-03-30 20:27:12 +0300

    [vcpkg] Add a overlay port for premock
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

---
+++ 3rd-party/vcpkg-ports/premock/portfile.cmake
@@ -0,0 +1,13 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO atilaneves/premock
+    REF ef6e62da55760b6d81f053b34cde82b883eb27b8
+    SHA512 ae67a2eee725d79b9d548fa7470fd7518430db66d823a5838c5b24a0729c0ab325e9699b6ec7074ee11f73dedc6fec9243a70d2d5659caee825e902b5b5a2dc4
+    HEAD_REF master
+)
+
+file(INSTALL "${SOURCE_PATH}/premock.hpp"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/include"
+)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
