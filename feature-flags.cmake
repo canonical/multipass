2be27b99e804a520e99caa9af9ddcf9c999240cf
Scott Harder <scott.harder@canonical.com>
2026-06-05 20:14:34 -0500

    [cmake] AppleVZ backend depends on AZ feature

--- feature-flags.cmake
+++ feature-flags.cmake
@@ -15,7 +15,7 @@
 include(src/cmake/feature-flag.cmake)
 
 # Multipass backend integrating with Apple Virtualization framework
-feature_flag(APPLEVZ_ENABLED "AppleVZ backend" APPLE)
+feature_flag(APPLEVZ_ENABLED "AppleVZ backend" "APPLE;AVAILABILITY_ZONES_ENABLED")
 
 # The new Windows backend based on Hyper-V Host Compute System / Host Compute Networking APIs
 feature_flag(HYPERV_HCS_ENABLED "Hyper-V HCS backend" WIN32)
