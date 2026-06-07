e9b70fc4195ad1feeebcb3a4bf24fc1395027d5a
Ahmed Magdy Ali <tamtamawy333@gmail.com>
2026-06-07 11:13:42 +0300

    [platform] Add max path/file name platform query

--- src/platform/platform_win.cpp
+++ src/platform/platform_win.cpp
@@ -1379,3 +1379,46 @@ QString mp::platform::Platform::path_to_qstr(const std::filesystem::path& path) 
 {
     return QString::fromStdWString(path.generic_wstring());
 }
+
+size_t mp::platform::Platform::get_maximum_path_length(
+    const std::filesystem::path& target_dir) const
+{
+    const auto ws = target_dir.wstring();
+    const bool has_extended_prefix = ws.starts_with(L"\\\\?\\");
+
+    if (has_extended_prefix)
+    {
+        return 32767u;
+    }
+
+#ifdef MAX_PATH
+    return static_cast<size_t>(MAX_PATH);
+#else
+    return 260u;
+#endif
+}
+
+size_t mp::platform::Platform::get_maximum_file_name_length(
+    const std::filesystem::path& target_dir) const
+{
+    constexpr size_t volume_name_size = 260;
+    wchar_t volume_name[volume_name_size] = {0};
+
+    DWORD max_component_length = 0;
+    DWORD file_system_flags = 0;
+
+    if (GetVolumePathNameW(target_dir.wstring().c_str(), volume_name, std::size(volume_name)) &&
+        GetVolumeInformationW(volume_name,
+                              NULL,
+                              0,
+                              NULL,
+                              &max_component_length,
+                              &file_system_flags,
+                              NULL,
+                              0))
+    {
+        return static_cast<size_t>(max_component_length > 0 ? max_component_length : 255u);
+    }
+
+    return 255u;
+}
