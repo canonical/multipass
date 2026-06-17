b68985a65182011cb7a660be2fe925c856e53e2a
Ricardo Abreu <ricardo.abreu@canonical.com>
2026-06-17 20:14:35 +0100

    [drive-by] Drop obsolete include

--- src/sshfs_mount/sftp_server.cpp
+++ src/sshfs_mount/sftp_server.cpp
@@ -24,8 +24,6 @@
 #include <multipass/platform.h>
 #include <multipass/ssh/plain_ssh_process.h>
 #include <multipass/ssh/ssh_session.h>
-#include <multipass/ssh/throw_on_error.h>
-
 #include <multipass/utils.h>
 
 extern "C"
