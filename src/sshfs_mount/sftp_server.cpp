c7d77bb3cee3af7bb340c08df2764dcbbd571a48
Ricardo Abreu <ricardo.abreu@canonical.com>
2026-06-25 01:14:31 +0100

    [sftp] Create sshfs process and borrow channel
    Reproduce existing code to initialize the sshfs process into the
    PlainSftpServerSession, with a minor tweak of dropping an unused param.
    Mark old versions for removal.

--- src/sshfs_mount/sftp_server.cpp
+++ src/sshfs_mount/sftp_server.cpp
@@ -225,6 +225,7 @@ auto to_unix_permissions(QFile::Permissions perms)
     return out;
 }
 
+// TODO@sftp dump
 void check_sshfs_status(mp::SSHProcess& sshfs_process)
 {
     if (sshfs_process.exit_recognized(250ms))
@@ -236,6 +237,7 @@ void check_sshfs_status(mp::SSHProcess& sshfs_process)
     }
 }
 
+// TODO@sftp dump
 auto create_sshfs_process(mp::SSHSession& session,
                           const std::string& sshfs_exec_line,
                           const std::string& source,
