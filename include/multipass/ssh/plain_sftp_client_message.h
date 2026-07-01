53f8d31c2b66e891ec490ac64f21af78fef08abe
Ricardo Abreu <ricardo.abreu@canonical.com>
2026-07-01 20:28:04 +0100

    [sftp] Add SftpMessage types
    Add an SftpMessage interface and a concrete PlainSftpMessage
    implementation backed by libssh. This allows will allow the SftpServer
    to read messages off of an SftpSession without having to assume a
    particular implementation.

---
+++ include/multipass/ssh/plain_sftp_client_message.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <multipass/sshfs_mount/sftp_client_message.h>
+
+#include <memory>
+
+struct sftp_client_message_struct;
+
+namespace multipass
+{
+
+/**
+ * A concrete SftpMessage backed by a raw libssh message
+ */
+class PlainSftpMessage final : public SftpMessage
+{
+public:
+    ~PlainSftpMessage() override;
+
+    explicit PlainSftpMessage(sftp_client_message_struct* message);
+    PlainSftpMessage(const PlainSftpMessage&) = delete;
+    PlainSftpMessage& operator=(const PlainSftpMessage&) = delete;
+
+    // TODO@sftp Make class final before enabling these
+    PlainSftpMessage(PlainSftpMessage&&) = delete;
+    PlainSftpMessage& operator=(PlainSftpMessage&&) = delete;
+
+private:
+    struct RawMsgDeleter
+    {
+        void operator()(sftp_client_message_struct* message) const;
+    };
+    using RawSftpMsgUptr = std::unique_ptr<sftp_client_message_struct, RawMsgDeleter>;
+
+    RawSftpMsgUptr message;
+};
+
+} // namespace multipass
---
+++ include/multipass/sshfs_mount/sftp_client_message.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+namespace multipass
+{
+
+/**
+ * A unit of communication exchange between an SFTP client and an SFTP server, in either direction.
+ */
+class SftpMessage
+{
+public:
+    virtual ~SftpMessage() = default;
+
+    // No copies
+    SftpMessage(const SftpMessage&) = delete;
+    SftpMessage& operator=(const SftpMessage&) = delete;
+
+protected:
+    SftpMessage() = default;
+};
+
+} // namespace multipass
