2ed2278ff4d4524291086f559d975d37b41b57b1
Ricardo Abreu <ricardo.abreu@canonical.com>
2026-07-09 23:08:13 +0100

    [cmake] Pass compilers and linker along to vcpkg
    Vcpkg builds each dependency in its own process, so the outer CMake
    configuration doesn't propagate to it. Instead, set environment
    variables to influence dependency builds. Reflect their contents in
    the cache hash to avoid mixing ABIs.

--- 3rd-party/vcpkg-triplets/x64-linux.cmake
+++ 3rd-party/vcpkg-triplets/x64-linux.cmake
@@ -4,3 +4,8 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
+# Avoid potentially incompatible ABIs
+# See https://learn.microsoft.com/en-us/vcpkg/users/triplets#vcpkg_env_passthrough
+# > The values of these environment variables will be hashed into the package abi.
+set(VCPKG_ENV_PASSTHROUGH CC CXX LDFLAGS)
+
