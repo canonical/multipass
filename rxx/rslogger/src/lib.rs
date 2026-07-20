21e8198767b95e207222327e508b406a355c6228
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2026-07-20 12:14:21 +0200

    [rslogger] Add logging functions from C++

--- rxx/rslogger/src/lib.rs
+++ rxx/rslogger/src/lib.rs
@@ -15,6 +15,8 @@
  *
  */
 
+pub mod log;
+
 #[cxx::bridge(namespace = "logger")]
 pub mod ffi {
     #[namespace = "multipass::logging"]
@@ -31,21 +33,21 @@ pub mod ffi {
     unsafe extern "C++" {
         include!("multipass/logging/level.h");
         type Level;
-        //#[cxx_name = "Level"]
-        //fn level_from(in_: i32) -> Level;
         include!("multipass/logging/log.h");
         #[namespace = "multipass::logging::rust"]
         fn log_message(level: Level, category: String, message: String) -> Result<()>;
-        //Result<1Type> in a CXX bridge unsafe extern C++ will convert to a try-catch in the C++
-        //side which will convert the C++ exception to a rust error.
+    }
+    #[namespace = "rxx::test::logging"]
+    extern "Rust" {
+        //Integration tests purpose only
+        fn test_log(level: Level, category: String, message: String) -> Result<()>;
     }
 }
 
-pub fn log_message(level: ffi::Level, category: String, message: String) {
-    match ffi::log_message(level, category, message) {
-        Ok(()) => (),
-        Err(e) => {
-            println!("Log message exception: {e}");
-        }
-    };
+pub fn test_log(
+    level: ffi::Level,
+    category: String,
+    message: String,
+) -> Result<(), cxx::Exception> {
+    ffi::log_message(level, category, message)
 }
