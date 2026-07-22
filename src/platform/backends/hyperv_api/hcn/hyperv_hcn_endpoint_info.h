ea68ccfb089abeafba1fd1c4987e4e755d169b95
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2026-07-22 18:08:53 +0300

    [hyperv-hcn] Pull MAC from endpoint info

--- src/platform/backends/hyperv_api/hcn/hyperv_hcn_endpoint_info.h
+++ src/platform/backends/hyperv_api/hcn/hyperv_hcn_endpoint_info.h
@@ -17,6 +17,7 @@
 
 #pragma once
 
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -24,6 +25,7 @@ namespace multipass::hyperv::hcn
 {
 struct HcnEndpointInfo
 {
+    std::optional<std::string> mac_address;
     std::vector<std::string> ip_addresses;
 };
 } // namespace multipass::hyperv::hcn
