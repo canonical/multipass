d7249a2ec11836fbc86577f4c5c743c49d364344
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2026-07-22 18:01:37 +0300

    [win] Add permanent_ipv4_neighbor helper

---
+++ src/platform/backends/shared/windows/network_utils.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <optional>
+#include <string>
+
+namespace multipass
+{
+[[nodiscard]] std::optional<std::string> permanent_ipv4_neighbor(const std::string& mac_address);
+}
