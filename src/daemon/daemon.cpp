e841eb12bf1cd8501dfb622a1dfd42fd9b279bc8
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2026-09-02 13:17:00 +0200

    [az] Small fixes to AZ support detection

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -2906,6 +2906,7 @@ try // clang-format on
         reply_zone->set_name(zone.get().get_name());
         reply_zone->set_subnet(zone.get().get_subnet().to_cidr());
         reply_zone->set_available(zone.get().is_available());
+        reply_zone->set_supported(true);
     }
 
     server->Write(response);
