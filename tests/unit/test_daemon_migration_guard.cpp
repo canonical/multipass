c0ff01956b357643a51f79ae171038d0adc9112e
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2026-09-09 13:26:30 +0300

    [daemon] Better scope reject if migrating

--- tests/unit/test_daemon_migration_guard.cpp
+++ tests/unit/test_daemon_migration_guard.cpp
@@ -19,6 +19,8 @@
 #include "daemon_test_fixture.h"
 
 #include "common.h"
+#include "mock_daemon.h"
+#include "mock_daemon_rpc_context.h"
 #include "mock_permission_utils.h"
 #include "mock_platform.h"
 #include "mock_server_reader_writer.h"
@@ -29,6 +31,15 @@
 #include <src/daemon/daemon.h>
 
 #include <multipass/constants.h>
+
+#include <QCoreApplication>
+#include <QEvent>
+#include <QThread>
+
+#include <array>
+#include <functional>
+#include <thread>
+#include <tuple>
 
 #if defined(HYPERV_HCS_ENABLED)
 #include "hyperv_api/mock_hyperv_hcn_wrapper.h"
@@ -44,27 +55,31 @@ using namespace testing;
 
 namespace
 {
-// White-box subclass for migration-state setup.
-struct GuardTestDaemon : public mp::Daemon
+template <typename Base>
+struct MigrationTestDaemon : public Base
 {
-    using mp::Daemon::Daemon;
+    using Base::Base;
+    using Base::connect_rpc;
 
-    void begin_migration()
+    void set_migrating(bool migrating)
     {
-        migration_in_progress = true;
+        this->migration_in_progress = migrating;
     }
 
     bool is_migrating() const
     {
-        return migration_in_progress.load();
+        return this->migration_in_progress.load();
     }
 
     void add_instance(const std::string& name, mp::VirtualMachine::ShPtr vm)
     {
-        operative_instances.emplace(name, std::move(vm));
-        vm_instance_specs.emplace(name, mp::VMSpecs{});
+        this->operative_instances.emplace(name, std::move(vm));
+        this->vm_instance_specs.emplace(name, mp::VMSpecs{});
     }
 };
+
+using GuardTestDaemon = MigrationTestDaemon<mp::Daemon>;
+using MockGuardTestDaemon = MigrationTestDaemon<StrictMock<mpt::MockDaemon>>;
 
 struct TestDaemonMigrationGuard : public mpt::DaemonTestFixture
 {
@@ -74,17 +89,6 @@ struct TestDaemonMigrationGuard : public mpt::DaemonTestFixture
         EXPECT_CALL(mock_settings, unregister_handler).Times(AnyNumber());
         config_builder.vault = std::make_unique<NiceMock<mpt::MockVMImageVault>>();
         config_builder.server_address = "localhost:0";
-    }
-
-    template <typename Reply, typename Request, typename DaemonSlotPtr>
-    grpc::Status call_while_migrating(DaemonSlotPtr slot, const Request& request)
-    {
-        GuardTestDaemon daemon{config_builder.build()};
-        daemon.begin_migration();
-        // A guarded RPC must reject before doing any work, so it must never write a reply.
-        StrictMock<mpt::MockServerReaderWriter<Reply, Request>> server;
-        EXPECT_CALL(server, Write(_, _)).Times(0);
-        return call_daemon_slot(daemon, slot, request, std::move(server));
     }
 
     mpt::MockPlatform::GuardedMock platform_attr{mpt::MockPlatform::inject<NiceMock>()};
@@ -98,37 +102,261 @@ struct TestDaemonMigrationGuard : public mpt::DaemonTestFixture
         mpt::MockPermissionUtils::inject<NiceMock>();
     mpt::MockPermissionUtils& mock_permission_utils = *mock_permission_utils_injection.first;
 };
+
+struct TestDaemonRpcMigrationGuard : public TestDaemonMigrationGuard
+{
+    void SetUp() override
+    {
+        TestDaemonMigrationGuard::SetUp();
+        auto config = config_builder.build();
+        const auto& rpc_config = *config;
+        daemon = std::make_unique<MockGuardTestDaemon>(std::move(config));
+        rpc = std::make_unique<mp::DaemonRpc>(rpc_config.server_address,
+                                              *rpc_config.cert_provider,
+                                              rpc_config.client_cert_store.get(),
+                                              rpc_config.logger);
+        daemon->connect_rpc(*rpc);
+    }
+
+    std::unique_ptr<MockGuardTestDaemon> daemon;
+    std::unique_ptr<mp::DaemonRpc> rpc;
+};
+
+const auto finish_rpc = [](auto*, auto*, mp::DaemonRpcContext* context) {
+    context->set_value(grpc::Status::OK);
+};
+
+struct RpcAdmissionCase
+{
+    const char* name;
+    bool blocked_during_migration;
+    std::function<void(mp::DaemonRpc&, mp::DaemonRpcContext*)> dispatch;
+    std::function<void(mpt::MockDaemon&)> expect_dispatch;
+};
+
+template <typename Signal>
+RpcAdmissionCase rpc_case(const char* name,
+                          bool blocked_during_migration,
+                          Signal signal,
+                          std::function<void(mpt::MockDaemon&)> expect_dispatch)
+{
+    return {name,
+            blocked_during_migration,
+            [signal](mp::DaemonRpc& rpc, mp::DaemonRpcContext* context) {
+                // Only mocked handlers receive these; neither request nor stream is accessed.
+                (rpc.*signal)(nullptr, nullptr, context);
+            },
+            std::move(expect_dispatch)};
+}
+
+const auto rpc_cases = std::array{
+    rpc_case("create",
+             true,
+             &mp::DaemonRpc::on_create,
+             [](auto& daemon) { EXPECT_CALL(daemon, create).WillOnce(finish_rpc); }),
+    rpc_case("launch",
+             true,
+             &mp::DaemonRpc::on_launch,
+             [](auto& daemon) { EXPECT_CALL(daemon, launch).WillOnce(finish_rpc); }),
+    rpc_case("purge",
+             true,
+             &mp::DaemonRpc::on_purge,
+             [](auto& daemon) { EXPECT_CALL(daemon, purge).WillOnce(finish_rpc); }),
+    rpc_case("find",
+             false,
+             &mp::DaemonRpc::on_find,
+             [](auto& daemon) { EXPECT_CALL(daemon, find).WillOnce(finish_rpc); }),
+    rpc_case("info",
+             false,
+             &mp::DaemonRpc::on_info,
+             [](auto& daemon) { EXPECT_CALL(daemon, info).WillOnce(finish_rpc); }),
+    rpc_case("list",
+             false,
+             &mp::DaemonRpc::on_list,
+             [](auto& daemon) { EXPECT_CALL(daemon, list).WillOnce(finish_rpc); }),
+    rpc_case("clone",
+             true,
+             &mp::DaemonRpc::on_clone,
+             [](auto& daemon) { EXPECT_CALL(daemon, clone).WillOnce(finish_rpc); }),
+    rpc_case("networks",
+             false,
+             &mp::DaemonRpc::on_networks,
+             [](auto& daemon) { EXPECT_CALL(daemon, networks).WillOnce(finish_rpc); }),
+    rpc_case("mount",
+             true,
+             &mp::DaemonRpc::on_mount,
+             [](auto& daemon) { EXPECT_CALL(daemon, mount).WillOnce(finish_rpc); }),
+    rpc_case("recover",
+             true,
+             &mp::DaemonRpc::on_recover,
+             [](auto& daemon) { EXPECT_CALL(daemon, recover).WillOnce(finish_rpc); }),
+    rpc_case("ssh_info",
+             false,
+             &mp::DaemonRpc::on_ssh_info,
+             [](auto& daemon) { EXPECT_CALL(daemon, ssh_info).WillOnce(finish_rpc); }),
+    rpc_case("start",
+             true,
+             &mp::DaemonRpc::on_start,
+             [](auto& daemon) { EXPECT_CALL(daemon, start).WillOnce(finish_rpc); }),
+    rpc_case("stop",
+             true,
+             &mp::DaemonRpc::on_stop,
+             [](auto& daemon) { EXPECT_CALL(daemon, stop).WillOnce(finish_rpc); }),
+    rpc_case("suspend",
+             true,
+             &mp::DaemonRpc::on_suspend,
+             [](auto& daemon) { EXPECT_CALL(daemon, suspend).WillOnce(finish_rpc); }),
+    rpc_case("restart",
+             true,
+             &mp::DaemonRpc::on_restart,
+             [](auto& daemon) { EXPECT_CALL(daemon, restart).WillOnce(finish_rpc); }),
+    rpc_case("delete",
+             true,
+             &mp::DaemonRpc::on_delete,
+             [](auto& daemon) { EXPECT_CALL(daemon, delet).WillOnce(finish_rpc); }),
+    rpc_case("umount",
+             true,
+             &mp::DaemonRpc::on_umount,
+             [](auto& daemon) { EXPECT_CALL(daemon, umount).WillOnce(finish_rpc); }),
+    rpc_case("version",
+             false,
+             &mp::DaemonRpc::on_version,
+             [](auto& daemon) { EXPECT_CALL(daemon, version).WillOnce(finish_rpc); }),
+    rpc_case("get",
+             false,
+             &mp::DaemonRpc::on_get,
+             [](auto& daemon) { EXPECT_CALL(daemon, get).WillOnce(finish_rpc); }),
+    rpc_case("set",
+             true,
+             &mp::DaemonRpc::on_set,
+             [](auto& daemon) { EXPECT_CALL(daemon, set).WillOnce(finish_rpc); }),
+    rpc_case("keys",
+             false,
+             &mp::DaemonRpc::on_keys,
+             [](auto& daemon) { EXPECT_CALL(daemon, keys).WillOnce(finish_rpc); }),
+    rpc_case("authenticate",
+             false,
+             &mp::DaemonRpc::on_authenticate,
+             [](auto& daemon) { EXPECT_CALL(daemon, authenticate).WillOnce(finish_rpc); }),
+    rpc_case("snapshot",
+             true,
+             &mp::DaemonRpc::on_snapshot,
+             [](auto& daemon) { EXPECT_CALL(daemon, snapshot).WillOnce(finish_rpc); }),
+    rpc_case("restore",
+             true,
+             &mp::DaemonRpc::on_restore,
+             [](auto& daemon) { EXPECT_CALL(daemon, restore).WillOnce(finish_rpc); }),
+    rpc_case("daemon_info",
+             false,
+             &mp::DaemonRpc::on_daemon_info,
+             [](auto& daemon) { EXPECT_CALL(daemon, daemon_info).WillOnce(finish_rpc); }),
+    rpc_case("wait_ready",
+             false,
+             &mp::DaemonRpc::on_wait_ready,
+             [](auto& daemon) { EXPECT_CALL(daemon, wait_ready).WillOnce(finish_rpc); }),
+    rpc_case("zones",
+             false,
+             &mp::DaemonRpc::on_zones,
+             [](auto& daemon) { EXPECT_CALL(daemon, zones).WillOnce(finish_rpc); }),
+    rpc_case("zones_state", true, &mp::DaemonRpc::on_zones_state, [](auto& daemon) {
+        EXPECT_CALL(daemon, zones_state).WillOnce(finish_rpc);
+    })};
+
+struct TestDaemonRpcAdmission : public TestDaemonRpcMigrationGuard,
+                                public WithParamInterface<std::tuple<RpcAdmissionCase, bool>>
+{
+};
 } // namespace
 
-TEST_F(TestDaemonMigrationGuard, startRejectedWhileMigrating)
+TEST_P(TestDaemonRpcAdmission, dispatchHonoursMigrationPolicy)
 {
-    const auto status = call_while_migrating<mp::StartReply>(&mp::Daemon::start,
-                                                             mp::StartRequest{});
-    EXPECT_EQ(status.error_code(), grpc::StatusCode::FAILED_PRECONDITION);
-    EXPECT_THAT(status.error_message(),
-                AllOf(HasSubstr("start instances"), HasSubstr("migration")));
+    const auto& [rpc_case, migrating] = GetParam();
+    daemon->set_migrating(migrating);
+    StrictMock<mpt::MockDaemonRpcContext> context;
+
+    if (migrating && rpc_case.blocked_during_migration)
+    {
+        EXPECT_CALL(context, set_value).WillOnce([](const grpc::Status& status) {
+            EXPECT_EQ(status.error_code(), grpc::StatusCode::FAILED_PRECONDITION);
+            EXPECT_EQ(status.error_message(),
+                      "Cannot perform this operation while a Hyper-V instance migration is in "
+                      "progress. Please wait for the migration to finish and try again.");
+        });
+    }
+    else
+    {
+        rpc_case.expect_dispatch(*daemon);
+        EXPECT_CALL(context, set_value).WillOnce([](const grpc::Status& status) {
+            EXPECT_TRUE(status.ok());
+        });
+    }
+
+    rpc_case.dispatch(*rpc, &context);
 }
 
-TEST_F(TestDaemonMigrationGuard, settingsRejectedWhileMigrating)
+INSTANTIATE_TEST_SUITE_P(AllRpcs,
+                         TestDaemonRpcAdmission,
+                         Combine(ValuesIn(rpc_cases), Bool()),
+                         [](const auto& info) {
+                             return std::string{std::get<0>(info.param).name} +
+                                    (std::get<1>(info.param) ? "DuringMigration"
+                                                             : "WithoutMigration");
+                         });
+
+TEST_F(TestDaemonRpcMigrationGuard, rejectsQueuedRpcIfMigrationStartsBeforeDispatch)
 {
-    const auto status = call_while_migrating<mp::SetReply>(&mp::Daemon::set, mp::SetRequest{});
-    EXPECT_EQ(status.error_code(), grpc::StatusCode::FAILED_PRECONDITION);
-    EXPECT_THAT(status.error_message(), HasSubstr("Cannot change settings"));
+    StrictMock<mpt::MockDaemonRpcContext> context;
+    bool completed = false;
+    EXPECT_CALL(context, set_value).WillOnce([&](const grpc::Status& status) {
+        EXPECT_EQ(QThread::currentThread(), daemon->thread());
+        EXPECT_EQ(status.error_code(), grpc::StatusCode::FAILED_PRECONDITION);
+        completed = true;
+    });
+
+    std::thread{[&] { rpc->on_start(nullptr, nullptr, &context); }}.join();
+    EXPECT_FALSE(completed);
+    daemon->set_migrating(true);
+    QCoreApplication::sendPostedEvents(daemon.get(), QEvent::MetaCall);
+
+    EXPECT_TRUE(completed);
 }
 
-TEST_F(TestDaemonMigrationGuard, versionAllowedWhileMigrating)
+TEST_F(TestDaemonRpcMigrationGuard, allowsQueuedRpcIfMigrationEndsBeforeDispatch)
 {
-    GuardTestDaemon daemon{config_builder.build()};
-    daemon.begin_migration();
+    daemon->set_migrating(true);
+    StrictMock<mpt::MockDaemonRpcContext> context;
+    bool completed = false;
+    EXPECT_CALL(*daemon, start).WillOnce([&](auto* request, auto* server, auto* context) {
+        EXPECT_EQ(QThread::currentThread(), daemon->thread());
+        finish_rpc(request, server, context);
+    });
+    EXPECT_CALL(context, set_value).WillOnce([&](const grpc::Status& status) {
+        EXPECT_TRUE(status.ok());
+        completed = true;
+    });
 
-    StrictMock<mpt::MockServerReaderWriter<mp::VersionReply, mp::VersionRequest>> server;
-    EXPECT_CALL(server, Write(_, _)).Times(1);
+    std::thread{[&] { rpc->on_start(nullptr, nullptr, &context); }}.join();
+    EXPECT_FALSE(completed);
+    daemon->set_migrating(false);
+    QCoreApplication::sendPostedEvents(daemon.get(), QEvent::MetaCall);
 
-    const auto status = call_daemon_slot(daemon,
-                                         &mp::Daemon::version,
-                                         mp::VersionRequest{},
-                                         server);
-    EXPECT_TRUE(status.ok());
+    EXPECT_TRUE(completed);
+}
+
+TEST_F(TestDaemonRpcMigrationGuard, initiatingSettingCanAcquireMigrationGuard)
+{
+    StrictMock<mpt::MockDaemonRpcContext> context;
+    EXPECT_CALL(*daemon, set).WillOnce([&](auto* request, auto* server, auto* context) {
+        daemon->set_migrating(true);
+        finish_rpc(request, server, context);
+    });
+    EXPECT_CALL(context, set_value).WillOnce([](const grpc::Status& status) {
+        EXPECT_TRUE(status.ok());
+    });
+
+    rpc->on_set(nullptr, nullptr, &context);
+
+    EXPECT_TRUE(daemon->is_migrating());
 }
 
 #if defined(HYPERV_HCS_ENABLED)
