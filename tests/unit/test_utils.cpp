6c117ed038e1b4ce03125d50691c14ce749210b3
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2026-09-16 16:24:15 +0200

    [tests] Add tests for cloud-init shutdown support

--- tests/unit/test_utils.cpp
+++ tests/unit/test_utils.cpp
@@ -28,6 +28,8 @@
 #include <multipass/utils.h>
 #include <multipass/vm_image_vault.h>
 
+#include <yaml-cpp/yaml.h>
+
 #include <QRegularExpression>
 
 #include <gtest/gtest-death-test.h>
@@ -951,4 +953,48 @@ TEST_F(TestReapSSHProcess, reapThrowsSSHExecFailureOnNonZeroExitCode)
         EXPECT_EQ(e.exit_code(), 42);
         EXPECT_THAT(e.what(), HasSubstr("boom"));
     }
+}
+
+struct TestExpectsShutdownFromCloudInit : public TestWithParam<std::tuple<std::string, bool>>
+{
+};
+
+TEST_P(TestExpectsShutdownFromCloudInit, returnsExpectedResultForMode)
+{
+    const auto& [mode, expected_result] = GetParam();
+
+    YAML::Node user_data_config;
+    user_data_config["power_state"]["mode"] = mode;
+
+    EXPECT_EQ(mpu::expects_shutdown_from_cloud_init(user_data_config), expected_result);
+}
+
+INSTANTIATE_TEST_SUITE_P(Utils,
+                         TestExpectsShutdownFromCloudInit,
+                         Values(std::make_tuple("poweroff", true),
+                                std::make_tuple("halt", true),
+                                std::make_tuple("reboot", false),
+                                std::make_tuple("some_other_mode", false)));
+
+TEST(ExpectsShutdownFromCloudInit, returnsFalseWhenPowerStateMissing)
+{
+    YAML::Node user_data_config;
+    user_data_config["some_other_key"] = "some_value";
+
+    EXPECT_FALSE(mpu::expects_shutdown_from_cloud_init(user_data_config));
+}
+
+TEST(ExpectsShutdownFromCloudInit, returnsFalseWhenModeMissing)
+{
+    YAML::Node user_data_config;
+    user_data_config["power_state"]["message"] = "Bye Bye";
+
+    EXPECT_FALSE(mpu::expects_shutdown_from_cloud_init(user_data_config));
+}
+
+TEST(ExpectsShutdownFromCloudInit, returnsFalseWhenConfigEmpty)
+{
+    YAML::Node user_data_config;
+
+    EXPECT_FALSE(mpu::expects_shutdown_from_cloud_init(user_data_config));
 }
