79e8ddd7cea9803ac7f280987ef9edbaf1e50fdc
Ahmed Essam <ahmed.essam@canonical.com>
2026-09-22 07:07:53 +0300

    [tests] Use parameterized tests

--- tests/unit/windows/test_native_path.cpp
+++ tests/unit/windows/test_native_path.cpp
@@ -32,9 +32,53 @@ struct TestCase
     std::wstring json_path;
 };
 
-struct NativePath_UnitTests : public ::testing::Test
+struct NativePath_PUnitTests : public ::testing::TestWithParam<TestCase>
 {
-    const std::array<TestCase, 10> test_cases = {
+};
+
+TEST_P(NativePath_PUnitTests, uses_native_separators_on_construction)
+{
+    const auto& test_case = GetParam();
+    NativePath p{test_case.path};
+    EXPECT_EQ(p.wstring(), test_case.native_path);
+}
+
+TEST_P(NativePath_PUnitTests, uses_native_separators_on_assignment)
+{
+    const auto& test_case = GetParam();
+    NativePath p{"."};
+    p = test_case.path;
+    EXPECT_EQ(p.wstring(), test_case.native_path);
+}
+
+TEST_P(NativePath_PUnitTests, fmt_produces_json_serializable)
+{
+    const auto& test_case = GetParam();
+    EXPECT_EQ(fmt::to_wstring(NativePath{test_case.path}), test_case.json_path);
+    EXPECT_EQ(fmt::to_wstring(NativePath{test_case.native_path}), test_case.json_path);
+}
+
+TEST(NativePath_UnitTests, supports_same_type_assignment)
+{
+    const NativePath expected{"some/path"};
+    NativePath actual{"other/path"};
+    actual = expected;
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(NativePath_UnitTests, fmt_honors_string_format_options)
+{
+    const NativePath path{L"some/path"};
+
+    EXPECT_EQ(fmt::format("{:*>14}", path), "****some\\\\path");
+    EXPECT_EQ(fmt::format(L"{:*>14}", path), L"****some\\\\path");
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    NativePath,
+    NativePath_PUnitTests,
+    ::testing::ValuesIn({
         TestCase{L"E:/user/disk.vhdx", L"E:\\user\\disk.vhdx", L"E:\\\\user\\\\disk.vhdx"},
         TestCase{L"./user/disk.vhdx", L".\\user\\disk.vhdx", L".\\\\user\\\\disk.vhdx"},
         TestCase{L"user/disk.vhdx", L"user\\disk.vhdx", L"user\\\\disk.vhdx"},
@@ -45,51 +89,5 @@ struct NativePath_UnitTests : public ::testing::Test
         TestCase{L"用户/диск.vhdx", L"用户\\диск.vhdx", L"用户\\\\диск.vhdx"},
         TestCase{L"用户/", L"用户\\", L"用户\\\\"},
         TestCase{L"диск.vhdx", L"диск.vhdx", L"диск.vhdx"},
-    };
-};
-
-TEST_F(NativePath_UnitTests, uses_native_separators_on_construction)
-{
-    for (const auto& test_case : test_cases)
-    {
-        NativePath p{test_case.path};
-        EXPECT_EQ(p.wstring(), test_case.native_path);
-    }
-}
-
-TEST_F(NativePath_UnitTests, uses_native_separators_on_assignment)
-{
-    for (const auto& test_case : test_cases)
-    {
-        NativePath p{"."};
-        p = test_case.path;
-        EXPECT_EQ(p.wstring(), test_case.native_path);
-    }
-}
-
-TEST_F(NativePath_UnitTests, supports_same_type_assignment)
-{
-    const NativePath expected{"some/path"};
-    NativePath actual{"other/path"};
-    actual = expected;
-
-    EXPECT_EQ(actual, expected);
-}
-
-TEST_F(NativePath_UnitTests, fmt_produces_json_serializable)
-{
-    for (const auto& test_case : test_cases)
-    {
-        EXPECT_EQ(fmt::to_wstring(NativePath{test_case.path}), test_case.json_path);
-        EXPECT_EQ(fmt::to_wstring(NativePath{test_case.native_path}), test_case.json_path);
-    }
-}
-
-TEST_F(NativePath_UnitTests, fmt_honors_string_format_options)
-{
-    const NativePath path{L"some/path"};
-
-    EXPECT_EQ(fmt::format("{:*>14}", path), "****some\\\\path");
-    EXPECT_EQ(fmt::format(L"{:*>14}", path), L"****some\\\\path");
-}
+    }));
 } // namespace multipass::test
