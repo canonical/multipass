100f2723afea0da0b95c5b84ca9f08dc44992bcb
Scott Harder <scott.harder@canonical.com>
2026-09-25 13:33:16 -0500

    [cli] Dup snapshot list logic into new command

---
+++ src/client/cli/cmd/snapshots.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <multipass/cli/command.h>
+
+namespace multipass
+{
+class Formatter;
+
+namespace cmd
+{
+class Snapshots final : public Command
+{
+public:
+    using Command::Command;
+    ReturnCodeVariant run(ArgParser* parser) override;
+
+    std::string name() const override;
+    QString short_help() const override;
+    QString description() const override;
+
+private:
+    ParseCode parse_args(ArgParser* parser);
+
+    SnapshotsRequest request;
+    Formatter* chosen_formatter;
+};
+} // namespace cmd
+} // namespace multipass
--- tests/unit/test_cli_client.cpp
+++ tests/unit/test_cli_client.cpp
@@ -101,6 +101,11 @@ struct MockDaemonRpc : public mp::DaemonRpc
                  (grpc::ServerReaderWriter<mp::ListReply, mp::ListRequest> * server)),
                 (override));
     MOCK_METHOD(grpc::Status,
+                snapshots,
+                (grpc::ServerContext * context,
+                 (grpc::ServerReaderWriter<mp::SnapshotsReply, mp::SnapshotsRequest> * server)),
+                (override));
+    MOCK_METHOD(grpc::Status,
                 mount,
                 (grpc::ServerContext * context,
                  (grpc::ServerReaderWriter<mp::MountReply, mp::MountRequest> * server)),
@@ -2165,6 +2170,28 @@ TEST_F(Client, listCmdFailsWithIpv4AndSnapshots)
 {
     EXPECT_THAT(send_command({"list", "--no-ipv4", "--snapshots"}),
                 Eq(mp::ReturnCode::CommandLineError));
+}
+
+// snapshots cli tests
+TEST_F(Client, snapshotsCmdOkNoArgs)
+{
+    mp::SnapshotsReply reply;
+    reply.mutable_snapshot_list();
+
+    EXPECT_CALL(mock_daemon, snapshots)
+        .WillOnce(WithArg<1>(
+            check_request_and_return<mp::SnapshotsReply, mp::SnapshotsRequest>(_, ok, reply)));
+    EXPECT_THAT(send_command({"snapshots"}), Eq(mp::ReturnCode::Ok));
+}
+
+TEST_F(Client, snapshotsCmdFailsWithArgs)
+{
+    EXPECT_THAT(send_command({"snapshots", "foo"}), Eq(mp::ReturnCode::CommandLineError));
+}
+
+TEST_F(Client, snapshotsCmdHelpOk)
+{
+    EXPECT_THAT(send_command({"snapshots", "-h"}), Eq(mp::ReturnCode::Ok));
 }
 
 // mount cli tests
